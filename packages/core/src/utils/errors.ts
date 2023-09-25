befa3acb6987024cfafe804eb3a4f2dff56e57c7
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-09-25 11:11:36 +0200

    feat: Error constants in order to handling better errors

---
+++ packages/core/src/utils/errors.ts
@@ -0,0 +1,17 @@
+/**
+ * Error messages
+ */
+const ERRORS = {
+
+  /**
+   * Secp256k1 error messages
+   */
+  SECP256K1: {
+    INVALID_PRIVATE_KEY: 'Invalid private key',
+    INVALID_MESSAGE_HASH: 'Invalid message hash',
+    INVALID_SIGNATURE: 'Invalid signature',
+    INVALID_SIGNATURE_RECOVERY: 'Invalid signature recovery'
+  }
+}
+
+export { ERRORS }
