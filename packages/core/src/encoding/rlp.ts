bcc638d673eb1e59586a35b79ae35c81cc002768
Antonio Senatore <bacco1977@gmail.com>
2023-09-26 15:15:17 +0100

    Fixed LINT errors

--- packages/core/src/encoding/rlp.ts
+++ packages/core/src/encoding/rlp.ts
@@ -1,26 +1,24 @@
-import * as rlp from 'rlp';
-
+import * as rlp from 'rlp'
 class RLP {
-    /**
-     * Encodes data using the Ethereumjs RLP library.
-     * @param data - The data to be encoded.
-     * @returns The encoded data as a Buffer.
-     */
-    static encode(data: any): Buffer {
-        const encodedData = rlp.encode(data);
-        return Buffer.from(encodedData);
-    }
+  /**
+   * Encodes data using the Ethereumjs RLP library.
+   * @param data - The data to be encoded.
+   * @returns The encoded data as a Buffer.
+   */
+  encode (data: any): Buffer {
+    const encodedData = rlp.encode(data)
+    return Buffer.from(encodedData)
+  }
 
-    /**
-     * Decodes RLP-encoded data using the Ethereumjs RLP library.
-     * @param encodedData - The RLP-encoded data as a Buffer.
-     * @returns The decoded data or null if decoding fails.
-     */
-    static decode(encodedData: Buffer): any {
-        const decodedData = rlp.decode(encodedData);
-        return decodedData;
-    }
+  /**
+   * Decodes RLP-encoded data using the Ethereumjs RLP library.
+   * @param encodedData - The RLP-encoded data as a Buffer.
+   * @returns The decoded data or null if decoding fails.
+   */
+  decode (encodedData: Buffer): any {
+    const decodedData = rlp.decode(encodedData)
+    return decodedData
+  }
 }
 
-export {RLP};
-
+export { RLP }
--- packages/core/tests/rlp.test.ts
+++ packages/core/tests/rlp.test.ts
@@ -1,83 +1,57 @@
-
-
 import { describe, expect, it } from '@jest/globals'
-
-
 import { RLP } from '../src/encoding/rlp'
-
-
+const rlpInstance = new RLP()
 describe('RLP', () => {
   describe('encode', () => {
     it('should encode an array correctly', () => {
-      const dataToEncode = [1, 2, 3, [4, 5]];
-      const encodedData = RLP.encode(dataToEncode);
-      const expectedHex = 'c6010203c20405'; // The expected hexadecimal encoding
+      const dataToEncode = [1, 2, 3, [4, 5]]
+      const encodedData = rlpInstance.encode(dataToEncode)
+      const expectedHex = 'c6010203c20405' // The expected hexadecimal encoding
 
       // Assert that the encoded data matches the expected value
-      expect(encodedData.toString('hex')).toEqual(expectedHex);
-    });
+      expect(encodedData.toString('hex')).toEqual(expectedHex)
+    })
 
     it('should encode a single value correctly', () => {
-      const dataToEncode = 42;
-      const encodedData = RLP.encode(dataToEncode);
-      const expectedHex = '2a'; // The expected hexadecimal encoding
+      const dataToEncode = 42
+      const encodedData = rlpInstance.encode(dataToEncode)
+      const expectedHex = '2a' // The expected hexadecimal encoding
 
       // Assert that the encoded data matches the expected value
-      expect(encodedData.toString('hex')).toEqual(expectedHex);
-    });
+      expect(encodedData.toString('hex')).toEqual(expectedHex)
+    })
 
     it('should encode an empty array correctly', () => {
-      const dataToEncode: any[] = [];
-      const encodedData = RLP.encode(dataToEncode);
-      const expectedHex = 'c0'; // The expected hexadecimal encoding
+      const dataToEncode: any[] = []
+      const encodedData = rlpInstance.encode(dataToEncode)
+      const expectedHex = 'c0' // The expected hexadecimal encoding
 
       // Assert that the encoded data matches the expected value
-      expect(encodedData.toString('hex')).toEqual(expectedHex);
-    });
-  });
+      expect(encodedData.toString('hex')).toEqual(expectedHex)
+    })
+  })
 
   describe('decode', () => {
-   /* it('should decode an encoded array correctly', () => {
-      const encodedHex = 'c583010203c5040405';
-      const encodedData = Buffer.from(encodedHex, 'hex');
-      const decodedData = RLP.decode(encodedData);
-      const expectedDecodedData = [1, 2, 3, [4, 5]]; // The expected decoded data
-
-      // Assert that the decoded data matches the expected value
-      expect(decodedData).toEqual(expectedDecodedData);
-    });*/
-
     it('should decode an encoded single value correctly', () => {
-      const encodedHex = '2a';
-      const encodedData = Buffer.from(encodedHex, 'hex');
-      const decodedData = RLP.decode(encodedData);
+      const encodedHex = '2a'
+      const encodedData = Buffer.from(encodedHex, 'hex')
+      const decodedData = rlpInstance.decode(encodedData)
 
       // Adjust the expected decoded data to match the received Buffer
-      const expectedDecodedData = Buffer.from([42]);
+      const expectedDecodedData = Buffer.from([42])
 
       // Assert that the decoded data matches the expected value
-      expect(decodedData.equals(expectedDecodedData)).toBe(true);
-    });
+      expect(decodedData.equals(expectedDecodedData)).toBe(true)
+    })
 
     it('should decode an empty encoded array correctly', () => {
-      const encodedHex = 'c0';
-      const encodedData = Buffer.from(encodedHex, 'hex');
-      const decodedData = RLP.decode(encodedData);
-      const expectedDecodedData: any[] = []; // The expected decoded data
+      const encodedHex = 'c0'
+      const encodedData = Buffer.from(encodedHex, 'hex')
+      const decodedData = rlpInstance.decode(encodedData)
+      const expectedDecodedData: any[] = [] // The expected decoded data
 
       // Assert that the decoded data matches the expected value
-      expect(decodedData).toEqual(expectedDecodedData);
-    });
-
-   /* it('should decode an encoded string correctly', () => {
-      // Encoding the string "hello" in RLP format
-      const encodedHex = '8368656c6c6f'; // Correct encoding of "hello"
-      const encodedData = Buffer.from(encodedHex, 'hex');
-      const decodedData = RLP.decode(encodedData);
-      const expectedDecodedData = 'hello'; // The expected decoded data
-
-      // Assert that the decoded data matches the expected value
-      expect(decodedData).toEqual(expectedDecodedData);
-    });*/
-  });
-});
+      expect(decodedData).toEqual(expectedDecodedData)
+    })
+  })
+})
