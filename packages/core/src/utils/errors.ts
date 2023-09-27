1471dbd38a700cd155c98ff5daa41975dcf484dd
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2023-09-27 08:28:15 +0100

    feat: added standard Eslint ruling for codebase source files (#50)
    * feat: eslint ruling
    
    * feat: prettier config
    
    * feat: vscode workspace settings for auto formatting on save
    
    * feat: test & format scripts
    
    * refactor: eslint formatting on source files
    
    * chore: package json & minors
    
    * fix: couldn't find the plugin "eslint-plugin-prettier"

--- packages/core/src/utils/errors.ts
+++ packages/core/src/utils/errors.ts
@@ -2,16 +2,15 @@
  * Error messages
  */
 const ERRORS = {
+    /**
+     * Secp256k1 error messages
+     */
+    SECP256K1: {
+        INVALID_PRIVATE_KEY: 'Invalid private key',
+        INVALID_MESSAGE_HASH: 'Invalid message hash',
+        INVALID_SIGNATURE: 'Invalid signature',
+        INVALID_SIGNATURE_RECOVERY: 'Invalid signature recovery'
+    }
+};
 
-  /**
-   * Secp256k1 error messages
-   */
-  SECP256K1: {
-    INVALID_PRIVATE_KEY: 'Invalid private key',
-    INVALID_MESSAGE_HASH: 'Invalid message hash',
-    INVALID_SIGNATURE: 'Invalid signature',
-    INVALID_SIGNATURE_RECOVERY: 'Invalid signature recovery'
-  }
-}
-
-export { ERRORS }
+export { ERRORS };
