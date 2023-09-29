726146498985fe90ec562cedff067f3a5bc6ba09
fabiorigam <fabio.rigamonti@icloud.com>
2023-09-29 15:01:09 +0200

    fix: lint and format mnemonic

--- packages/core/src/mnemonic/mnemonic.ts
+++ packages/core/src/mnemonic/mnemonic.ts
@@ -1,23 +1,23 @@
 import { ethers } from 'ethers';
-import { randomBytes } from 'crypto'
-import { HDNode } from './hdnode/hdnode';
+import { randomBytes } from 'crypto';
+import { HDNode } from '../hdnode/hdnode';
 
 /**
  * generate BIP39 mnemonic words
  * @param rng the optional random number generator, which generates 16~32 (step 4) random bytes.
  * Every 4 bytes produce 3 words.
  */
-function generate(rng?: () => Buffer) {
-    rng = rng ?? (() => randomBytes(128 / 8))
-    return ethers.Mnemonic.fromEntropy(rng())
+function generate(rng?: () => Buffer): ethers.Mnemonic {
+    rng = rng ?? (() => randomBytes(128 / 8));
+    return ethers.Mnemonic.fromEntropy(rng());
 }
 
 /**
  * check if the given mnemonic words have valid checksum
  * @param words mnemonic words
  */
-function validate(words: string[]) {
-    return ethers.Mnemonic.isValidMnemonic(words.join(' '))
+function validate(words: string[]): boolean {
+    return ethers.Mnemonic.isValidMnemonic(words.join(' '));
 }
 
 /**
@@ -25,11 +25,12 @@ function validate(words: string[]) {
  * the derivation path is defined at https://github.com/satoshilabs/slips/blob/master/slip-0044.md
  */
 function derivePrivateKey(words: string[]): Buffer {
-    return HDNode.fromMnemonic(words).derive(0).privateKey!
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return HDNode.fromMnemonic(words).derive(0).privateKey!;
 }
 
 export const mnemonic = {
     generate,
     validate,
     derivePrivateKey
-}
+};
