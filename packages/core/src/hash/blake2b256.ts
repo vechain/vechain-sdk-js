7e3611c1116c7968ccb4180c712c037879bdbe78
Piero Bassa <pierobassa222@gmail.com>
2023-10-03 14:51:22 +0200

    refactor: blake2b256 due to buffer output needed for bloom filter

--- packages/core/src/hash/blake2b256.ts
+++ packages/core/src/hash/blake2b256.ts
@@ -2,23 +2,40 @@ import blake from 'blakejs';
 import { type HashInput } from './types';
 
 /**
- * computes blake2b 256bit hash of given data
- * @param data one or more Buffer | string
+ * Internal function to compute the blake2b 256-bit hash of the given data.
+ *
+ * This function allows hashing multiple data items (Buffers or strings) in a single hash context,
+ * aggregating the hash of all these items into a single result.
+ *
+ * @param ...data - One or more data items (either Buffer or string) to be hashed.
+ * @returns A Buffer containing the 256-bit blake2b hash of the provided data.
  */
-function blake2b256(data: HashInput): string {
-    // Convert BytesLike to Buffer
-    const dataBytesLike = [data] as Array<Buffer | string>;
-
-    // Apply blake2b256 hash on buffer
+function _blake2b256(...data: Array<Buffer | string>): Buffer {
     const ctx = blake.blake2bInit(32);
-    dataBytesLike.forEach((d) => {
+    data.forEach((d) => {
         if (Buffer.isBuffer(d)) {
             blake.blake2bUpdate(ctx, d);
         } else {
             blake.blake2bUpdate(ctx, Buffer.from(d, 'utf8'));
         }
     });
-    return `0x${Buffer.from(blake.blake2bFinal(ctx)).toString('hex')}`;
+    return Buffer.from(blake.blake2bFinal(ctx));
 }
 
-export { blake2b256 };
+/**
+ * Computes the blake2b 256-bit hash of the given data and returns the hash as a hex string.
+ *
+ * This function provides a simplified and human-readable interface to obtain the blake2b hash.
+ * It ensures that the resulting hash is prefixed with `0x` and is in hexadecimal format.
+ *
+ * @param data - The input data (either a Buffer or string) for which the hash needs to be computed.
+ * @returns A string representing the hexadecimal format of the 256-bit blake2b hash, prefixed with `0x`.
+ */
+function blake2b256(data: HashInput): string {
+    // Convert BytesLike to Buffer
+    const dataBytesLike = [data] as Array<Buffer | string>;
+
+    return `0x${_blake2b256(...dataBytesLike).toString('hex')}`;
+}
+
+export { _blake2b256, blake2b256 };
