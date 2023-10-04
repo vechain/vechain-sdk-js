ae14dede6502640688c3f6b0bec57dc6ac45f394
Piero Bassa <pierobassa222@gmail.com>
2023-10-04 14:57:50 +0200

    test: data utils

---
+++ packages/core/tests/utils/data.test.ts
@@ -0,0 +1,63 @@
+import { describe, expect, test } from '@jest/globals';
+import { toHexString, isHexString, removePrefix } from '../../src/utils';
+
+describe('utils/hex', () => {
+    describe('toHexString', () => {
+        test('should convert string to hex string without prefix by default', () => {
+            expect(toHexString('Hello')).toBe('48656c6c6f');
+        });
+
+        test('should convert Uint8Array to hex string without prefix by default', () => {
+            expect(toHexString(new Uint8Array([72, 101, 108, 108, 111]))).toBe(
+                '48656c6c6f'
+            );
+        });
+
+        test('should convert string to hex string with prefix when specified', () => {
+            expect(toHexString('Hello', { withPrefix: true })).toBe(
+                '0x48656c6c6f'
+            );
+        });
+
+        test('should convert Uint8Array to hex string with prefix when specified', () => {
+            expect(
+                toHexString(new Uint8Array([72, 101, 108, 108, 111]), {
+                    withPrefix: true
+                })
+            ).toBe('0x48656c6c6f');
+        });
+    });
+
+    describe('isHexString', () => {
+        const validHexStrings = ['0x48656c6c6f', '48656c6c6f', '0x', ''];
+
+        const invalidHexStrings = [
+            '0xG8656c6c6f',
+            'H8656c6c6f',
+            '0x ',
+            '0x48656c6c6fz'
+        ];
+
+        validHexStrings.forEach((hex) => {
+            test(`should return true for valid hex string: ${hex}`, () => {
+                expect(isHexString(hex)).toBe(true);
+            });
+        });
+
+        invalidHexStrings.forEach((hex) => {
+            test(`should return false for invalid hex string: ${hex}`, () => {
+                expect(isHexString(hex)).toBe(false);
+            });
+        });
+    });
+
+    describe('removePrefix', () => {
+        test('should remove "0x" prefix from hex string', () => {
+            expect(removePrefix('0x48656c6c6f')).toBe('48656c6c6f');
+        });
+
+        test('should not modify hex string without "0x" prefix', () => {
+            expect(removePrefix('48656c6c6f')).toBe('48656c6c6f');
+        });
+    });
+});
