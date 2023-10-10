4cc56995d66caa8bd768571763665d20ae2ff57e
Valazan <al-fede@hotmail.it>
2023-10-10 18:04:15 +0200

    feat: export core library modules

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,29 @@
+import * as address from './address';
+import * as encoding from './encoding';
+import * as mnemonic from './mnemonic';
+import * as hash from './hash';
+import * as hdnode from './hdnode';
+import * as keystore from './keystore';
+import * as secp256k1 from './secp256k1';
+import * as utils from './utils';
+
 export * from './address';
 export * from './encoding';
+export * from './mnemonic';
 export * from './hash';
 export * from './hdnode';
 export * from './keystore';
 export * from './secp256k1';
 export * from './utils';
+
+// Combine all exports into a single object
+export default {
+    ...address,
+    ...encoding,
+    ...mnemonic,
+    ...hash,
+    ...hdnode,
+    ...keystore,
+    ...secp256k1,
+    ...utils
+};
