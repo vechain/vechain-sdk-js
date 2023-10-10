79c517d5afecf2a53edf4ddb0fe4458a99ba4d59
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-10-10 17:02:15 +0200

    feat: fixtures

--- packages/core/tests/abi/fixture.ts
+++ packages/core/tests/abi/fixture.ts
@@ -1,80 +1,101 @@
 import { address } from '../../src';
 
 /**
- * Simple functions
+ * Simple functions fixtures
  */
-
-const function1 = {
-    constant: false,
-    inputs: [
-        {
-            name: 'a1',
-            type: 'uint256'
-        },
-        {
-            name: 'a2',
-            type: 'string'
-        }
-    ],
-    name: 'f1',
-    outputs: [
-        {
-            name: 'r1',
-            type: 'address'
-        },
-        {
-            name: 'r2',
-            type: 'bytes'
-        }
-    ],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function'
-};
-
-const function2 = {
-    inputs: [],
-    name: 'nodes',
-    payable: false,
-    outputs: [
-        {
-            components: [
+const functions = [
+    {
+        objectAbi: {
+            constant: false,
+            inputs: [
                 {
-                    internalType: 'address',
-                    name: 'master',
-                    type: 'address'
+                    name: 'a1',
+                    type: 'uint256'
                 },
                 {
-                    internalType: 'address',
-                    name: 'endorsor',
-                    type: 'address'
-                },
-                {
-                    internalType: 'bytes32',
-                    name: 'identity',
-                    type: 'bytes32'
-                },
-                {
-                    internalType: 'bool',
-                    name: 'active',
-                    type: 'bool'
+                    name: 'a2',
+                    type: 'string'
                 }
             ],
-            internalType: 'struct AuthorityUtils.Candidate[]',
-            name: 'list',
-            type: 'tuple[]'
-        }
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function'
-};
+            name: 'f1',
+            outputs: [
+                {
+                    name: 'r1',
+                    type: 'address'
+                },
+                {
+                    name: 'r2',
+                    type: 'bytes'
+                }
+            ],
+            payable: false,
+            stateMutability: 'nonpayable',
+            type: 'function'
+        },
+        full: 'function f1(uint256 a1, string a2) returns (address r1, bytes r2)',
+        minimal: 'function f1(uint256,string) returns (address,bytes)',
+        sighash: 'f1(uint256,string)',
+        signatureHash: '0x27fcbb2f',
+        jsonStringifiedAbi:
+            '{"type":"function","name":"f1","constant":false,"payable":false,"inputs":[{"type":"uint256","name":"a1"},{"type":"string","name":"a2"}],"outputs":[{"type":"address","name":"r1"},{"type":"bytes","name":"r2"}]}',
+        encodingTestsInputs: [
+            [1n, 'test'],
+            [2n, 'test2']
+        ]
+    },
+    {
+        objectAbi: {
+            inputs: [],
+            name: 'nodes',
+            payable: false,
+            outputs: [
+                {
+                    components: [
+                        {
+                            internalType: 'address',
+                            name: 'master',
+                            type: 'address'
+                        },
+                        {
+                            internalType: 'address',
+                            name: 'endorsor',
+                            type: 'address'
+                        },
+                        {
+                            internalType: 'bytes32',
+                            name: 'identity',
+                            type: 'bytes32'
+                        },
+                        {
+                            internalType: 'bool',
+                            name: 'active',
+                            type: 'bool'
+                        }
+                    ],
+                    internalType: 'struct AuthorityUtils.Candidate[]',
+                    name: 'list',
+                    type: 'tuple[]'
+                }
+            ],
+            stateMutability: 'nonpayable',
+            type: 'function'
+        },
+        full: 'function nodes() returns (tuple(address master, address endorsor, bytes32 identity, bool active)[] list)',
+        minimal:
+            'function nodes() returns (tuple(address,address,bytes32,bool)[])',
+        sighash: 'nodes()',
+        signatureHash: '0x5db82fa0',
+        jsonStringifiedAbi:
+            '{"type":"function","name":"nodes","constant":false,"payable":false,"inputs":[],"outputs":[{"type":"tuple[]","name":"list","components":[{"type":"address","name":"master"},{"type":"address","name":"endorsor"},{"type":"bytes32","name":"identity"},{"type":"bool","name":"active"}]}]}',
+        encodingTestsInputs: [[]]
+    }
+];
 
-const function2SimpleParametersData: Array<{
-    master: string;
-    endorsor: string;
-    identity: string;
-    active: boolean;
-}> = [
+/**
+ * Simple parameters for function 2 (functions[1]) fixture.
+ * Used to test encoding and decoding of multiple parameters in low level abi tests.
+ */
+const simpleParametersDataForFunction2 = [
     {
         master: address.toChecksumed(
             '0x0e8fd586e022f825a109848832d7e552132bc332'
@@ -100,117 +121,70 @@ const function2SimpleParametersData: Array<{
 ];
 
 /**
- * Simple events
+ * Simple events fixtures
  */
-
-const event1 = {
-    anonymous: false,
-    inputs: [
-        {
-            indexed: true,
-            name: 'a1',
-            type: 'uint256'
-        },
-        {
-            indexed: false,
-            name: 'a2',
-            type: 'string'
-        }
-    ],
-    name: 'E1',
-    type: 'event'
-};
-
-const event2 = {
-    anonymous: true,
-    inputs: [
-        {
-            indexed: true,
-            name: 'a1',
-            type: 'uint256'
-        },
-        {
-            indexed: false,
-            name: 'a2',
-            type: 'string'
-        }
-    ],
-    name: 'E2',
-    type: 'event'
-};
-
-const event3 = {
-    anonymous: false,
-    inputs: [
-        {
-            indexed: true,
-            name: 'a1',
-            type: 'uint256'
-        }
-    ],
-    name: 'E3',
-    type: 'event'
-};
-
-const event4 = {
-    inputs: [
-        {
-            indexed: true,
-            name: 'a1',
-            type: 'string'
-        }
-    ],
-    name: 'E4',
-    type: 'event'
-};
-
-const event5 = {
-    inputs: [
-        {
-            indexed: true,
-            name: 'a1',
-            type: 'bytes'
-        }
-    ],
-    name: 'E5',
-    type: 'event'
-};
-
-const event6 = {
-    anonymous: false,
-    inputs: [
-        {
-            components: [
+const events = [
+    {
+        objectAbi: {
+            anonymous: false,
+            inputs: [
                 {
-                    internalType: 'address',
-                    name: 'master',
-                    type: 'address'
+                    indexed: true,
+                    name: 'a1',
+                    type: 'uint256'
                 },
                 {
-                    internalType: 'address',
-                    name: 'endorsor',
-                    type: 'address'
-                },
-                {
-                    internalType: 'bytes32',
-                    name: 'identity',
-                    type: 'bytes32'
-                },
-                {
-                    internalType: 'bool',
-                    name: 'active',
-                    type: 'bool'
+                    indexed: false,
+                    name: 'a2',
+                    type: 'string'
                 }
             ],
-            internalType: 'struct AuthorityUtils.Candidate[]',
-            name: 'nodes',
-            indexed: false,
-            type: 'tuple[]'
-        }
-    ],
-    name: 'Added',
-    type: 'event'
-};
+            name: 'E1',
+            type: 'event'
+        },
+        full: 'event E1(uint256 indexed a1, string a2)',
+        minimal: 'event E1(uint256 indexed,string)',
+        sighash: 'E1(uint256,string)',
+        signatureHash:
+            '0x47b78f0ec63d97830ace2babb45e6271b15a678528e901a9651e45b65105e6c2',
+        jsonStringifiedAbi:
+            '{"type":"event","anonymous":false,"name":"E1","inputs":[{"type":"uint256","name":"a1","indexed":true},{"type":"string","name":"a2","indexed":false}]}',
+        encodingTestsInputs: [
+            [1n, 'test'],
+            [2n, 'test2']
+        ]
+    },
+    {
+        objectAbi: {
+            anonymous: false,
+            inputs: [
+                {
+                    indexed: true,
+                    name: 'a1',
+                    type: 'uint256'
+                },
+                {
+                    indexed: false,
+                    name: 'a2',
+                    type: 'string'
+                }
+            ],
+            name: 'E2',
+            type: 'event'
+        },
+        full: 'event E2(uint256 indexed a1, string a2)',
+        minimal: 'event E2(uint256 indexed,string)',
+        sighash: 'E2(uint256,string)',
+        signatureHash:
+            '0xed4f58f68ac84ec75a04444d287e454e22772efca5f47aca53950d7a135e6dab',
+        jsonStringifiedAbi:
+            '{"type":"event","anonymous":false,"name":"E2","inputs":[{"type":"uint256","name":"a1","indexed":true},{"type":"string","name":"a2","indexed":false}]}',
+        encodingTestsInputs: [
+            [1n, 'test'],
+            [2n, 'test2']
+        ]
+    }
+];
 
 /**
  * Encode and decode parameter valid values
@@ -279,15 +253,9 @@ const encodedDecodedInvalidValues = [
 ];
 
 export {
-    event1,
-    event2,
-    event3,
-    event4,
-    event5,
-    event6,
-    function1,
-    function2,
-    function2SimpleParametersData,
+    events,
+    functions,
+    simpleParametersDataForFunction2,
     encodedDecodedValues,
     encodedDecodedInvalidValues
 };
