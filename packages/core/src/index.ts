dc264e8cac1eaec2c3380be442c8093ef0d82ff9
Valazan <al-fede@hotmail.it>
2023-10-11 19:22:24 +0200

    refactor: export default removed

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,29 +1,5 @@
-import * as address from './address';
-import * as encoding from './encoding';
-import * as mnemonic from './mnemonic';
-import * as hash from './hash';
-import * as hdnode from './hdnode';
-import * as keystore from './keystore';
-import * as secp256k1 from './secp256k1';
-import * as utils from './utils';
+import * as core from './core';
 
-export * from './address';
-export * from './encoding';
-export * from './mnemonic';
-export * from './hash';
-export * from './hdnode';
-export * from './keystore';
-export * from './secp256k1';
-export * from './utils';
+export { core };
 
-// Combine all exports into a single object
-export default {
-    ...address,
-    ...encoding,
-    ...mnemonic,
-    ...hash,
-    ...hdnode,
-    ...keystore,
-    ...secp256k1,
-    ...utils
-};
+export * from './core';
