7650a2412514bbf1ef99c12d39b0dc31b11273c2
Piero Bassa <pierobassa222@gmail.com>
2023-10-11 21:17:22 +0200

    chore: indexing & minors

---
+++ packages/core/src/encoding/rlp/kind/index.ts
@@ -0,0 +1,6 @@
+import { BufferKind } from './bufferkind';
+import { NumericKind } from './numerickind';
+import { ScalarKind } from './skalarkind.abstract';
+
+export const RLPProfiles = { ScalarKind, BufferKind, NumericKind };
+export type { ScalarKind };
