b93e6edb5e06b0f4a1bb9676aac5e28fea2d9b7a
Piero Bassa <pierobassa222@gmail.com>
2023-10-13 11:52:45 +0200

    docs: rlp helpers tests

--- packages/core/tests/rlp/helpers.test.ts
+++ packages/core/tests/rlp/helpers.test.ts
@@ -11,8 +11,27 @@ import {
     validateNumberTestCases
 } from './helpers.fixture';
 
+/**
+ * Test suite for NumericKind helpers.
+ *
+ * This suite contains tests for helper functions meant to validate and manipulate numeric kinds.
+ * The numeric kinds are validated both as numbers and buffers, ensuring correctness
+ * and throwing errors for invalid inputs.
+ */
 describe('test NumericKind helpers', () => {
+    /**
+     * Test subset for `validateNumericKindData` function.
+     *
+     * The tests ensure that the function accurately validates and converts numeric
+     * input data to BigInt, respecting the rules for numeric kind validity.
+     */
     describe('validateNumericKindData', () => {
+        /**
+         * Tests for valid number input scenarios.
+         *
+         * This test iterates over cases where the input number is considered valid,
+         * asserting that the function successfully returns the expected BigInt.
+         */
         validateNumberTestCases.forEach(({ number, context, expected }) => {
             test(`should return BigInt when data is a valid number ${number}`, () => {
                 const result = validateNumericKindData(number, context);
@@ -21,6 +40,12 @@ describe('test NumericKind helpers', () => {
             });
         });
 
+        /**
+         * Tests for invalid number input scenarios.
+         *
+         * This test iterates over cases where the input number is considered invalid,
+         * asserting that the function throws an error with a corresponding message.
+         */
         invalidNumberTestCases.forEach(({ number, context, expected }) => {
             test(`should throw error when data is invalid ${JSON.stringify(
                 number
@@ -33,7 +58,19 @@ describe('test NumericKind helpers', () => {
         });
     });
 
+    /**
+     * Test subset for `assertValidNumericKindBuffer` function.
+     *
+     * This subset contains tests that validate the function’s ability to
+     * ascertain the validity of buffer inputs, based on specific numeric kind rules.
+     */
     describe('assertValidNumericKindBuffer', () => {
+        /**
+         * Tests for valid buffer input scenarios.
+         *
+         * This test iterates over valid buffer cases, asserting that
+         * the function does not throw an error for valid inputs.
+         */
         validNumericBufferTestCases.forEach(({ buffer, context, maxBytes }) => {
             test(`should not throw error when buffer is valid ${buffer.toString(
                 'hex'
@@ -44,6 +81,13 @@ describe('test NumericKind helpers', () => {
             });
         });
 
+        /**
+         * Tests for invalid buffer input scenarios.
+         *
+         * This test iterates over invalid buffer cases, asserting that
+         * the function throws an error for invalid inputs and the error message
+         * aligns with expectations.
+         */
         invalidNumericBufferTestCases.forEach(
             ({ buffer, context, maxBytes, expected }) => {
                 test(`should throw error when buffer is invalid ${buffer.toString(
