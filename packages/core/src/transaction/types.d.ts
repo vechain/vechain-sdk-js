ddbba7c68fe3661bd90c29a88692b875175bdda5
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-10-16 15:55:32 +0200

    feat: types for transaction

---
+++ packages/core/src/transaction/types.d.ts
@@ -0,0 +1,83 @@
+/**
+ * Simple type for transaction clause.
+ * @public
+ */
+interface Clause {
+    /**
+     * Destination address where:
+     * * transfer token to or
+     * * invoke contract method on.
+     *
+     * @note Set null destination to deploy a contract.
+     */
+    to: string | null;
+
+    /**
+     * Amount of token to transfer to the destination
+     */
+    value: string | number;
+
+    /**
+     * Input data for contract method invocation or deployment
+     */
+    data: string;
+}
+
+/**
+ * Type for transaction body.
+ * @public
+ */
+interface Body {
+    /**
+     * Last byte of genesis block ID
+     */
+    chainTag: number;
+
+    /**
+     * 8 bytes prefix of some block's ID
+     */
+    blockRef: string;
+
+    /**
+     * Constraint of time bucket
+     */
+    expiration: number;
+
+    /**
+     * Array of clauses
+     */
+    clauses: Clause[];
+
+    /**
+     * Coefficient applied to base gas price [0,255]
+     */
+    gasPriceCoef: number;
+
+    /**
+     * Max gas provided for execution
+     */
+    gas: string | number;
+
+    /**
+     * ID of another tx that is depended
+     */
+    dependsOn: string | null;
+
+    /**
+     * Nonce value for various purposes
+     */
+    nonce: string | number;
+
+    /**
+     * Reserved field for future usage.
+     * e.g. Fee delegation
+     */
+    reserved?: {
+        /**
+         * Tx feature bits
+         */
+        features?: number;
+        unused?: Buffer[];
+    };
+}
+export type { Clause };
