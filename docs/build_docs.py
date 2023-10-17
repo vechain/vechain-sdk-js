479baa38ba81d0fc9d742cf63d89b8e12e041f94
clayton neal <clayton.neal@vechain.org>
2023-10-17 17:14:16 +0100

    chore: wip

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -1,54 +1,48 @@
 import os
 from pathlib import Path
 
-CODE_PERMA_LINK ='[https://github.com/vechainfoundation/vechain-sdk/blob/'
-PERMA_LINK_END = ']'
-SNIPPET_HEADER = '```typescript { name=<name>, category=example }'
-SNIPPET_FOOTER = '```'
+FILE_TAG = 'file:'
+CODE_BLOCK_HEADER = '```typescript { name=<name>, category=example }'
+CODE_BLOCK = '```'
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+BUILD_DIR = os.path.join(CURRENT_DIR, "build")
 
-def build_snippet_from_file(file_path: str) -> str:
-    snippet_file = Path(file_path)
-    snippet_content = snippet_file.read_text()
-    snippet_name =  os.path.splittext(os.path.basename(snippet_file))[0]
-    snippet_header = SNIPPET_HEADER.replace('<name>', snippet_name)
-    snippet = f"{snippet_header}\n{snippet_content}\n{SNIPPET_FOOTER}\n"
-    return snippet
+def get_snippet(file_line: str) -> str:
+    """ Gets snippet text from referenced file """
+    print(f"\t\tGetting snippet from: {file_line}")
+    snippet_file_rel_path = file_line.replace(FILE_TAG, '').strip()
+    snippet_file_abs_path = os.path.join(CURRENT_DIR, Path(snippet_file_rel_path))
+    snippet_content = Path(snippet_file_abs_path).read_text()
+    return snippet_content
 
-def replace_perma_link(content: str, start_index: int) -> str:
-    print(f"\t\tReplacing link at position {start_index}")
-    finish_index = content.find(PERMA_LINK_END, start_index + 1)
-    temp_link = content[start_index:finish_index].replace(CODE_PERMA_LINK, '')
-    first_slash = temp_link.find('/')
-    snippet_file = temp_link[first_slash:]
-    snippet_content = build_snippet_from_file(snippet_file)
-    content_list = content.split()
-    content_list.insert(finish_index+1, snippet_content)
-    return " ".join(content_list)
-
-def build_md_file(mk_file_name: str):
-    print(f"\tBuilding file {mk_file_name}")
+def build_md_file(md_file_name: str):
+    """ Builds individual markdown file"""
+    print(f"\tBuilding file {md_file_name}")
     try:
-        md_file = Path(mk_file_name)
-        mk_content = md_file.read_text()
-        start_index = 0
-        while True:
-            link_index = mk_content.find(CODE_PERMA_LINK, start_index)
-            if link_index == -1:
-                break
+        input_file_path = Path(md_file_name)
+        output_file_path = Path(os.path.join(BUILD_DIR, os.path.basename(input_file_path)))
+        input_content = input_file_path.read_text().splitlines()
+        for line_index in range(0, len(input_content)):
+            if FILE_TAG in input_content[line_index]:
+                snippet = get_snippet(input_content[line_index])
+                with output_file_path.open("a") as f:
+                    f.write(f"{snippet}\n")
             else:
-                replace_perma_link(mk_content, link_index)
-                start_index = link_index + 1
+                with output_file_path.open("a") as f:
+                    f.write(f"{input_content[line_index]}\n") 
     except Exception as e:
-        print(f"\tUnable to build file {mk_file_name}")
+        print(f"\tUnable to build file {md_file_name}")
         raise e
 
 def get_markdown_files() -> []:
+    """ Gets list of markdown files to process"""
     files = [f.lower() for f in os.listdir() if os.path.isfile(f) and f.endswith('.md')]
     files.remove('readme.md')
     print(f"\t{len(files)} markdown files found")
     return files
 
 def main():
+    """ Entry point """
     print('Replacing code snippets in markdown files:')
     md_files = get_markdown_files()
     for mdfile in md_files:
