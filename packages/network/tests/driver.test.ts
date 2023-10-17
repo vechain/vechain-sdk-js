cb239db5a5f673dfbcbd792d4ffecf0024c181dc
fabiorigam <fabio.rigamonti@icloud.com>
2023-10-17 10:56:34 +0200

    fix: improve code quality

--- packages/network/tests/driver.test.ts
+++ packages/network/tests/driver.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test, jest } from '@jest/globals';
 import { SimpleNet } from '../src/driver/simple-net';
 import axios from 'axios';
+import { firstTestnetBlock, testnetUrl } from './utils/fixture';
 // import { SimpleWebSocketReader } from '../src/driver/simple-websocket-reader';
 
 describe('SimpleNet', () => {
@@ -12,34 +13,11 @@ describe('SimpleNet', () => {
             headers: {}
         });
 
-        const net = new SimpleNet('https://testnet.vechain.org/');
+        const net = new SimpleNet(testnetUrl);
         const response = await net.http('GET', '/blocks/1?expanded=false');
 
         expect(JSON.stringify(response)).toEqual(
-            JSON.stringify({
-                number: 1,
-                id: '0x000000019015bbd98fc1c9088d793ba9add53896a29cd9aa3a4dcabd1f561c38',
-                size: 236,
-                parentID:
-                    '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127',
-                timestamp: 1530014410,
-                gasLimit: 10000000,
-                beneficiary: '0xb4094c25f86d628fdd571afc4077f0d0196afb48',
-                gasUsed: 0,
-                totalScore: 1,
-                txsRoot:
-                    '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
-                txsFeatures: 0,
-                stateRoot:
-                    '0x4ec3af0acbad1ae467ad569337d2fe8576fe303928d35b8cdd91de47e9ac84bb',
-                receiptsRoot:
-                    '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
-                com: false,
-                signer: '0x25ae0ef84da4a76d5a1dfe80d3789c2c46fee30a',
-                isTrunk: true,
-                isFinalized: true,
-                transactions: []
-            })
+            JSON.stringify(firstTestnetBlock)
         );
 
         // Restore the original method after the test
@@ -51,7 +29,7 @@ describe('SimpleNet', () => {
         const axiosRequest = jest.spyOn(axios, 'request');
         axiosRequest.mockRejectedValue(new Error('Test Error'));
 
-        const net = new SimpleNet('https://testnet.vechain.org/');
+        const net = new SimpleNet(testnetUrl);
 
         await expect(net.http('GET', '/error-test-path')).rejects.toThrow(
             '404 get /error-test-path: 404 page not found'
---
+++ packages/network/tests/utils/fixture.ts
@@ -0,0 +1,26 @@
+export const testnetUrl = 'https://testnet.vechain.org/';
+
+export const firstTestnetBlock = {
+    number: 1,
+    id: '0x000000019015bbd98fc1c9088d793ba9add53896a29cd9aa3a4dcabd1f561c38',
+    size: 236,
+    parentID:
+        '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127',
+    timestamp: 1530014410,
+    gasLimit: 10000000,
+    beneficiary: '0xb4094c25f86d628fdd571afc4077f0d0196afb48',
+    gasUsed: 0,
+    totalScore: 1,
+    txsRoot:
+        '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
+    txsFeatures: 0,
+    stateRoot:
+        '0x4ec3af0acbad1ae467ad569337d2fe8576fe303928d35b8cdd91de47e9ac84bb',
+    receiptsRoot:
+        '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
+    com: false,
+    signer: '0x25ae0ef84da4a76d5a1dfe80d3789c2c46fee30a',
+    isTrunk: true,
+    isFinalized: true,
+    transactions: []
+};
