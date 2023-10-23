ccb0d655ddb7b980c9f613ade1d0ed863bf8b20c
Piero Bassa <pierobassa222@gmail.com>
2023-10-23 11:49:51 +0200

    feat: sha256 buffer or string return type with overloaded functions

--- packages/core/src/hash/sha256.ts
+++ packages/core/src/hash/sha256.ts
@@ -1,21 +1,65 @@
 import { ethers } from 'ethers';
-import { type HashInput } from './types';
+import { type ReturnType, type HashInput } from './types';
+import { isValidReturnType } from './helpers';
+import { ERRORS } from '../utils';
+
+/* --- Overloaded functions start --- */
 
 /**
- * Sha256 hash function implementation.
- * It uses last version of ethers.js library.
+ * Computes the sha256 hash of the given data.
+ * Returns the hash as a Buffer.
  *
- * @param data Data to hash
- * @returns Hash of data
+ * @param data - The input data (either a Buffer or string) for which the hash needs to be computed.
+ * @returns A Buffer containing the 256-bit sha256 hash of the provided data.
  */
-function sha256(data: HashInput): string {
-    // Data is not bytes-like
-    if (ethers.isBytesLike(data)) {
-        return ethers.sha256(data);
+function sha256(data: HashInput): Buffer;
+
+/**
+ * Computes the sha256 hash of the given data.
+ * Returns the hash as a Buffer.
+ *
+ * @param data - The input data (either a Buffer or string) for which the hash needs to be computed.
+ * @returns A Buffer containing the 256-bit sha256 hash of the provided data.
+ */
+function sha256(data: HashInput, returnType: 'buffer'): Buffer;
+
+/**
+ * Computes the sha256 hash of the given data.
+ * Returns the hash as a hex string, prefixed with `0x`.
+ *
+ * @param data - The input data (either a Buffer or string) for which the hash needs to be computed.
+ * @returns A string representing the hexadecimal format of the 256-bit sha256 hash, prefixed with `0x`.
+ */
+function sha256(data: HashInput, returnType: 'hex'): string;
+
+/* --- Overloaded functions end --- */
+
+/**
+ * Computes the sha256 hash of the given data.
+ * Returns the hash as a Buffer or hex string, depending on the returnType.
+ * If no returnType is provided, the hash is returned as a Buffer.
+ *
+ * @param data - The input data (either a Buffer or string) for which the hash needs to be computed.
+ * @param returnType - The type of the return value. Either 'buffer' or 'hex'. Defaults to 'buffer'.
+ * @returns A Buffer or string representing the 256-bit sha256 hash.
+ *
+ * @throws Will throw an error if an invalid returnType is provided.
+ */
+function sha256(
+    data: HashInput,
+    returnType: ReturnType = 'buffer'
+): Buffer | string {
+    if (!isValidReturnType(returnType)) {
+        throw new Error(
+            ERRORS.DATA.INVALID_RETURN_TYPE("either 'buffer' or 'hex'")
+        );
     }
 
-    // Data is string
-    return ethers.sha256(ethers.toUtf8Bytes(data));
+    const hash = ethers.isBytesLike(data)
+        ? ethers.sha256(data)
+        : ethers.sha256(ethers.toUtf8Bytes(data));
+
+    return returnType === 'buffer' ? Buffer.from(hash.slice(2), 'hex') : hash;
 }
 
 export { sha256 };
