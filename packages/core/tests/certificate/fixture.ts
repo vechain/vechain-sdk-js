d2f10deab14aee86d024df5d63e8f1efc10a8a90
Antonio Senatore <bacco1977@gmail.com>
2023-10-30 10:56:52 +0000

    fix: added comments to the fixture for certificate

--- packages/core/tests/certificate/fixture.ts
+++ packages/core/tests/certificate/fixture.ts
@@ -1,11 +1,17 @@
 import { secp256k1 } from '../../src/secp256k1';
 import { address } from '../../src/address';
 
+/**
+ * Private Key used for digital signature during certificate creation
+ */
 const privKey = Buffer.from(
     '7582be841ca040aa940fff6c05773129e135623e41acce3e0b8ba520dc1ae26a',
     'hex'
 );
 
+/**
+ * Certificate n.1 to be used for testing, mostly for encoding and verify functions
+ */
 const cert = {
     purpose: 'identification',
     payload: {
@@ -17,6 +23,9 @@ const cert = {
     signer: address.fromPublicKey(secp256k1.derivePublicKey(privKey))
 };
 
+/**
+ * Certificate n.2 to be used for testing, mostly for encoding and verify functions
+ */
 const cert2 = {
     domain: 'localhost',
     timestamp: 1545035330,
