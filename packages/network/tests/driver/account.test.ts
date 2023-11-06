7681f3e766241850b7ebacd1185d4f3fbdae9532
fabiorigam <fabio.rigamonti@icloud.com>
2023-11-06 12:23:28 +0100

    fix: improve test comments

--- packages/network/tests/driver/account.test.ts
+++ packages/network/tests/driver/account.test.ts
@@ -3,11 +3,12 @@ import { ThorClient } from '../../src/client/thor/thor-client';
 import { network } from './fixture';
 
 /**
- * SimpleNet class tests
+ * Integration tests for account-related functionality.
+ *
  * @group integration/account
  */
-describe('account', () => {
-    test('account 1', async () => {
+describe('Account', () => {
+    test('Should retrieve account information successfully', async () => {
         const client = await ThorClient.connect(network);
         const account = await client.getAccount(
             '0xc3bE339D3D20abc1B731B320959A96A08D479583'
