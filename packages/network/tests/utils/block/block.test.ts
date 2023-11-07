293d5baf346e8356a1c23d554d6abbb9f0444b9d
fabiorigam <fabio.rigamonti@icloud.com>
2023-11-07 16:13:41 +0100

    feat: add test

---
+++ packages/network/tests/utils/block/block.test.ts
@@ -0,0 +1,24 @@
+import { blockUtils } from '../../../src/utils';
+import { describe, expect, test } from '@jest/globals';
+import { testAccount } from '../../fixture';
+
+/**
+ * Unit tests for the blockUtils module.
+ *
+ * @group unit/utils
+ */
+describe('blockUtils', () => {
+    /**
+     * Test case for the `isBlockRevision` function.
+     */
+    test('isBlockRevision', () => {
+        // Test if an invalid address string is not a valid block revision
+        expect(blockUtils.isBlockRevision('invalid-address')).toBe(false);
+
+        // Test if a valid account address is a valid block revision
+        expect(blockUtils.isBlockRevision(testAccount)).toBe(true);
+
+        // Test if a valid decimal string is a valid block revision
+        expect(blockUtils.isBlockRevision('100')).toBe(true);
+    });
+});
