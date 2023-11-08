9164d1933feb5438227a89fd75a951fb34a05583
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-11-08 11:44:31 +0100

    feat: use fixture

--- packages/network/tests/utils/block/block.test.ts
+++ packages/network/tests/utils/block/block.test.ts
@@ -1,6 +1,6 @@
 import { blockUtils } from '../../../src/utils';
 import { describe, expect, test } from '@jest/globals';
-import { testAccount } from '../../fixture';
+import { blockRevisions } from './fixture';
 
 /**
  * Unit tests for the blockUtils module.
@@ -12,22 +12,8 @@ describe('blockUtils', () => {
      * Test case for the `isBlockRevision` function.
      */
     test('isBlockRevision', () => {
-        // Test if an invalid address string is not a valid block revision
-        expect(blockUtils.isBlockRevision('invalid-address')).toBe(false);
-
-        // Test if a valid account address is a valid block revision
-        expect(blockUtils.isBlockRevision(testAccount)).toBe(true);
-
-        // Test if a valid decimal string is a valid block revision
-        expect(blockUtils.isBlockRevision('100')).toBe(true);
-
-        // Test if an invalid hex string is a valid block revision
-        expect(blockUtils.isBlockRevision('0xG8656c6c6f')).toBe(false);
-
-        // Test if 'best' string is a valid block revision
-        expect(blockUtils.isBlockRevision('best')).toBe(true);
-
-        // Test if 'finalizes' string is a valid block revision
-        expect(blockUtils.isBlockRevision('finalized')).toBe(true);
+        blockRevisions.forEach(({ revision, expected }) => {
+            expect(blockUtils.isBlockRevision(revision)).toBe(expected);
+        });
     });
 });
