ef13ecf59811ba7e7cb6f914b17c41ba8979f38e
fabiorigam <fabio.rigamonti@icloud.com>
2023-11-08 16:24:56 +0100

    fix: add comments

--- packages/network/src/client/thor/block/block-client.ts
+++ packages/network/src/client/thor/block/block-client.ts
@@ -2,13 +2,23 @@ import { type HttpClient } from '../../http';
 import { thorest } from '../../../utils';
 import { type BlockDetail } from './types';
 
-class blockClient {
+/**
+ * The `BlockClient` class provides methods to interact with block-related endpoints
+ * of the VechainThor blockchain. It allows fetching details of a specific blockchain block.
+ */
+class BlockClient {
     /**
      * Initializes a new instance of the `BlockClient` class.
      * @param httpClient - The HTTP client instance used for making HTTP requests.
      */
     constructor(protected readonly httpClient: HttpClient) {}
 
+    /**
+     * Retrieves details of a specific block identified by its revision (block number or ID).
+     *
+     * @param revision - The block number or ID to query details for.
+     * @returns A promise that resolves to an object containing the block details.
+     */
     public async getBlock(revision: string | number): Promise<BlockDetail> {
         return (await this.httpClient.http(
             'GET',
@@ -17,4 +27,4 @@ class blockClient {
     }
 }
 
-export { blockClient };
+export { BlockClient };
--- packages/network/src/utils/thorest/block.ts
+++ packages/network/src/utils/thorest/block.ts
@@ -1,3 +1,9 @@
+/**
+ * Constructs the endpoint URL for retrieving block details based on block revision.
+ *
+ * @param revision - The block number or ID for which to fetch details.
+ * @returns The URL endpoint to fetch block details.
+ */
 const BLOCK_DETAIL = (revision: string | number): string =>
     `/blocks/${revision}`;
 
