81d80c6f03ec64820b093e2907889d79fcb60490
Valazan <al-fede@hotmail.it>
2023-11-09 17:01:56 +0100

    docs: build contract transaction tests

--- packages/core/tests/contract/contract.test.ts
+++ packages/core/tests/contract/contract.test.ts
@@ -8,30 +8,44 @@ import {
 import { contract, networkInfo } from '../../src/core';
 
 /**
- * Unit tests for Contract-related functionality.
+ * Unit tests for building contract transactions.
  * @group unit/contract
  */
 describe('Contract', () => {
+    /**
+     * Test to ensure building a transaction to deploy a contract.
+     */
     test('Build a transaction to deploy a contract', () => {
-        const compiledContract = compileContract(
-            'tests/contract/sample',
-            'Example.sol',
-            'Example'
-        );
-        const transaction = buildDeployContractTransaction(
-            compiledContract.bytecode
-        );
-        expect(transaction.body.clauses[0].data).toEqual(
-            compiledContract.bytecode
-        );
-    });
-    test('Build a transaction to deploy a contract with a custom transaction body', () => {
+        // Compile the contract from a sample file
         const compiledContract = compileContract(
             'tests/contract/sample',
             'Example.sol',
             'Example'
         );
 
+        // Build a transaction to deploy the compiled contract
+        const transaction = buildDeployContractTransaction(
+            compiledContract.bytecode
+        );
+
+        // Ensure the transaction body contains the compiled bytecode
+        expect(transaction.body.clauses[0].data).toEqual(
+            compiledContract.bytecode
+        );
+    });
+
+    /**
+     * Test to ensure building a transaction to deploy a contract with a custom transaction body.
+     */
+    test('Build a transaction to deploy a contract with a custom transaction body', () => {
+        // Compile the contract from a sample file
+        const compiledContract = compileContract(
+            'tests/contract/sample',
+            'Example.sol',
+            'Example'
+        );
+
+        // Create a custom transaction body
         const transactionBody: TransactionBodyOverride = {
             nonce: 1,
             chainTag: networkInfo.mainnet.chainTag,
@@ -41,10 +55,13 @@ describe('Contract', () => {
             dependsOn: null
         };
 
+        // Build a transaction to deploy the compiled contract with the custom transaction body
         const transaction = buildDeployContractTransaction(
             compiledContract.bytecode,
             transactionBody
         );
+
+        // Ensure the transaction body and properties match the custom values
         expect(transaction.body.clauses[0].data).toEqual(
             compiledContract.bytecode
         );
@@ -54,28 +71,42 @@ describe('Contract', () => {
         expect(transaction.body.gasPriceCoef).toEqual(0);
         expect(transaction.body.dependsOn).toEqual(null);
     });
+
+    /**
+     * Test to ensure building a call contract transaction.
+     */
     test('Build a call contract transaction', () => {
+        // Compile the contract from a sample file
         const contractCompiled = compileContract(
             'tests/contract/sample',
             'Example.sol',
             'Example'
         );
 
+        // Build a transaction to call a function on the contract
         const callFunctionTransaction = buildCallContractTransaction(
             '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
             contractCompiled.abi,
             'set',
             [1]
         );
+
+        // Ensure the transaction body contains data for the function call
         expect(callFunctionTransaction.body.clauses[0].data).toBeDefined();
     });
+
+    /**
+     * Test to ensure compiling a sample contract and creating an interface from the ABI.
+     */
     test('Compile a sample contract and create an interface from the abi', () => {
+        // Compile the contract from a sample file
         const contractCompiled = compileContract(
             'tests/contract/sample',
             'Example.sol',
             'Example'
         );
 
+        // Ensure the contract compilation is successful
         expect(contractCompiled).toBeDefined();
 
         // Create an instance of a Contract interface using the ABI
@@ -83,6 +114,7 @@ describe('Contract', () => {
             contractCompiled.abi
         );
 
+        // Ensure the contract interface is created successfully
         expect(contractInterface).toBeDefined();
     });
 });
