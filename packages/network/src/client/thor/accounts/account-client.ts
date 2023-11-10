6001860d388b0b46cd943dbff9fd7f1fc1f610a3
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-11-10 10:36:05 +0100

    fix: move simulateTransactionCall to transaction-client.ts

--- packages/network/src/client/thor/accounts/account-client.ts
+++ packages/network/src/client/thor/accounts/account-client.ts
@@ -141,31 +141,6 @@ class AccountClient {
 
         return result.value;
     }
-
-    public async simulateTransactionCall(revision: string): Promise<unknown> {
-        return await this.httpClient.http(
-            'POST',
-            thorest.accounts.post.ACCOUNT(revision),
-            {
-                body: {
-                    clauses: [
-                        {
-                            to: '0x5034aa590125b64023a0262112b98d72e3c8e40e',
-                            value: '0xde0b6b3a7640000',
-                            data: '0x5665436861696e2054686f72'
-                        }
-                    ],
-                    gas: 50000,
-                    gasPrice: '1000000000000000',
-                    caller: '0x7567d83b7b8d80addcb281a71d54fc7b3364ffed',
-                    provedWork: '1000',
-                    gasPayer: '0xd3ae78222beadb038203be21ed5ce7c9b1bff602',
-                    expiration: 1000,
-                    blockRef: '0x00000000851caf3c'
-                }
-            }
-        );
-    }
 }
 
 export { AccountClient };
