73c52f64d879bd3f6b379e76896ae278d54204fb
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-11-13 09:08:39 +0100

    feat: tests. Missing simulate transaction call

---
+++ packages/network/tests/client/thor-client/transactions/fixture.ts
@@ -0,0 +1,173 @@
+import { InvalidDataTypeError } from '@vechain-sdk/errors';
+
+/**
+ * Transaction details function fixture.
+ */
+const transactionDetails = {
+    correct: [
+        {
+            testName:
+                'Should be able to retrieve a transaction - NO RAW FORMAT',
+            transaction: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                raw: false,
+                pending: false
+            },
+            expected: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                chainTag: 39,
+                blockRef: '0x010284a0b704e751',
+                expiration: 2000,
+                clauses: [
+                    {
+                        to: '0x5d57f07dfeb8c224121433d5b1b401c82bd88f3d',
+                        value: '0x2ea11e32ad50000',
+                        data: '0x'
+                    }
+                ],
+                gasPriceCoef: 0,
+                gas: 41192,
+                origin: '0x2d4ed6b8abd00bc2ef0bdb2258a946c214d9d0af',
+                delegator: null,
+                nonce: '0x76eed751cef0e52d',
+                dependsOn: null,
+                size: 130,
+                meta: {
+                    blockID:
+                        '0x010284a1fea0635a2e47dd21f8a1761406df1013e5f4af79e311d8a27373980d',
+                    blockNumber: 16942241,
+                    blockTimestamp: 1699453780
+                }
+            }
+        },
+        {
+            testName: 'Should be able to retrieve a transaction - RAW FORMAT',
+            transaction: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                raw: true,
+                pending: true
+            },
+            expected: {
+                raw: '0xf8802788010284a0b704e7518207d0e0df945d57f07dfeb8c224121433d5b1b401c82bd88f3d8802ea11e32ad50000808082a0e8808876eed751cef0e52dc0b841b2bb1ba31c7b78383bf7e01097038d26e6f1c685bdc73fce981f574eb7bb3abd6e59660e522c4870f1d033c0f82420448f44139e0fb2254a36d98ed387964c3c00',
+                meta: {
+                    blockID:
+                        '0x010284a1fea0635a2e47dd21f8a1761406df1013e5f4af79e311d8a27373980d',
+                    blockNumber: 16942241,
+                    blockTimestamp: 1699453780
+                }
+            }
+        }
+    ],
+    errors: [
+        {
+            testName: 'Should throw error when invalid transaction id is given',
+            transaction: {
+                id: 'WRONG_ID'
+            },
+            expected: InvalidDataTypeError
+        },
+        {
+            testName: 'Should throw error when invalid head of block is given',
+            transaction: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                raw: false,
+                pending: false,
+                head: 'WRONG_HEAD'
+            },
+            expected: InvalidDataTypeError
+        }
+    ]
+};
+
+/**
+ * Transaction receipts function fixture.
+ */
+const transactionReceipts = {
+    correct: [
+        {
+            testName: 'Should be able to retrieve a transaction receipt',
+            transaction: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb'
+            },
+            expected: {
+                gasUsed: 21000,
+                gasPayer: '0x2d4ed6b8abd00bc2ef0bdb2258a946c214d9d0af',
+                paid: '0x2ea11e32ad50000',
+                reward: '0xdfd22a8cd98000',
+                reverted: false,
+                meta: {
+                    blockID:
+                        '0x010284a1fea0635a2e47dd21f8a1761406df1013e5f4af79e311d8a27373980d',
+                    blockNumber: 16942241,
+                    blockTimestamp: 1699453780,
+                    txID: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                    txOrigin: '0x2d4ed6b8abd00bc2ef0bdb2258a946c214d9d0af'
+                },
+                outputs: [
+                    {
+                        contractAddress: null,
+                        events: [],
+                        transfers: [
+                            {
+                                sender: '0x2d4ed6b8abd00bc2ef0bdb2258a946c214d9d0af',
+                                recipient:
+                                    '0x5d57f07dfeb8c224121433d5b1b401c82bd88f3d',
+                                amount: '0x2ea11e32ad50000'
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    ],
+    errors: [
+        {
+            testName: 'Should throw error when invalid transaction id is given',
+            transaction: {
+                id: 'WRONG_ID'
+            },
+            expected: InvalidDataTypeError
+        },
+        {
+            testName: 'Should throw error when invalid head of block is given',
+            transaction: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                head: 'WRONG_HEAD'
+            },
+            expected: InvalidDataTypeError
+        },
+        {
+            testName:
+                'Should throw error when invalid head of block hex string length is given',
+            transaction: {
+                id: '0x46d195f69e1ac3922d42c207e4705a3d1642883d97e58f7efc72f179ea326adb',
+                head: '0x1234'
+            },
+            expected: InvalidDataTypeError
+        }
+    ]
+};
+
+/**
+ * Send transaction function errors fixture.
+ */
+const sendTransactionErrors = [
+    {
+        testName:
+            'Should throw error when invalid encoded raw transaction hex string is given',
+        transaction: {
+            raw: 'INVALID_HEX_STRING'
+        },
+        expected: InvalidDataTypeError
+    },
+    {
+        testName:
+            'Should throw error when invalid encoded raw transaction is given',
+        transaction: {
+            raw: '0x123456789abcdef'
+        },
+        expected: InvalidDataTypeError
+    }
+];
+
+export { transactionDetails, transactionReceipts, sendTransactionErrors };
---
+++ packages/network/tests/client/thor-client/transactions/transaction.solo.test.ts
@@ -0,0 +1,82 @@
+import { describe, expect, test } from '@jest/globals';
+import { TEST_ACCOUNTS, thorSoloClient } from '../../../fixture';
+import {
+    Transaction,
+    TransactionHandler,
+    TransactionUtils
+} from '@vechain-sdk/core';
+import { sendTransactionErrors } from './fixture';
+
+/**
+ * ThorClient class tests.
+ *
+ * @NOTE: This test suite run on solo network because it requires to send transactions.
+ *
+ * @group integration/client/thor/transactions
+ */
+describe('ThorClient - Transactions', () => {
+    /**
+     * sendTransaction tests
+     */
+    describe('sendTransaction', () => {
+        /**
+         * SendTransaction - correct cases
+         */
+        test('Should be able to send a transaction', async () => {
+            // 1- Init transaction
+            // Clauses
+            const clauses = [
+                {
+                    to: TEST_ACCOUNTS.TRANSACTION.TRANSACTION_RECEIVER.address,
+                    value: 1000000,
+                    data: '0x'
+                }
+            ];
+
+            // Get latest block
+            const latestBlock = await thorSoloClient.blocks.getBlock('best');
+
+            // Get gas @NOTE it is approximation. This part must be improved.
+            const gas = 5000 + TransactionUtils.intrinsicGas(clauses) * 5;
+
+            // Create transaction
+            const transaction = new Transaction({
+                chainTag: 0xf6,
+                blockRef: latestBlock.id.slice(0, 18),
+                expiration: 32,
+                clauses,
+                gasPriceCoef: 128,
+                gas,
+                dependsOn: null,
+                nonce: 12345678
+            });
+
+            const raw = TransactionHandler.sign(
+                transaction,
+                Buffer.from(
+                    TEST_ACCOUNTS.TRANSACTION.TRANSACTION_SENDER.privateKey,
+                    'hex'
+                )
+            ).encoded;
+
+            // 2 - Send transaction
+            const send = await thorSoloClient.transactions.sendTransaction(
+                `0x${raw.toString('hex')}`
+            );
+            expect(send).toBeDefined();
+        });
+
+        /**
+         * SendTransaction - error cases
+         */
+        sendTransactionErrors.forEach((testCase) => {
+            test(testCase.testName, async () => {
+                await expect(
+                    thorSoloClient.transactions.sendTransaction(
+                        testCase.transaction.raw
+                    )
+                ).rejects.toThrow(testCase.expected);
+            });
+        });
+    });
+});
