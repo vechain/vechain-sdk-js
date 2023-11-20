a05d56136fe642b8a32395d41ef498a2174b6210
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2023-11-20 11:51:03 +0000

    feat: encode & decode bytes32 strings (#267)
    * chore: removed windowZoom constaint vscode settings
    
    * refactor: removed HexString type
    
    * refactor: removed HexString usage
    
    * feat: encodeBytes32String & decodeBytes32String
    
    * test: encodeBytes32String & decodeBytes32String
    
    * docs: added when decode bytes 32 throws
    
    * docs: encode & decode bytes 32 test cases
    
    * docs: encode & decode bytes 32 fixtures
    
    * refactor: suggestions
    
    * refactor: removed redundant value in error
    
    * docs: refactor zeropadding

--- packages/core/tests/utils/data/data.test.ts
+++ packages/core/tests/utils/data/data.test.ts
@@ -1,6 +1,10 @@
 import { describe, expect, test } from '@jest/globals';
 import { dataUtils } from '../../../src';
 import {
+    decodeBytes32StringTestCases,
+    encodeBytes32StringTestCases,
+    invalidDecodeBytes32StringTestCases,
+    invalidEncodeBytes32StringTestCases,
     invalidHexStrings,
     invalidThorIDs,
     isNumericTestCases,
@@ -113,5 +117,67 @@ describe('utils/hex', () => {
                 expect(dataUtils.isNumeric(value)).toBe(expected);
             });
         });
+    });
+
+    /**
+     * Encode bytes32 string
+     */
+    describe('encodeBytes32String', () => {
+        /**
+         * Test cases for encodeBytes32String function.
+         */
+        encodeBytes32StringTestCases.forEach(
+            ({ value, zeroPadding, expected }) => {
+                test(`should return ${expected} for ${JSON.stringify(
+                    value
+                )}`, () => {
+                    expect(
+                        dataUtils.encodeBytes32String(value, zeroPadding)
+                    ).toBe(expected);
+                });
+            }
+        );
+
+        /**
+         * Test cases for invalid encodeBytes32String function.
+         */
+        invalidEncodeBytes32StringTestCases.forEach(
+            ({ value, zeroPadding, expectedError }) => {
+                test(`should throw for ${JSON.stringify(value)}`, () => {
+                    expect(() =>
+                        dataUtils.encodeBytes32String(value, zeroPadding)
+                    ).toThrowError(expectedError);
+                });
+            }
+        );
+    });
+
+    /**
+     * Decode bytes32 string
+     */
+    describe('decodeBytes32String', () => {
+        /**
+         * Test cases for decodeBytes32String function.
+         */
+        decodeBytes32StringTestCases.forEach(({ value, expected }) => {
+            test(`should return ${expected} for ${JSON.stringify(
+                value
+            )}`, () => {
+                expect(dataUtils.decodeBytes32String(value)).toBe(expected);
+            });
+        });
+
+        /**
+         * Test cases for invalid decodeBytes32String function.
+         */
+        invalidDecodeBytes32StringTestCases.forEach(
+            ({ value, expectedError }) => {
+                test(`should throw for ${JSON.stringify(value)}`, () => {
+                    expect(() =>
+                        dataUtils.decodeBytes32String(value)
+                    ).toThrowError(expectedError);
+                });
+            }
+        );
     });
 });
--- packages/core/tests/utils/data/fixture.ts
+++ packages/core/tests/utils/data/fixture.ts
@@ -1,3 +1,5 @@
+import { InvalidDataTypeError } from '@vechainfoundation/vechain-sdk-errors';
+
 /**
  * Valid hex strings.
  */
@@ -163,11 +165,123 @@ const isNumericTestCases = [
     }
 ];
 
+/**
+ * Test cases for encodeBytes32String function.
+ */
+const encodeBytes32StringTestCases: Array<{
+    value: string;
+    zeroPadding?: 'left' | 'right';
+    expected: string;
+}> = [
+    {
+        value: 'Hello',
+        zeroPadding: 'left',
+        expected:
+            '0x00000000000000000000000000000000000000000000000000000048656c6c6f'
+    },
+    {
+        value: 'Hello',
+        zeroPadding: 'right',
+        expected:
+            '0x48656c6c6f000000000000000000000000000000000000000000000000000000'
+    },
+    {
+        value: "Hello World! I'm  with  32 bytes",
+        zeroPadding: 'left',
+        expected:
+            '0x48656c6c6f20576f726c64212049276d20207769746820203332206279746573'
+    },
+    {
+        value: "Hello World! I'm  with  32 bytes",
+        zeroPadding: 'right',
+        expected:
+            '0x48656c6c6f20576f726c64212049276d20207769746820203332206279746573'
+    },
+    {
+        value: 'base-gas-price',
+        zeroPadding: 'left',
+        expected:
+            '0x000000000000000000000000000000000000626173652d6761732d7072696365'
+    },
+
+    {
+        value: 'base-gas-price',
+        zeroPadding: undefined,
+        expected:
+            '0x000000000000000000000000000000000000626173652d6761732d7072696365'
+    },
+    {
+        value: 'base-gas-price',
+        zeroPadding: 'right',
+        expected:
+            '0x626173652d6761732d7072696365000000000000000000000000000000000000'
+    }
+];
+
+/**
+ * Test cases for invalid encodeBytes32String function.
+ */
+const invalidEncodeBytes32StringTestCases: Array<{
+    value: string;
+    zeroPadding?: 'left' | 'right';
+    expectedError: typeof InvalidDataTypeError;
+}> = [
+    {
+        value: 'Too-many-characters-Too-many-characters-Too-many-characters-Too-many-characters-Too-many-characters-Too-many-characters-Too-many-characters',
+        zeroPadding: 'left',
+        expectedError: InvalidDataTypeError // value exceeds 32 bytes
+    }
+];
+
+/**
+ * Test cases for decodeBytes32String function.
+ */
+const decodeBytes32StringTestCases = [
+    {
+        value: '0x00000000000000000000000000000000000000000000000000000048656c6c6f',
+        expected: 'Hello'
+    },
+    {
+        value: '0x48656c6c6f000000000000000000000000000000000000000000000000000000',
+        expected: 'Hello'
+    },
+    {
+        value: '0x48656c6c6f20576f726c64212049276d20207769746820203332206279746573',
+        expected: "Hello World! I'm  with  32 bytes"
+    },
+    {
+        value: '0x000000000000000000000000000000000000626173652d6761732d7072696365',
+        expected: 'base-gas-price'
+    }
+];
+
+/**
+ * Test cases for invalid decodeBytes32String function.
+ */
+const invalidDecodeBytes32StringTestCases = [
+    {
+        value: 'non-hex-string',
+        expectedError: InvalidDataTypeError
+    },
+    {
+        value: '0x432345123',
+        expectedError: InvalidDataTypeError
+    },
+    {
+        value: '0x48656c6c6fz0576f726c64212049276d20207769746820203332206279746573', // Invalid hex contains 'z'
+        expectedError: InvalidDataTypeError
+    }
+];
+
 export {
     validHexStrings,
     invalidHexStrings,
     validThorIDs,
     invalidThorIDs,
     prefixedAndUnprefixedStrings,
-    isNumericTestCases
+    isNumericTestCases,
+    encodeBytes32StringTestCases,
+    invalidEncodeBytes32StringTestCases,
+    decodeBytes32StringTestCases,
+    invalidDecodeBytes32StringTestCases
 };
