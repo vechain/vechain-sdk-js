57fb2a36d1c7b314d6d772f5c5406c2f27172e40
Piero Bassa <pierobassa222@gmail.com>
2023-11-28 15:40:12 +0100

    refactor: thor client's node module

--- packages/network/src/clients/thor-client/nodes/nodes-module.ts
+++ packages/network/src/clients/thor-client/nodes/nodes-module.ts
@@ -6,9 +6,9 @@ import { assert, DATA } from '@vechainfoundation/vechain-sdk-errors';
 import { type BlockDetail, BlocksClient } from '../../thorest-client';
 
 /**
- * Provides utility method for checking the health of a node.
+ * The `NodesModule` class serves as a module for node-related functionality, for example, checking the health of a node.
  */
-class NodesClient {
+class NodesModule {
     /**
      * Internal blocks client instance used for interacting with block-related endpoints.
      */
@@ -92,4 +92,4 @@ class NodesClient {
     };
 }
 
-export { NodesClient };
+export { NodesModule };
