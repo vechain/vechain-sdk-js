d02db79a3bbe01c1bbc71f5aff1308236af53ec2
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-11-28 17:21:50 +0100

    feat: common assertion for transaction thorest client

---
+++ packages/network/src/clients/thorest-client/transactions/helpers/assertions.ts
@@ -0,0 +1,31 @@
+import { assert, DATA } from '@vechainfoundation/vechain-sdk-errors';
+import { dataUtils } from '@vechainfoundation/vechain-sdk-core';
+
+/**
+ * Assert if transaction ID is valid
+ *
+ * @param transactionId - Transaction ID to assert
+ */
+function assertInvalidTransactionId(transactionId: string): void {
+    assert(
+        dataUtils.isThorId(transactionId, true),
+        DATA.INVALID_DATA_TYPE,
+        'Invalid transaction ID given as input. Input must be an hex string of length 64.',
+        { transactionId }
+    );
+}
+
+/**
+ * Assert if transaction head is valid
+ * @param head - Transaction head to assert
+ */
+function assertInvalidTransactionHead(head?: string): void {
+    assert(
+        head === undefined || dataUtils.isThorId(head, true),
+        DATA.INVALID_DATA_TYPE,
+        'Invalid head given as input. Input must be an hex string of length 64.',
+        { head }
+    );
+}
+
+export { assertInvalidTransactionId, assertInvalidTransactionHead };
