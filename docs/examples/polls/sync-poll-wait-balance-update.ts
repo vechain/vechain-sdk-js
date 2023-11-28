bbc02861748047760ab2b72571c9b65fcf6cfc46
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-11-28 09:44:46 +0100

    feat: sync poll example

--- docs/examples/polls/sync-poll-wait-balance-update.ts
+++ docs/examples/polls/sync-poll-wait-balance-update.ts
@@ -1,106 +1,121 @@
-// WAIT FOR SOLO NODE IMPLEMENTATION
-// import { describe, expect, test } from '@jest/globals';
-// import { TEST_ACCOUNTS, thorestSoloClient } from '../../../fixture';
-// import { Poll } from '../../../../src/utils/poll';
-// import {
-//     dataUtils,
-//     Transaction,
-//     TransactionHandler,
-//     TransactionUtils
-// } from '@vechainfoundation/vechain-sdk-core';
-// import { sendTransactionErrors } from '../../../clients/thorest-client/transactions/fixture';
-//
-// /**
-//  * Test the Synchronous poll on Solo node with real world example
-//  * @group integration/utils/sync-poll-example-solo
-//  */
-// describe('Synchronous poll tests - Solo', () => {
-//     /**
-//      * Test the updated balance event
-//      */
-//     test('Wait until a balance is updated after a transaction', async () => {
-//         // 1- Init transaction
-//
-//         // Get latest block
-//         const latestBlock = await thorestSoloClient.blocks.getBestBlock();
-//
-//         // Get gas @NOTE it is approximation. This part must be improved.
-//         const gas =
-//             5000 +
-//             TransactionUtils.intrinsicGas(
-//                 sendTransactionErrors.correct[0].transaction.clauses
-//             ) *
-//             5;
-//
-//         // Create transactions
-//         const transaction = new Transaction({
-//             chainTag: 0xf6,
-//             blockRef:
-//                 latestBlock !== null ? latestBlock.id.slice(0, 18) : '0x0',
-//             expiration: 32,
-//             clauses: sendTransactionErrors.correct[0].transaction.clauses,
-//             gasPriceCoef: 128,
-//             gas,
-//             dependsOn: null,
-//             nonce: 12345678
-//         });
-//
-//         // Normal signature and delegation signature
-//         const rawNormalSigned = TransactionHandler.sign(
-//             transaction,
-//             Buffer.from(
-//                 TEST_ACCOUNTS.TRANSACTION.TRANSACTION_SENDER.privateKey,
-//                 'hex'
-//             )
-//         ).encoded;
-//
-//         // 2 - Get the sender and receiver balance before the transaction
-//         const senderBalanceBefore = (
-//             await thorestSoloClient.accounts.getAccount(
-//                 TEST_ACCOUNTS.TRANSACTION.TRANSACTION_SENDER.address
-//             )
-//         ).balance;
-//         const receiverBalanceBefore = (
-//             await thorestSoloClient.accounts.getAccount(
-//                 TEST_ACCOUNTS.TRANSACTION.TRANSACTION_RECEIVER.address
-//             )
-//         ).balance;
-//
-//         // 3 - Send transaction
-//
-//         const send = await thorestSoloClient.transactions.sendTransaction(
-//             `0x${rawNormalSigned.toString('hex')}`
-//         );
-//         expect(send).toBeDefined();
-//         expect(send).toHaveProperty('id');
-//         expect(dataUtils.isHexString(send.id)).toBe(true);
-//
-//         // 4 -Wait until balance is updated
-//         const newBalanceSender = await Poll.SyncPoll(
-//             async () =>
-//                 (
-//                     await thorestSoloClient.accounts.getAccount(
-//                         TEST_ACCOUNTS.TRANSACTION.TRANSACTION_SENDER.address
-//                     )
-//                 ).balance
-//         ).waitUntil((newBalance) => {
-//             return newBalance !== senderBalanceBefore;
-//         });
-//
-//         const newBalanceReceiver = await Poll.SyncPoll(
-//             async () =>
-//                 (
-//                     await thorestSoloClient.accounts.getAccount(
-//                         TEST_ACCOUNTS.TRANSACTION.TRANSACTION_SENDER.address
-//                     )
-//                 ).balance
-//         ).waitUntil((newBalance) => {
-//             return newBalance !== receiverBalanceBefore;
-//         });
-//
-//         expect(newBalanceSender).toBeDefined();
-//         expect(newBalanceReceiver).toBeDefined();
-//         expect(newBalanceSender).not.toBe(senderBalanceBefore);
-//         expect(newBalanceReceiver).not.toBe(receiverBalanceBefore);
-//     }, 30000);
-// });
+import {
+    HttpClient,
+    Poll,
+    ThorestClient
+} from '@vechainfoundation/vechain-sdk-network';
+import {
+    dataUtils,
+    Transaction,
+    TransactionHandler,
+    TransactionUtils
+} from '@vechainfoundation/vechain-sdk-core';
+import { expect } from 'expect';
+
+// 1 - Create client for solo network
+
+const _soloUrl = 'http://localhost:8669';
+const soloNetwork = new HttpClient(_soloUrl);
+const thorestSoloClient = new ThorestClient(soloNetwork);
+
+// 2- Init transaction
+
+// 2.1 - Get latest block
+const latestBlock = await thorestSoloClient.blocks.getFinalBlock();
+
+// 2.2 - Transaction sender and receiver
+const sender = {
+    address: '0x2669514f9fe96bc7301177ba774d3da8a06cace4',
+    privateKey: Buffer.from(
+        'ea5383ac1f9e625220039a4afac6a7f868bf1ad4f48ce3a1dd78bd214ee4ace5',
+        'hex'
+    )
+};
+
+const receiver = {
+    address: '0x9e7911de289c3c856ce7f421034f66b6cde49c39',
+    privateKey: Buffer.from(
+        '1758771c54938e977518e4ff1c297aca882f6598891df503030734532efa790e',
+        'hex'
+    )
+};
+
+// 2.2 - Create transaction clauses
+const clauses = [
+    {
+        to: receiver.address,
+        value: 1000000,
+        data: '0x'
+    }
+];
+
+// 2.3 - Calculate gas
+// @NOTE: To improve the performance, we use a fixed gas price here.
+const gas = 5000 + TransactionUtils.intrinsicGas(clauses) * 5;
+
+// 2.4 - Create transactions
+const transaction = new Transaction({
+    // Solo network chain tag
+    chainTag: 0xf6,
+    // Solo network block ref
+    blockRef: latestBlock !== null ? latestBlock.id.slice(0, 18) : '0x0',
+    expiration: 32,
+    clauses,
+    gasPriceCoef: 128,
+    gas,
+    dependsOn: null,
+    nonce: 12345678
+});
+
+// 2.5 - Sign and get raw transaction
+const encoded = TransactionHandler.sign(transaction, sender.privateKey).encoded;
+const raw = `0x${encoded.toString('hex')}`;
+
+// 3 - Get the sender and receiver balance before the transaction
+
+const senderBalanceBefore = (
+    await thorestSoloClient.accounts.getAccount(sender.address)
+).balance;
+
+const receiverBalanceBefore = (
+    await thorestSoloClient.accounts.getAccount(receiver.address)
+).balance;
+
+console.log('Sender balance before:', senderBalanceBefore);
+console.log('Receiver balance before:', receiverBalanceBefore);
+
+// 4 - Send transaction
+
+const sentedTransaction =
+    await thorestSoloClient.transactions.sendTransaction(raw);
+
+// 4.1 - Check if the transaction is sent successfully (check if the transaction id is a valid hex string)
+expect(sentedTransaction).toBeDefined();
+expect(sentedTransaction).toHaveProperty('id');
+expect(dataUtils.isHexString(sentedTransaction.id)).toBe(true);
+
+console.log('Transaction sent:', sentedTransaction);
+
+// 4 -Wait until balance is updated
+
+const newBalanceSender = await Poll.SyncPoll(
+    async () =>
+        (await thorestSoloClient.accounts.getAccount(sender.address)).balance
+).waitUntil((newBalance) => {
+    return newBalance !== senderBalanceBefore;
+});
+
+const newBalanceReceiver = await Poll.SyncPoll(
+    async () =>
+        (await thorestSoloClient.accounts.getAccount(receiver.address)).balance
+).waitUntil((newBalance) => {
+    return newBalance !== receiverBalanceBefore;
+});
+
+expect(newBalanceSender).toBeDefined();
+expect(newBalanceReceiver).toBeDefined();
+
+expect(newBalanceSender).not.toBe(senderBalanceBefore);
+expect(newBalanceReceiver).not.toBe(receiverBalanceBefore);
+
+console.log('New balance of sender:', newBalanceSender);
+console.log('New balance of receiver:', newBalanceReceiver);
--- docs/examples/polls/sync-poll-wait-new-block.ts
+++ docs/examples/polls/sync-poll-wait-new-block.ts
@@ -5,23 +5,28 @@ import {
 } from '@vechainfoundation/vechain-sdk-network';
 import { expect } from 'expect';
 
-// Create client for testnet
+// 1 - Create client for testnet
 const _testnetUrl = 'https://testnet.vechain.org';
 const testNetwork = new HttpClient(_testnetUrl);
 const thorestClient = new ThorestClient(testNetwork);
 
-// Current block
+// 2 - Get current block
 const currentBlock = await thorestClient.blocks.getBestBlock();
-console.log(currentBlock);
 
-// Wait until a new block is created
+console.log('Current block:', currentBlock);
+
+// 3 - Wait until a new block is created
 const newBlock = await Poll.SyncPoll(
+    // Get the latest block as polling target function
     async () => await thorestClient.blocks.getBlock('best'),
+    // Polling interval is 3 seconds
     { requestIntervalInMilliseconds: 3000 }
 ).waitUntil((newBlockData) => {
+    // Stop polling when the new block number is greater than the current block number
     return (newBlockData?.number as number) > (currentBlock?.number as number);
 });
 
 expect(newBlock).toBeDefined();
 expect(newBlock?.number).toBeGreaterThan(currentBlock?.number as number);
-console.log(newBlock);
+
+console.log('New block:', newBlock);
