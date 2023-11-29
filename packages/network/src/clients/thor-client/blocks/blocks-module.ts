0f40706c0e093547538c390ce31747deb47f36b9
fabiorigam <fabio.rigamonti@icloud.com>
2023-11-29 12:12:49 +0100

    fix: add the maximum timeout option

--- packages/network/src/clients/thor-client/blocks/blocks-module.ts
+++ packages/network/src/clients/thor-client/blocks/blocks-module.ts
@@ -27,26 +27,32 @@ class BlocksModule {
      * @returns A promise that resolves to an object containing the block details.
      */
     public async waitForBlock(
-        blockNumber: number
+        blockNumber: number,
+        maximumWaitingTimeInMilliseconds?: number
     ): Promise<BlockDetail | null> {
-        const bestBlock = await this.blocksClient.getBestBlock();
-        const bestBlockNumber = bestBlock != null ? bestBlock.number : 0;
         assert(
             blockNumber === undefined ||
                 blockNumber === null ||
                 typeof blockNumber !== 'number' ||
-                blockNumber > bestBlockNumber,
+                blockNumber > 0,
             DATA.INVALID_DATA_TYPE,
             'Invalid blockNumber. The blockNumber must be a number representing a block number in the future.',
             { blockNumber }
         );
+
+        const bestBlock = await this.blocksClient.getBestBlock();
+        if (bestBlock != null && blockNumber <= bestBlock?.number) {
+            return await this.blocksClient.getBlock(blockNumber);
+        }
 
         // Use the Poll.SyncPoll utility to repeatedly call getBestBlock with a specified interval
         const block = await Poll.SyncPoll(
             async () => await this.blocksClient.getBestBlock(),
             {
                 // Set the interval for making requests in milliseconds
-                requestIntervalInMilliseconds: 1000
+                requestIntervalInMilliseconds: 1000,
+                // Set the maximum timeout for polling in milliseconds
+                maximumWaitingTimeInMilliseconds
             }
         ).waitUntil((result) => {
             // Continue polling until the result's block number matches the specified revision
--- packages/network/tests/clients/thor-client/blocks/blocks.testnet.test.ts
+++ packages/network/tests/clients/thor-client/blocks/blocks.testnet.test.ts
@@ -19,11 +19,22 @@ describe('ThorClient - Blocks', () => {
     }, 25000);
 
     test('waitForBlock - invalid blockNumber', async () => {
-        // Create ThorClient
         await expect(
-            async () => await thorClient.blocks.waitForBlock(1)
+            async () => await thorClient.blocks.waitForBlock(0)
         ).rejects.toThrowError(
             'Invalid blockNumber. The blockNumber must be a number representing a block number in the future.'
         );
+    });
+
+    test('waitForBlock - maximumWaitingTimeInMilliseconds', async () => {
+        // Get best block
+        const bestBlock = await thorestClient.blocks.getBestBlock();
+        if (bestBlock != null) {
+            const block = await thorClient.blocks.waitForBlock(
+                bestBlock?.number + 2,
+                1000
+            );
+            expect(block).toBeDefined();
+        }
     });
 });
