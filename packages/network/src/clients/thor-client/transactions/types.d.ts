3f3981675fd0fa1b8703dc1a919ccc55e543cd2c
Piero Bassa <pierobassa222@gmail.com>
2023-12-10 21:23:38 +0100

    feat: estimate gas result type

--- packages/network/src/clients/thor-client/transactions/types.d.ts
+++ packages/network/src/clients/thor-client/transactions/types.d.ts
@@ -18,4 +18,38 @@ interface WaitForTransactionOptions {
 
 /* --- Input options end --- */
 
-export type { WaitForTransactionOptions };
+/* --- Responses Outputs start --- */
+
+/**
+ * The result of estimating the gas cost of a transaction.
+ */
+interface EstimateGasResult {
+    /**
+     * The total gas cost estimation of the transaction.
+     */
+    totalGas: number;
+
+    /**
+     * Boolean indicating whether the transaction reverted or not.
+     */
+    reverted: boolean;
+
+    /**
+     * Decoded Solidity revert reasons for each clause.
+     * If the n-th clause reverted, then the n-th element of this array will be the decoded revert reason for the n-th clause.
+     *
+     * @note revertReasons will be undefined if the transaction did not revert.
+     */
+    revertReasons: Array<string | bigint>;
+
+    /**
+     * The error message produced by the Virtual Machine.
+     *
+     * @note vmErrors will be undefined if the transaction did not revert.
+     */
+    vmErrors: string[];
+}
+
+/* --- Responses Outputs end --- */
+
+export type { WaitForTransactionOptions, EstimateGasResult };
