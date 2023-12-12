8e1cdc0a24cd3589d3e1eaddca3d6b5f5d4eb20e
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-12-12 13:24:38 +0100

    fix: change thor solo node strategy

--- packages/ethers-adapter/jest.config.js
+++ packages/ethers-adapter/jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    globalSetup: '<rootDir>/jest.setup.ts',
-    globalTeardown: '<rootDir>/jest.teardown.ts',
     preset: 'ts-jest',
     testEnvironment: 'node',
     coverageReporters: ['html', 'lcov', 'json'],
