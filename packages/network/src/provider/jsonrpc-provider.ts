90c44799c37b223c6b4020cb7da4a9e3b442c092
rodolfopietro97 <rodolfo@rodolfopietro.it>
2023-12-12 13:03:35 +0100

    fix: remove provider to separate package 'ethers-adapter'

--- packages/network/src/provider/jsonrpc-provider.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ------- TEMPORARY COMMENT -------
- * We can then decide to extend our abstract provider to allow json rpc calls
- *
- * This way also a json rpc wrapper (https://github.com/vechain/thorify) can be included in the sdk
- * ------------------------------
- * @TODO
- */
