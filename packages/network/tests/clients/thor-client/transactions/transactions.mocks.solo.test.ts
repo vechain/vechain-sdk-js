179162b6468794d2f051852ca1204672410cd564
Piero Bassa <pierobassa222@gmail.com>
2023-12-16 14:30:16 +0100

    test: buildTransactionBody

---
+++ packages/network/tests/clients/thor-client/transactions/transactions.mocks.solo.test.ts
@@ -0,0 +1,46 @@
+import { jest, describe, test, expect } from '@jest/globals';
+import { ThorClient, ThorestClient } from '../../../../src';
+import { transferTransactionBody } from './fixture';
+import { soloNetwork } from '../../../fixture';
+import { TransactionBodyError } from '@vechainfoundation/vechain-sdk-errors';
+
+/**
+ * Transactions module tests with mocks.
+ *
+ * @group integration/clients/thor-client/transactions
+ */
+describe('buildTransactionBody with mocks', () => {
+    test('Should throw error when genesis block is not found', async () => {
+        const thorestSoloClient = new ThorestClient(soloNetwork);
+
+        // Mock the getBlock method to return null
+        jest.spyOn(thorestSoloClient.blocks, 'getBlock').mockResolvedValue(
+            null
+        );
+
+        const thorSoloClient = new ThorClient(thorestSoloClient);
+
+        await expect(
+            thorSoloClient.transactions.buildTransactionBody([
+                transferTransactionBody.clauses[0]
+            ])
+        ).rejects.toThrowError(TransactionBodyError);
+    });
+
+    test('Should throw error when gest block is not found', async () => {
+        const thorestSoloClient = new ThorestClient(soloNetwork);
+
+        // Mock the getBestBlock method to return null
+        jest.spyOn(thorestSoloClient.blocks, 'getBestBlock').mockResolvedValue(
+            null
+        );
+
+        const thorSoloClient = new ThorClient(thorestSoloClient);
+
+        await expect(
+            thorSoloClient.transactions.buildTransactionBody([
+                transferTransactionBody.clauses[0]
+            ])
+        ).rejects.toThrowError(TransactionBodyError);
+    });
+});
---
+++ packages/network/tests/clients/thor-client/transactions/transactions.testnet.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, test } from '@jest/globals';
+import { buildTransactionBodyClausesTestCases } from './fixture';
+import { thorClient } from '../../../fixture';
+
+/**
+ * Transactions module tests suite.
+ *
+ * @group integration/clients/thor-client/transactions
+ */
+describe('Transactions module Testnet tests suite', () => {
+    /**
+     * Test suite for buildTransactionBody method
+     */
+    describe('buildTransactionBody', () => {
+        /**
+         * buildTransactionBody test cases with different options
+         */
+        buildTransactionBodyClausesTestCases.forEach(
+            ({ description, clauses, options, expected }) => {
+                test(description, async () => {
+                    const txBody =
+                        await thorClient.transactions.buildTransactionBody(
+                            clauses,
+                            options
+                        );
+
+                    expect(txBody).toBeDefined();
+                    expect(txBody.clauses).toStrictEqual(
+                        expected.testnet.clauses
+                    );
+                    expect(txBody.expiration).toBe(expected.testnet.expiration);
+                    expect(txBody.gas).toBe(expected.testnet.gas);
+                    expect(txBody.dependsOn).toBe(expected.testnet.dependsOn);
+                    expect(txBody.gasPriceCoef).toBe(
+                        expected.testnet.gasPriceCoef
+                    );
+                    expect(txBody.reserved).toStrictEqual(
+                        expected.testnet.reserved
+                    );
+                    expect(txBody.chainTag).toBe(expected.testnet.chainTag);
+                });
+            }
+        );
+    });
+});
