79540a69180044cf309d5e72148eb5a2113e7b4c
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2023-12-18 11:56:26 +0100

    feat: build transaction body  (#374)
    * refactor: clause builder instead of transaction builder in core package
    
    * feat: exporting different networks available with genesis and chainTag
    
    * test: refactored tests for clause builder
    
    * feat: transaction body builder functionality in tx module
    
    * refactor: contracts module using clause builder and tx body builder
    
    * feat: contract module input types
    
    * fix: removed circular dependency in gas module with contract module
    
    * feat: tx body options type
    
    * refactor: contracts module tests
    
    * refactor: gas module tests
    
    * test: buildTransactionBody
    
    * fix: contracts docs example test
    
    * chore: vechain casing and import duplication
    
    ---------
    
    Co-authored-by: Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>

--- packages/network/src/clients/thor-client/contracts/types.d.ts
+++ packages/network/src/clients/thor-client/contracts/types.d.ts
@@ -1,4 +1,5 @@
 import type { TransactionSimulationResult } from '../../thorest-client';
+import { type TransactionBodyOptions } from '../transactions';
 
 /**
  * Represents the result of a transaction involving a smart contract, including its ID
@@ -16,4 +17,10 @@ interface ContractTransactionResult {
     clausesResults: TransactionSimulationResult[];
 }
 
-export type { ContractTransactionResult };
+/* --------- Input types Start --------- */
+
+type ContractTransactionOptions = Omit<TransactionBodyOptions, 'isDelegated'>;
+
+/* --------- Input types End --------- */
+
+export type { ContractTransactionResult, ContractTransactionOptions };
