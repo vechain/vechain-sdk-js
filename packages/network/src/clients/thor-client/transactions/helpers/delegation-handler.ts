565a84d50a24c3ba77f2afc178cd3b33fd3a3d10
Piero Bassa <pierobassa222@gmail.com>
2023-12-21 12:58:47 +0100

    feat: delegation handler

---
+++ packages/network/src/clients/thor-client/transactions/helpers/delegation-handler.ts
@@ -0,0 +1,55 @@
+import { type Transaction } from '@vechainfoundation/vechain-sdk-core';
+import { type HttpClient } from '../../../../utils';
+import { type GetDelegationSignatureResult } from '../types';
+import { TRANSACTION, buildError } from '@vechainfoundation/vechain-sdk-errors';
+
+/**
+ * Retrieves the signature of a delegation transaction from a delegator given the endpoint
+ * from which to retrieve the signature.
+ *
+ * @param delegatorUrl - The URL of the endpoint of the delegator.
+ * @param tx - The transaction to delegate.
+ * @param originAddress - The address of the origin account.
+ * @param httpClient - The HTTP client instance used for making HTTP requests.
+ *
+ * @returns A promise that resolves to the signature of the delegation transaction.
+ *
+ * @see [Simple Gas Payer Standard](https://github.com/vechain/VIPs/blob/master/vips/VIP-201.md)
+ *
+ * @throws an error if the delegation fails.
+ */
+const getDelegationSignature = async (
+    tx: Transaction,
+    delegatorUrl: string,
+    originAddress: string,
+    httpClient: HttpClient
+): Promise<Buffer> => {
+    const rawTx = `0x${tx.encoded.toString('hex')}`;
+
+    /**
+     * The request body for the delegation transaction.
+     *
+     * @see [Obtaining Gas Payer Signature](https://github.com/vechain/VIPs/blob/master/vips/VIP-201.md#obtaining-gas-payer-signature)
+     */
+    const sponsorRequestBody = {
+        origin: originAddress,
+        raw: rawTx
+    };
+
+    try {
+        const response = (await httpClient.http('POST', delegatorUrl, {
+            body: sponsorRequestBody
+        })) as GetDelegationSignatureResult;
+
+        return Buffer.from(response.signature.slice(2), 'hex');
+    } catch (error) {
+        throw buildError(
+            TRANSACTION.INVALID_DELEGATION,
+            "Delegation failed: Can't get signature from delegator.",
+            { delegatorUrl },
+            error
+        );
+    }
+};
+
+export { getDelegationSignature };
