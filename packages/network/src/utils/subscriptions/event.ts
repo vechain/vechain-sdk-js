c0457f967f8e4df5dc4d71d8c371a2f8571595e5
Piero Bassa <pierobassa222@gmail.com>
2023-12-29 15:38:35 +0100

    feat: get event subscription url

---
+++ packages/network/src/utils/subscriptions/event.ts
@@ -0,0 +1,53 @@
+import {
+    abi,
+    vechain_sdk_core_ethers
+} from '@vechainfoundation/vechain-sdk-core';
+import { thorest } from '../thorest';
+import { type EventLike, type EventSubscriptionOptions } from './types';
+
+/**
+ * Returns the URL for subscribing to an event through a websocket connection.
+ *
+ * @param baseURL - The URL of the node to request the subscription from.
+ * @param event - The event to subscribe to.
+ *                Can be an event object or a string representing an event.
+ *                @see [Ethers Format Types](https://docs.ethers.org/v5/api/utils/abi/interface/#Interface--formatting
+ *
+ * @param indexedValues - The values of the indexed parameters to construct the topic filters.
+ * @param options - (optional) other optional parameters for the request.
+ *                  `blockID` - The block id to start from, defaults to the best block.
+ *                  `address` - The contract address to filter events by.
+ *
+ * @returns The websocket subscription URL.
+ *
+ * @throws Will throw an error if the event is not a valid event or if the indexed values to encode are invalid.
+ */
+const getEventSubscriptionUrl = (
+    baseURL: string,
+    event: EventLike,
+    indexedValues?: unknown[],
+    options?: EventSubscriptionOptions
+): string => {
+    // If the event is an ethers' EventFragment, format it to a 'full' event string
+    if (vechain_sdk_core_ethers.EventFragment.isFragment(event)) {
+        event = event.format('full');
+    }
+
+    const ev = new abi.Event(event);
+
+    // Encode the indexed parameters to construct the topic filters
+    const encodedTopics = ev.encodeFilterTopics(indexedValues ?? []);
+
+    return thorest.subscriptions.get.EVENT(
+        baseURL,
+        options?.blockID,
+        options?.address,
+        encodedTopics[0] ?? undefined,
+        encodedTopics[1] ?? undefined,
+        encodedTopics[2] ?? undefined,
+        encodedTopics[3] ?? undefined,
+        encodedTopics[4] ?? undefined
+    );
+};
+
+export { getEventSubscriptionUrl };
