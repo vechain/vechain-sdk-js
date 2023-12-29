4ec7899b6c4180f3f41a11e9af0e596543f06c40
Piero Bassa <pierobassa222@gmail.com>
2023-12-29 15:53:55 +0100

    feat: get new transactions subscription url

---
+++ packages/network/src/utils/subscriptions/transfer.ts
@@ -0,0 +1,29 @@
+import { thorest } from '../thorest';
+import { type VETtransfersSubscriptionOptions } from './types';
+
+/**
+ * Returns the URL for subscribing to new VET transfers through a websocket connection.
+ *
+ * @param baseURL - The URL of the node to request the subscription from.
+ * @param options - (optional) other optional parameters for the request.
+ *                  `blockID` - The block id to start from, defaults to the best block.
+ *                  `signerAddress` - The address of the signer of the transaction to filter transfers by.
+ *                  `sender` - The sender address to filter transfers by.
+ *                  `recipient` - The recipient address to filter transfers by.
+ *
+ * @returns The websocket subscription URL.
+ */
+const getVETtransfersSubscriptionUrl = (
+    baseURL: string,
+    options?: VETtransfersSubscriptionOptions
+): string => {
+    return thorest.subscriptions.get.VET_TRANSFER(
+        baseURL,
+        options?.blockID,
+        options?.signerAddress,
+        options?.sender,
+        options?.recipient
+    );
+};
+
+export { getVETtransfersSubscriptionUrl };
