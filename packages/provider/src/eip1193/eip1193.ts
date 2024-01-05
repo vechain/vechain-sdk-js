02b178520bb0220ceb606b78d7047822c3565072
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-01-05 16:57:13 +0100

    fix: uncomment to solve smell, but for sure this code will be removed after a better analisys

--- packages/provider/src/eip1193/eip1193.ts
+++ packages/provider/src/eip1193/eip1193.ts
@@ -1,4 +1,4 @@
-// import { type EIP1193Provider } from './types';
+import { type EIP1193ProviderMessage } from './types';
 
 /**
  * Implements the [EIP-1193 Provider](https://eips.ethereum.org/EIPS/eip-1193) API.
@@ -13,20 +13,20 @@
  * Probably we can remove this file and use directly the `BackwardsCompatibilityProviderAdapter` of hardhat
  * ------------------------------
  */
-// interface EIP1193JSONRPCProvider extends EIP1193Provider {
-//     /**
-//      * Alternative to `request` method. Used for retro-compatibility.
-//      *
-//      * Hardhat predates the EIP1193 (Javascript Ethereum Provider) standard. It was
-//      * built following a draft of that spec, but then it changed completely. We
-//      * still need to support the draft api, but internally we use EIP1193. So we
-//      * use BackwardsCompatibilityProviderAdapter to wrap EIP1193 providers before
-//      * exposing them to the user.
-//      *
-//      * @param method - RPC method
-//      * @param params - Parameters of the RPC method
-//      */
-//     send: (method: string, params?: unknown[]) => Promise<unknown>;
-// }
+interface EIP1193JSONRPCProvider extends EIP1193ProviderMessage {
+    /**
+     * Alternative to `request` method. Used for retro-compatibility.
+     *
+     * Hardhat predates the EIP1193 (Javascript Ethereum Provider) standard. It was
+     * built following a draft of that spec, but then it changed completely. We
+     * still need to support the draft api, but internally we use EIP1193. So we
+     * use BackwardsCompatibilityProviderAdapter to wrap EIP1193 providers before
+     * exposing them to the user.
+     *
+     * @param method - RPC method
+     * @param params - Parameters of the RPC method
+     */
+    send: (method: string, params?: unknown[]) => Promise<unknown>;
+}
 
-// export { type EIP1193JSONRPCProvider };
+export { type EIP1193JSONRPCProvider };
