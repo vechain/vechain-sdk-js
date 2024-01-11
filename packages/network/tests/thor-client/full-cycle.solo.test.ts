f62316e3b63ee99127016fa426f58799f71264f4
fabiorigam <fabio.rigamonti@icloud.com>
2024-01-11 09:47:33 +0100

    fix: add delegator test

--- packages/network/tests/thor-client/full-cycle.solo.test.ts
+++ packages/network/tests/thor-client/full-cycle.solo.test.ts
@@ -17,14 +17,15 @@ import { HttpClient, ThorClient } from '../../src';
  * @group integration/full-cycle
  */
 describe('Full Cycle', () => {
-    test('test', async () => {
+    test('simple test no delegator', async () => {
+        // 1 - Create the client
         const _soloUrl = 'http://localhost:8669/';
         const soloNetwork = new HttpClient(_soloUrl);
         const thorSoloClient = new ThorClient(soloNetwork, {
             isPollingEnabled: false
         });
 
-        // 2 - create the transaction
+        // 2 - Create the transaction clauses
         const transaction = {
             clauses: [
                 contract.clauseBuilder.transferVET(
@@ -75,5 +76,57 @@ describe('Full Cycle', () => {
         console.log(signedTransaction);
 
         await thorSoloClient.transactions.sendTransaction(signedTransaction);
+    });
+
+    test('simple test with delegator', async () => {
+        // 1 - Create the client
+        const _soloUrl = 'http://localhost:8669/';
+        const soloNetwork = new HttpClient(_soloUrl);
+        const thorSoloClient = new ThorClient(soloNetwork, {
+            isPollingEnabled: false
+        });
+
+        // 2 - Create the transaction clauses
+        const transaction = {
+            clauses: [
+                contract.clauseBuilder.transferVET(
+                    '0xb717b660cd51109334bd10b2c168986055f58c1a',
+                    unitsUtils.parseVET('1')
+                )
+            ],
+            simulateTransactionOptions: {
+                caller: '0x7a28e7361fd10f4f058f9fefc77544349ecff5d6'
+            }
+        };
+
+        // Create private key
+        const privateKey = secp256k1.generatePrivateKey();
+
+        const gas = TransactionUtils.intrinsicGas(transaction.clauses);
+
+        const delegatedTransaction = new Transaction({
+            chainTag: 0xf6,
+            blockRef: '0x0000000000000000',
+            expiration: 32,
+            clauses: transaction.clauses,
+            gasPriceCoef: 128,
+            gas,
+            dependsOn: null,
+            nonce: 12345678,
+            reserved: {
+                features: 1
+            }
+        });
+
+        const rawDelegatedSigned = TransactionHandler.signWithDelegator(
+            delegatedTransaction,
+            privateKey,
+            Buffer.from(
+                '7f9290cc44c5fd2b95fe21d6ad6fe5fa9c177e1cd6f3b4c96a97b13e09eaa158',
+                'hex'
+            )
+        );
+
+        await thorSoloClient.transactions.sendTransaction(rawDelegatedSigned);
     });
 });
