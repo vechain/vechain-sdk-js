1ea60ddce005682d18a0806199ea5eef3fa86ab6
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-01-12 11:56:53 +0100

    feat: first not implemented methods

---
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_estimateGas.ts
@@ -0,0 +1,32 @@
+import { type ThorClient } from '@vechain/vechain-sdk-network';
+import { buildError, FUNCTION } from '@vechain/vechain-sdk-errors';
+
+/**
+ * RPC Method eth_estimateGas implementation
+ *
+ * @param thorClient - The thor client instance to use.
+ * @param params - The standard array of rpc call parameters.
+ * @note:
+ * * params[0]: ...
+ * * params[1]: ...
+ * * params[n]: ...
+ */
+const ethEstimateGas = async (
+    thorClient: ThorClient,
+    params: unknown[]
+): Promise<void> => {
+    // To avoid eslint error
+    await Promise.resolve(0);
+
+    // Not implemented yet
+    throw buildError(
+        FUNCTION.NOT_IMPLEMENTED,
+        'Method "eth_estimateGas" not not implemented yet',
+        {
+            params,
+            thorClient
+        }
+    );
+};
+
+export { ethEstimateGas };
