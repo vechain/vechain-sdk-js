1b3423e7d339c804317c91cd33f8978e015bb1b7
Piero Bassa <pierobassa222@gmail.com>
2024-01-15 17:07:59 +0100

    test: added test cases for the vechain provider

--- packages/provider/tests/fixture.ts
+++ packages/provider/tests/fixture.ts
@@ -170,6 +170,40 @@ const blockWithTransactionsNotExpanded = {
         '0x0000000000000000000000000000000000000000000000000000000000000000'
 };
 
+/**
+ * Valid transaction hash fixture
+ */
+const validTransactionHashTestnet =
+    '0xb2e3f6e9782f462d797b72f9cbf5a4c38ca20cabcc1a091f9de6d3e6736c1f7c';
+
+/**
+ * Valid transaction detail fixture of the `validTransactionHashTestnet` transaction
+ */
+const validTransactionDetailTestnet = {
+    blockHash:
+        '0x010b7b5f0192003f70bf2a6a502221e075cb32d676e3443614d21003cc2ee440',
+    blockNumber: '0x10b7b5f',
+    from: '0x8c59c63d6458c71b6ff88d57698437524a703084',
+    gas: '0x618af',
+    chainId:
+        '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127',
+    hash: '0xb2e3f6e9782f462d797b72f9cbf5a4c38ca20cabcc1a091f9de6d3e6736c1f7c',
+    nonce: '0x19b4782',
+    transactionIndex: '0x0',
+    input: '0xf14fcbc8ad2a4a05c94893cc69b721955da2fb2e93ba001224f6ec7250ad110765065541',
+    to: '0xaeb29614bb9af450a7fff539bbba319455a1aca7',
+    value: '0x0',
+    gasPrice: '0x',
+    type: '0x',
+    v: '0x',
+    r: '0x',
+    s: '0x',
+    accessList: [],
+    maxFeePerGas: '0x',
+    maxPriorityFeePerGas: '0x',
+    yParity: '0x'
+};
+
 export {
     testnetUrl,
     soloUrl,
@@ -177,5 +211,7 @@ export {
     soloNetwork,
     testAccount,
     blockWithTransactionsExpanded,
-    blockWithTransactionsNotExpanded
+    blockWithTransactionsNotExpanded,
+    validTransactionHashTestnet,
+    validTransactionDetailTestnet
 };
--- packages/provider/tests/providers/fixture.ts
+++ packages/provider/tests/providers/fixture.ts
@@ -1,5 +1,9 @@
 import { vechain_sdk_core_ethers } from '@vechain/vechain-sdk-core';
 import { zeroBlock } from '../rpc-mapper/methods/eth_getBlockByNumber/fixture';
+import {
+    validTransactionDetailTestnet,
+    validTransactionHashTestnet
+} from '../fixture';
 
 /**
  * Test cases for provider methods
@@ -11,6 +15,19 @@ const providerMethodsTestCases = [
         method: 'eth_getBlockByNumber',
         params: [vechain_sdk_core_ethers.toQuantity(0), false],
         expected: zeroBlock
+    },
+    {
+        description: 'Should be able to call eth_chainId',
+        method: 'eth_chainId',
+        params: [],
+        expected:
+            '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127'
+    },
+    {
+        description: `Should be able to call eth_getTransactionByHash with ${validTransactionHashTestnet} as the transaction hash`,
+        method: 'eth_getTransactionByHash',
+        params: [validTransactionHashTestnet],
+        expected: validTransactionDetailTestnet
     }
 ];
 
