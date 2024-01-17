21b40cf15add3e974b7f7a012ed88752c178cb9b
Piero Bassa <pierobassa222@gmail.com>
2024-01-17 16:57:48 +0100

    test: added fixture with invalid slot key

--- packages/provider/tests/rpc-mapper/methods/eth_getStorageAt/fixture.ts
+++ packages/provider/tests/rpc-mapper/methods/eth_getStorageAt/fixture.ts
@@ -21,6 +21,17 @@ const ethGetStorageAtTestCases = [
     },
     {
         description:
+            'Should return storage slot value for a given smart contract that has a storage slot value different than NULL',
+        params: [
+            '0x93Ae8aab337E58A6978E166f8132F59652cA6C56',
+            '0x0000000000000000000000000000000000000000000000000000000000000001',
+            '0x10afdf1' // Block n. 17497585
+        ],
+        expected:
+            '0x0000000000000000000000000000000000000000000000000000000061474260'
+    },
+    {
+        description:
             'Should return null slot value for an address that does not have a storage slot value at the given position',
         params: [
             dataUtils.toHexString(ZERO_BUFFER(20), {
@@ -56,6 +67,11 @@ const invalidEthGetStorageAtTestCases = [
     {
         description: 'Should throw error for invalid address',
         params: ['0x123', '0x1', '0x10afdf1'],
+        expectedError: ProviderRpcError
+    },
+    {
+        description: 'Should throw error for invalid slot',
+        params: ['0x123', `0x${'0'.repeat(65)}1`, '0x10afdf1'],
         expectedError: ProviderRpcError
     }
 ];
