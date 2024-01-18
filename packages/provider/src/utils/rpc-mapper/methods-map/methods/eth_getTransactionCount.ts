7d457427055c99e700d687028256d1a001011d20
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-01-18 16:22:44 +0100

    chore: improve doc

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_getTransactionCount.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_getTransactionCount.ts
@@ -8,11 +8,12 @@ import { addressUtils } from '@vechain/vechain-sdk-core';
  * @link [eth_getTransactionCount](https://docs.infura.io/networks/ethereum/json-rpc-methods/eth_gettransactioncount)
  *
  * @param params - The standard array of rpc call parameters.
- * @note:
- * * params[0]: address: string, is the address to get the number of transactions from.
+ *                * params[0]: address: string, is the address to get the number of transactions from.
  *
  * @note: To respect differences between vechain and Ethereum, in this function we will give a random number as output.
  * Basically Ethereum to get nonce use the number of transactions sent from an address, while vechain use a random number.
+ *
+ * @throws {InvalidDataTypeError} - When address parameter is invalid.
  */
 const ethGetTransactionCount = async (params: unknown[]): Promise<string> => {
     // Input validation - Invalid params
