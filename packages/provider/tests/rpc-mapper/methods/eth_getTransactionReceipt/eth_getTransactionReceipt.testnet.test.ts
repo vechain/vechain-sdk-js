aab3f0cf4249e90bd8b715b68e189faa338974bd
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-01-19 11:12:00 +0100

    fix: test naming following convention

--- packages/provider/tests/rpc-mapper/methods/eth_getTransactionReceipt/eth_getTransactionReceipt.testnet.test.ts
+++ packages/provider/tests/rpc-mapper/methods/eth_getTransactionReceipt/eth_getTransactionReceipt.testnet.test.ts
@@ -1,16 +1,8 @@
-import {
-    afterEach,
-    beforeEach,
-    describe,
-    expect,
-    jest,
-    test
-} from '@jest/globals';
+import { afterEach, beforeEach, describe, expect, test } from '@jest/globals';
 import { RPC_METHODS, RPCMethodsMap } from '../../../../src';
 import { ThorClient } from '@vechain/vechain-sdk-network';
 import { testNetwork } from '../../../fixture';
 import { getReceiptCorrectCasesTestNetwork } from './fixture';
-import { ProviderRpcError } from '@vechain/vechain-sdk-errors';
 
 /**
  * RPC Mapper integration tests for 'eth_getTransactionReceipt' method
@@ -57,29 +49,6 @@ describe('RPC Mapper - eth_getTransactionReceipt method tests', () => {
                 },
                 7000
             );
-        });
-    });
-
-    /**
-     * eth_getTransactionReceipt RPC call tests - Negative cases
-     */
-    describe('eth_getTransactionReceipt - Negative cases', () => {
-        /**
-         * Negative case 1 - An error occurs while retrieving the transaction receipt
-         */
-        test('eth_getTransactionReceipt - negative case 1', async () => {
-            // Mock the getTransactionReceipt method to throw an error
-            jest.spyOn(
-                thorClient.transactions,
-                'getTransactionReceipt'
-            ).mockRejectedValue(new Error());
-
-            // Call eth_getTransactionReceipt with a valid transaction hash BUT an error occurs while retrieving the transaction receipt
-            await expect(
-                RPCMethodsMap(thorClient)[
-                    RPC_METHODS.eth_getTransactionReceipt
-                ]([getReceiptCorrectCasesTestNetwork[0].hash])
-            ).rejects.toThrowError(ProviderRpcError);
         });
     });
 });
