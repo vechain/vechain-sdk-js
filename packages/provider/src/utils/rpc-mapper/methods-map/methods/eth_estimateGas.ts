0ac42fb5fee85d212e5aa6b7ac47d49c4343f881
fabiorigam <fabio.rigamonti@icloud.com>
2024-01-29 17:29:09 +0100

    fix: add eth_estimategas first test

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_estimateGas.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_estimateGas.ts
@@ -3,6 +3,7 @@ import {
     type ThorClient
 } from '@vechain/vechain-sdk-network';
 import { TransactionUtils } from '@vechain/vechain-sdk-core';
+import { assert, DATA } from '@vechain/vechain-sdk-errors';
 
 /**
  * RPC Method eth_estimateGas implementation
@@ -10,20 +11,23 @@ import { TransactionUtils } from '@vechain/vechain-sdk-core';
  * @link [eth_estimateGas](https://docs.infura.io/networks/ethereum/json-rpc-methods/eth_estimategas)
  *
  * @param thorClient - The thor client instance to use.
- * @param params - The standard array of rpc call parameters.
- * @note:
- * * params[0]: ...
- * * params[1]: ...
- * * params[n]: ...
+ * @param params - The transaction call object
  */
 const ethEstimateGas = async (
     thorClient: ThorClient,
     params: unknown[]
 ): Promise<string> => {
+    // Input validation - Invalid params
+    assert(
+        params.length === 1,
+        DATA.INVALID_DATA_TYPE,
+        'Invalid params length, expected 1.'
+    );
+
     // The intrinsic gas of the transaction
-    const intrinsicGas = TransactionUtils.intrinsicGas(
+    const intrinsicGas = TransactionUtils.intrinsicGas([
         params as unknown as SimulateTransactionClause
-    ).toString(16);
+    ]).toString(16);
 
     return await Promise.resolve(intrinsicGas);
 };
--- packages/provider/tests/rpc-mapper/methods/eth_estimateGas/eth_estimateGas.test.ts
+++ packages/provider/tests/rpc-mapper/methods/eth_estimateGas/eth_estimateGas.test.ts
@@ -3,6 +3,7 @@ import { NotImplementedError } from '@vechain/vechain-sdk-errors';
 import { RPC_METHODS, RPCMethodsMap } from '../../../../src';
 import { ThorClient } from '@vechain/vechain-sdk-network';
 import { testNetwork } from '../../../fixture';
+import { TRANSACTIONS_GAS_CONSTANTS } from '@vechain/vechain-sdk-core';
 
 /**
  * RPC Mapper integration tests for 'eth_estimateGas' method
@@ -38,31 +39,41 @@ describe('RPC Mapper - eth_estimateGas method tests', () => {
          * Positive case 1 - ... Description ...
          */
         test('eth_estimateGas - positive case 1', async () => {
-            // NOT IMPLEMENTED YET!
-            await expect(
-                async () =>
-                    await RPCMethodsMap(thorClient)[
-                        RPC_METHODS.eth_estimateGas
-                    ]([-1])
-            ).rejects.toThrowError(NotImplementedError);
+            const clauses = [
+                {
+                    to: '0x0000000000000000000000000000000000000000',
+                    value: 0,
+                    data: ''
+                }
+            ];
+            const expected =
+                TRANSACTIONS_GAS_CONSTANTS.CLAUSE_GAS * 5 +
+                TRANSACTIONS_GAS_CONSTANTS.TX_GAS;
+
+            const estimatedGas =
+                await RPCMethodsMap(thorClient)[RPC_METHODS.eth_estimateGas](
+                    clauses
+                );
+
+            expect(estimatedGas).toBe(expected);
         });
     });
 
     /**
      * eth_estimateGas RPC call tests - Negative cases
      */
-    describe('eth_estimateGas - Negative cases', () => {
-        /**
-         * Negative case 1 - ... Description ...
-         */
-        test('eth_estimateGas - negative case 1', async () => {
-            // NOT IMPLEMENTED YET!
-            await expect(
-                async () =>
-                    await RPCMethodsMap(thorClient)[
-                        RPC_METHODS.eth_estimateGas
-                    ](['SOME_RANDOM_PARAM'])
-            ).rejects.toThrowError(NotImplementedError);
-        });
-    });
+    // describe('eth_estimateGas - Negative cases', () => {
+    //     /**
+    //      * Negative case 1 - ... Description ...
+    //      */
+    //     test('eth_estimateGas - negative case 1', async () => {
+    //         // NOT IMPLEMENTED YET!
+    //         await expect(
+    //             async () =>
+    //                 await RPCMethodsMap(thorClient)[
+    //                     RPC_METHODS.eth_estimateGas
+    //                 ](['SOME_RANDOM_PARAM'])
+    //         ).rejects.toThrowError(NotImplementedError);
+    //     });
+    // });
 });
