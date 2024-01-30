a6c00b319711868a6d089113eddad3b3f3b2d52c
fabiorigam <fabio.rigamonti@icloud.com>
2024-01-30 15:41:20 +0100

    fix: add comments

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/evm_mine.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/evm_mine.ts
@@ -13,9 +13,11 @@ import {
  * @returns The new block or null if the block is not available.
  */
 const evmMine = async (thorClient: ThorClient): Promise<BlockDetail | null> => {
+    // Get best block
     const bestBlock = await thorClient.blocks.getBestBlock();
     let newBlock = null;
     if (bestBlock != null) {
+        // Wait for new block
         newBlock = await thorClient.blocks.waitForBlock(bestBlock.number + 1);
     }
     return newBlock;
