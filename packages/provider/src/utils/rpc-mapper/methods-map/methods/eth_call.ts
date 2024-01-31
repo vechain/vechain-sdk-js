b436d970b2618fead9a309239af7e868e4db94ca
fabiorigam <fabio.rigamonti@icloud.com>
2024-01-31 11:13:35 +0100

    fix: improve eth_call and relative tests

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_call.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_call.ts
@@ -3,6 +3,7 @@ import {
     type ThorClient
 } from '@vechain/vechain-sdk-network';
 import { assert, DATA } from '@vechain/vechain-sdk-errors';
+import { JSONRPC, buildProviderError } from '../../../../../../errors/dist';
 
 /**
  * RPC Method eth_call implementation
@@ -11,6 +12,8 @@ import { assert, DATA } from '@vechain/vechain-sdk-errors';
  *
  * @param thorClient - The thor client instance to use.
  * @param params - The transaction call object
+ *
+ * @returns The return value of executed contract.
  */
 const ethCall = async (
     thorClient: ThorClient,
@@ -23,13 +26,26 @@ const ethCall = async (
         'Invalid params length, expected at least 1.'
     );
 
-    // Simulate transaction
-    const simulatedTx = await thorClient.transactions.simulateTransaction(
-        params as SimulateTransactionClause[]
-    );
+    try {
+        // Simulate transaction
+        const simulatedTx = await thorClient.transactions.simulateTransaction(
+            params as SimulateTransactionClause[]
+        );
 
-    // Return simulated transaction data
-    return simulatedTx[0].data;
+        // Return simulated transaction data
+        return simulatedTx[0].data;
+    } catch (e) {
+        throw buildProviderError(
+            JSONRPC.INTERNAL_ERROR,
+            `Method 'eth_call' failed: Error while simulating transaction\n
+            Params: ${JSON.stringify(params)}\n
+            URL: ${thorClient.httpClient.baseURL}`,
+            {
+                params,
+                innerError: JSON.stringify(e)
+            }
+        );
+    }
 };
 
 export { ethCall };
