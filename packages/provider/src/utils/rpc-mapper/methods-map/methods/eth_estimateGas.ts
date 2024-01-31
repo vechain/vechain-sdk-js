077bf567a4e326e843e5474599067046e52694bb
fabiorigam <fabio.rigamonti@icloud.com>
2024-01-31 11:20:56 +0100

    fix: improve eth_estimateGas

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_estimateGas.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_estimateGas.ts
@@ -3,6 +3,7 @@ import {
     TransactionUtils
 } from '@vechain/vechain-sdk-core';
 import { assert, DATA } from '@vechain/vechain-sdk-errors';
+import { JSONRPC, buildProviderError } from '../../../../../../errors/dist';
 
 /**
  * RPC Method eth_estimateGas implementation
@@ -10,6 +11,8 @@ import { assert, DATA } from '@vechain/vechain-sdk-errors';
  * @link [eth_estimateGas](https://docs.infura.io/networks/ethereum/json-rpc-methods/eth_estimategas)
  *
  * @param params - The transaction call object
+ *
+ * @returns A hexadecimal of the estimate of the gas for the given transaction.
  */
 const ethEstimateGas = async (params: unknown[]): Promise<string> => {
     // Input validation - Invalid params
@@ -19,13 +22,27 @@ const ethEstimateGas = async (params: unknown[]): Promise<string> => {
         'Invalid params length, expected 1.'
     );
 
-    // The intrinsic gas of the transaction
-    const intrinsicGas = TransactionUtils.intrinsicGas(
-        params as TransactionClause[]
-    );
+    try {
+        // The intrinsic gas of the transaction
+        const intrinsicGas = TransactionUtils.intrinsicGas(
+            params as TransactionClause[]
+        );
 
-    // Convert intrinsic gas to hex string and return
-    return await Promise.resolve('0x' + intrinsicGas.toString(16));
+        // Convert intrinsic gas to hex string and return
+        return await Promise.resolve('0x' + intrinsicGas.toString(16));
+    } catch (e) {
+        throw buildProviderError(
+            JSONRPC.INTERNAL_ERROR,
+            `Method 'eth_estimateGas' failed: Error while calculating gas for ${
+                params[0] as string
+            } transaction\n
+            Params: ${JSON.stringify(params)}\n`,
+            {
+                params,
+                innerError: JSON.stringify(e)
+            }
+        );
+    }
 };
 
 export { ethEstimateGas };
