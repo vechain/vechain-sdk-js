a44e84f49491620ab6dab6464f364b98e670186d
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-02-06 16:18:23 +0100

    fix: removed unuseful type

--- packages/provider/src/utils/rpc-mapper/types.d.ts
+++ packages/provider/src/utils/rpc-mapper/types.d.ts
@@ -1,8 +1,3 @@
-import {
-    type SimulateTransactionClause,
-    type SimulateTransactionOptions
-} from '@vechain/vechain-sdk-network';
-
 /**
  * Type for the method handler.
  * It is basically a function that takes an array of parameters and returns a promise.
@@ -11,9 +6,4 @@ type MethodHandlerType<TParams, TReturnType> = (
     params: TParams[]
 ) => Promise<TReturnType>;
 
-/**
- * Type for the transaction object.
- */
-type TransactionObj = SimulateTransactionClause & SimulateTransactionOptions;
-
-export { type MethodHandlerType, type TransactionObj };
+export { type MethodHandlerType };
