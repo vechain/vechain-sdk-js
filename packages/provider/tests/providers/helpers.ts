0da055979286f84c0f26c7effaff6237f016aafd
Valazan <al-fede@hotmail.it>
2024-02-09 14:46:13 +0100

    test: multiple log subscriptions

--- packages/provider/tests/providers/helpers.ts
+++ packages/provider/tests/providers/helpers.ts
@@ -1,6 +1,12 @@
 import { type SubscriptionEvent, type VechainProvider } from '../../src';
-import { ERC20_ABI, ERC20_BYTECODE, TEST_ACCOUNT } from './fixture';
+import {
+    ERC20_ABI,
+    ERC20_BYTECODE,
+    ERC721_BYTECODE,
+    TEST_ACCOUNT
+} from './fixture';
 import { type Contract, type ThorClient } from '@vechain/vechain-sdk-network';
+import { ERC721_ABI } from '@vechain/vechain-sdk-core';
 
 export async function waitForMessage(
     provider: VechainProvider
@@ -19,6 +25,20 @@ export async function deployERC20Contract(
     const factory = thorClient.contracts.createContractFactory(
         ERC20_ABI,
         ERC20_BYTECODE,
+        TEST_ACCOUNT.privateKey
+    );
+
+    await factory.startDeployment();
+
+    return await factory.waitForDeployment();
+}
+
+export async function deployERC721Contract(
+    thorClient: ThorClient
+): Promise<Contract> {
+    const factory = thorClient.contracts.createContractFactory(
+        ERC721_ABI,
+        ERC721_BYTECODE,
         TEST_ACCOUNT.privateKey
     );
 
