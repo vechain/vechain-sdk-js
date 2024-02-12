72d17f5f015cde2047309f879cb50997e8adcc73
fabiorigam <fabio.rigamonti@icloud.com>
2024-02-12 16:34:17 +0100

    fix: update eth_getCode comments

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_getCode.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_getCode.ts
@@ -16,7 +16,9 @@ import {
  *               * params[0]: The address to get the code for as a hex string.
  *               * params[1]: The block number to get the code at as a hex string or "latest".
  *
- * @returns the code of the account at the given address formatted to the RPC standard.
+ * @returns The code of the account at the given address formatted to the RPC standard.
+ *
+ * @note Only 'latest' and 'finalized' block numbers are supported.
  *
  * @throws {ProviderRpcError} - Will throw an error if the retrieval of the code fails.
  */
