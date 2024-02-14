a7f4dc108c40797596e0a7cd556c2de0d3321be8
fabiorigam <fabio.rigamonti@icloud.com>
2024-02-14 12:19:10 +0100

    fix: improve eth_chainId

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_chainId.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_chainId.ts
@@ -8,25 +8,29 @@ import { buildProviderError, JSONRPC } from '@vechain/vechain-sdk-errors';
  *
  * @param thorClient - ThorClient instance.
  *
- * @returns A hexadecimal of the current chain ID or '0x0' if the chain id is not available.
+ * @returns The chain id
  */
 const ethChainId = async (thorClient: ThorClient): Promise<string> => {
+    let genesisBlock;
     try {
-        const genesisBlock = await thorClient.blocks.getGenesisBlock();
-        if (genesisBlock != null) {
-            const chainId = genesisBlock?.id;
-            return (chainId as unknown as number).toString(16);
-        } else {
-            return '0x0';
-        }
+        genesisBlock = await thorClient.blocks.getGenesisBlock();
     } catch (e) {
         throw buildProviderError(
             JSONRPC.INTERNAL_ERROR,
-            `Method 'eth_chainId' failed: Error while getting the chain Id.\n
+            `Method 'eth_chainId' failed: Error while getting the chain id.\n
             URL: ${thorClient.httpClient.baseURL}`,
             {
                 innerError: JSON.stringify(e)
             }
+        );
+    }
+    if (genesisBlock != null) {
+        return genesisBlock.id;
+    } else {
+        throw buildProviderError(
+            JSONRPC.INTERNAL_ERROR,
+            `Method 'eth_chainId' failed: Genesis block is null.\n
+            URL: ${thorClient.httpClient.baseURL}`
         );
     }
 };
