1b1ed53d5786eb32dfe36e13edffe47a207e02de
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-02-15 12:22:07 +0100

    fix: change a little bit debug rpc output endpoints

--- packages/network/src/thor-client/debug/types-by-name/call-name-types.d.ts
+++ packages/network/src/thor-client/debug/types-by-name/call-name-types.d.ts
@@ -7,13 +7,24 @@ type CallNameConfig = Record<string, unknown>;
  * Return type for the 'call' name type
  */
 interface CallNameReturnType {
+    /**
+     * Transaction parameters
+     */
     from: string;
     gas: string;
     gasUsed: string;
     to: string;
     input: string;
     output: string;
-    // Trace clause type (/debug/tracers endpoint)
+
+    /**
+     * Transaction errors (if any)
+     */
+    error?: string;
+
+    /**
+     * Trace clause type (/debug/tracers endpoint)
+     */
     calls?: Array<{
         from: string;
         gas: string;
@@ -23,7 +34,10 @@ interface CallNameReturnType {
         output: string;
         type: string;
     }>;
-    // Trace contract type (/debug/tracers/call endpoint)
+
+    /**
+     * Trace contract type (/debug/tracers/call endpoint)
+     */
     value?: string;
     type?: string;
 }
