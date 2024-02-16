c643d75407bf5a9e2e63a476dd93df019d5c2e5f
Valazan <al-fede@hotmail.it>
2024-02-16 10:06:19 +0100

    refactor: eth_gasPrice, eth_getTransactionCount and eth_getLogs (#579)
    * refactor: eth_gasPrice, eth_getTransactionCount and eth_getLogs JSON-RPC methods
    
    * revert: eth_getTransactionCount return value

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_gasPrice.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_gasPrice.ts
@@ -1,11 +1,11 @@
 /**
  * RPC Method eth_gasPrice implementation
- *
  * @link [ethGasPrice](https://docs.infura.io/networks/ethereum/json-rpc-methods/eth_gasprice)
+ * @returns The current gas price in Wei unit considering that 1 VTHO equals 1e18 Wei.
  */
 const ethGasPrice = async (): Promise<string> => {
     // Dummy implementation
-    return await Promise.resolve('0x0');
+    return await Promise.resolve('0x9184e72a000');
 };
 
 export { ethGasPrice };
