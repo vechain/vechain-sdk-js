9c4b4cc83dc45654ac15c83e10470c40a571a04e
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-02-19 12:08:17 +0100

    feat: solidity version and networks vechain

--- apps/vechain-sdk-hardhat-integration/hardhat.config.ts
+++ apps/vechain-sdk-hardhat-integration/hardhat.config.ts
@@ -1,8 +1,14 @@
 import { type HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@vechain/vechain-sdk-hardhat-plugin';
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.17'
+    solidity: '0.8.17',
+    networks: {
+        vechain: {
+            url: 'https://sync-testnet.vechain.org/'
+        }
+    }
 };
 
 export default config;
