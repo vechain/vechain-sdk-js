ced69b91282547b28ab9c9f057b1eb03ef610063
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-02-22 09:52:24 +0000

    feat: #496 provides a unified and coherent `Hex.of()` method

---
+++ docs/examples/debug/debug-retrieve-storage-range.js
@@ -0,0 +1,20 @@
+import { HttpClient, ThorClient } from '@vechain/vechain-sdk-network';
+// 1 - Create thor client for testnet
+const _testnetUrl = 'https://testnet.vechain.org';
+const testNetwork = new HttpClient(_testnetUrl);
+const thorClient = new ThorClient(testNetwork);
+// 2 - Retrieve the storage range.
+const result = await thorClient.debug.retrieveStorageRange({
+    target: {
+        blockID: '0x010e80e3278e234b8a5d1195c376909456b94d1f7cf3cb7bfab1e8998dbcfa8f',
+        transaction: 0,
+        clauseIndex: 0
+    },
+    options: {
+        address: '0x0000000000000000000000000000456E65726779',
+        keyStart: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        maxResult: 10
+    }
+});
+// 3 - Print the result.
+console.log(result);
