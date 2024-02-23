b39c331211cfc1760572db78070bcfa6c1bb1343
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-02-23 11:59:30 +0100

    fix: remove unused assertion

--- packages/core/src/transaction/helpers/assertions.ts
+++ packages/core/src/transaction/helpers/assertions.ts
@@ -2,20 +2,6 @@ import { assert, TRANSACTION } from '@vechain/vechain-sdk-errors';
 import { type Transaction } from '../transaction';
 
 /**
- * Assert if transaction is already signed
- *
- * @param transaction - Transaction to assert
- */
-function assertTransactionIsNotSigned(transaction: Transaction): void {
-    assert(
-        !transaction.isSigned,
-        TRANSACTION.ALREADY_SIGNED,
-        'Cannot sign Transaction. It is already signed.',
-        { transaction }
-    );
-}
-
-/**
  * Assert if transaction is not signed and cannot get field (e.g. delegator, origin, or id)
  *
  * @param transaction - Transaction to assert
@@ -32,7 +18,4 @@ function assertCantGetFieldOnUnsignedTransaction(
     );
 }
 
-export {
-    assertTransactionIsNotSigned,
-    assertCantGetFieldOnUnsignedTransaction
-};
+export { assertCantGetFieldOnUnsignedTransaction };
