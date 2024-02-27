022717979157074e3896301ac1aab534a34ba08f
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-02-27 17:13:54 +0000

    feat: #496 provides a unified and coherent `Hex.of*()` methods

--- packages/core/tests/utils/hex/hex.unit.test.ts
+++ packages/core/tests/utils/hex/hex.unit.test.ts
@@ -2,55 +2,42 @@ import { describe, expect, test } from '@jest/globals';
 import { Hex } from '../../../src/utils/hex/Hex';
 
 describe('Hex', () => {
-    test('ofBigInt', () => {
-        const output = Hex.ofBigInt(BigInt(10), 0);
+    test('hex of bigint', () => {
+        const output: string = Hex.of(BigInt(10), 0);
         expect(output).toBe('0a');
 
         expect(() => {
-            Hex.ofBigInt(BigInt(-10), 0);
+            Hex.of(BigInt(-10), 0);
         }).toThrow("Arg 'n' not negative.");
     });
 
-    test('ofNumber', () => {
-        const output = Hex.ofNumber(10, 0);
+    test('hex of buffer', () => {
+        const buffer: Uint8Array = new Uint8Array(1);
+        buffer[0] = 10;
+        const output: string = Hex.of(buffer, 0);
+        expect(output).toBe('0a');
+    });
+
+    test('hex of number', () => {
+        const output: string = Hex.of(10 as number, 0);
         expect(output).toBe('0a');
 
         expect(() => {
-            Hex.ofNumber(3.14, 0);
+            Hex.of(3.14, 0);
         }).toThrow(`Arg 'n' not an integer.`);
 
         expect(() => {
-            Hex.ofNumber(-10, 0);
+            Hex.of(-10, 0);
         }).toThrow("Arg 'n' not negative.");
     });
 
-    test('ofBuffer', () => {
-        const buffer = new Uint8Array(1);
-        buffer[0] = 10;
-        const output = Hex.ofBuffer(buffer, 0);
-        expect(output).toBe('0a');
-    });
-
-    test('ofString', () => {
-        const output = Hex.ofString('a', 0);
-        expect(output).toBe('61');
-    });
-
-    test('of', () => {
-        let output = Hex.of(10, 0);
-        expect(output).toBe('0a');
-
-        const buffer = new Uint8Array(1);
-        buffer[0] = 10;
-        output = Hex.of(buffer, 0);
-        expect(output).toBe('0a');
-
-        output = Hex.of('a', 0);
+    test('hex of string', () => {
+        const output: string = Hex.of('a', 0);
         expect(output).toBe('61');
     });
 
     test('of0x', () => {
-        const output = Hex.of0x(10, 0);
+        const output: string = Hex.of0x(10, 0);
         expect(output).toBe('0x0a');
     });
 });
