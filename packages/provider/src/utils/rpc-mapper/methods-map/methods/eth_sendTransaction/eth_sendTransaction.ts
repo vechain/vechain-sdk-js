8e8c966ff621f0df9b66e72e0e75215eb69a2a4a
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-03-07 11:03:49 +0100

    fix: move sign method into provider

--- packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_sendTransaction/eth_sendTransaction.ts
+++ packages/provider/src/utils/rpc-mapper/methods-map/methods/eth_sendTransaction/eth_sendTransaction.ts
@@ -1,8 +1,4 @@
-import {
-    DelegationHandler,
-    type SignTransactionOptions,
-    type ThorClient
-} from '@vechain/vechain-sdk-network';
+import { type ThorClient } from '@vechain/vechain-sdk-network';
 import {
     assert,
     buildProviderError,
@@ -11,15 +7,7 @@ import {
 } from '@vechain/vechain-sdk-errors';
 import { type VechainProvider } from '../../../../../providers';
 import { ethSendRawTransaction } from '../eth_sendRawTransaction/eth_sendRawTransaction';
-import {
-    clauseBuilder,
-    dataUtils,
-    Hex,
-    type TransactionClause
-} from '@vechain/vechain-sdk-core';
-import { type Wallet, type WalletAccount } from '@vechain/vechain-sdk-wallet';
 import { type TransactionObjectInput } from './types';
-import { randomBytes } from 'crypto';
 
 /**
  * RPC Method eth_sendTransaction implementation
@@ -91,75 +79,11 @@ const ethSendTransaction = async (
     const [transaction] = params as [TransactionObjectInput];
 
     try {
-        // 1 - Initiate the transaction clauses
-        const transactionClauses: TransactionClause[] =
-            transaction.to !== undefined
-                ? // Normal transaction
-                  [
-                      {
-                          to: transaction.to,
-                          data: transaction.data ?? '0x',
-                          value: transaction.value ?? '0x0'
-                      } satisfies TransactionClause
-                  ]
-                : // If 'to' address is not provided, it will be assumed that the transaction is a contract creation transaction.
-                  [clauseBuilder.deployContract(transaction.data ?? '0x')];
-
-        // 2 - Estimate gas
-        const gasResult = await thorClient.gas.estimateGas(
-            transactionClauses,
-            transaction.from
-        );
-
-        // 3 - Get the signed transaction (we already know wallet is defined, thanks to the input validation)
-        const walletTouse: Wallet = provider?.wallet as Wallet;
-
-        const delegatorIntoWallet: SignTransactionOptions | null =
-            await walletTouse.getDelegator();
-
-        const signerIntoWallet: WalletAccount | null =
-            await walletTouse.getAccount(transaction.from);
-
-        // 4 - Create transaction body
-        const transactionBody =
-            await thorClient.transactions.buildTransactionBody(
-                transactionClauses,
-                gasResult.totalGas,
-                {
-                    isDelegated:
-                        DelegationHandler(delegatorIntoWallet).isDelegated()
-                }
-            );
-
-        // NOTE: To be compliant with the standard and to avoid nonce overflow, we generate a random nonce of 6 bytes
-
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { nonce, ...transactionBodyWithoutNonce } = transactionBody;
-        const newNonce = `0x${dataUtils.toHexString(randomBytes(6))}`;
-
-        // At least, a signer private key is required
-        if (
-            signerIntoWallet?.privateKey === null ||
-            signerIntoWallet?.privateKey === undefined
-        ) {
-            throw buildProviderError(
-                JSONRPC.INTERNAL_ERROR,
-                `Method 'eth_sendTransaction' failed: Wallet has not the private key of signer.\n
-                Params: ${JSON.stringify(params)}\n
-                URL: ${thorClient.httpClient.baseURL}`
-            );
-        }
         // Sign the transaction
-        const signedTransaction = await thorClient.transactions.signTransaction(
-            { nonce: newNonce, ...transactionBodyWithoutNonce },
-            Hex.of(signerIntoWallet.privateKey),
-            DelegationHandler(delegatorIntoWallet).delegatorOrUndefined()
-        );
+        const signedTransaction = await provider?.sign(transaction);
 
         // Return the result
-        return await ethSendRawTransaction(thorClient, [
-            Hex.of0x(signedTransaction.encoded)
-        ]);
+        return await ethSendRawTransaction(thorClient, [signedTransaction]);
     } catch (e) {
         throw buildProviderError(
             JSONRPC.INTERNAL_ERROR,
