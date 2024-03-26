678db7be8b5e27aea2bf3e9def5f681cc55c5755
fabiorigam <fabio.rigamonti@icloud.com>
2024-03-26 14:50:16 +0100

    fix: add first draft of signer

---
+++ packages/wallet/src/signers/private-key-signer.ts
@@ -0,0 +1,57 @@
+import { type ThorClient } from '@vechain/sdk-network';
+import {
+    type SendTxOptions,
+    type ExtendedClause,
+    type SendTxResponse,
+    type TransactionSender
+} from './types';
+import { addressUtils, secp256k1, Transaction } from '@vechain/sdk-core';
+
+class PrivateKeySigner implements TransactionSender {
+    private readonly privateKey: Buffer;
+    private readonly address: string;
+    private readonly client: ThorClient;
+
+    constructor(client: ThorClient, privateKey: Buffer) {
+        this.client = client;
+        this.privateKey = privateKey;
+        this.address = addressUtils.fromPrivateKey(privateKey);
+    }
+
+    static new = (client: ThorClient, privateKey: Buffer): PrivateKeySigner => {
+        return new PrivateKeySigner(client, privateKey);
+    };
+
+    public sendTransaction: TransactionSender['sendTransaction'] = async (
+        clauses: ExtendedClause[],
+        options?: SendTxOptions | undefined
+    ): Promise<SendTxResponse> => {
+        const gas = await this.client.gas.estimateGas(clauses, this.address);
+
+        const txBody = await this.client.transactions.buildTransactionBody(
+            clauses,
+            gas.totalGas
+        );
+
+        if (options?.gas != null && options.gas < gas.totalGas) {
+            throw new Error('Insufficient gas');
+        }
+
+        const tx = new Transaction(txBody);
+
+        const signedTx = new Transaction(
+            tx.body,
+            secp256k1.sign(tx.getSignatureHash(), this.privateKey)
+        );
+
+        const sendTxRes =
+            await this.client.transactions.sendTransaction(signedTx);
+
+        return {
+            id: sendTxRes.id,
+            signer: this.address
+        };
+    };
+}
+
+export { PrivateKeySigner };
---
+++ packages/wallet/src/signers/types.d.ts
@@ -0,0 +1,58 @@
+import { type TransactionClause } from '@vechain/sdk-core';
+
+interface TransactionSender {
+    sendTransaction: (
+        clauses: ExtendedClause[],
+        options?: SendTxOptions
+    ) => Promise<SendTxResponse>;
+}
+
+/**
+ * Defines the extended clause for sending a transaction.
+ * @property comment - The comment for the transaction clause.
+ * @property abi - The ABI for the transaction clause.
+ */
+type ExtendedClause = TransactionClause & {
+    comment?: string;
+    abi?: object;
+};
+
+/**
+ * Defines the options for sending a transaction.
+ * @property signer - Request an account to send the transaction.
+ * @property gas - The gas limit for the transaction.
+ * @property dependsOn - The transaction hash that this transaction depends on.
+ * @property link - The callback URL for the transaction.
+ * @property comment - The comment for the transaction.
+ * @property delegator - The delegator for the transaction.
+ * @property onAccepted - The callback function that is called when the transaction is sent to the wallet.
+ */
+interface SendTxOptions {
+    signer?: string;
+    gas?: number;
+    dependsOn?: string;
+    link?: string;
+    comment?: string;
+    delegator?: {
+        url: string;
+        signer?: string;
+    };
+    onAccepted?: () => void;
+}
+
+/**
+ * Defines the response for sending a transaction.
+ * @property id - The transaction ID.
+ * @property signer - The signer for the transaction.
+ */
+interface SendTxResponse {
+    readonly id: string;
+    readonly signer: string;
+}
+
+export type {
+    TransactionSender,
+    SendTxResponse,
+    ExtendedClause,
+    SendTxOptions
+};
