6c942f7574b4f56792c38cd125cddeafef7d1eae
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-04-02 18:34:15 +0200

    feat: type for block input RPC and adapt getCorrectBlockNumberRPCToVechain

--- packages/provider/src/utils/const/blocks/blocks.ts
+++ packages/provider/src/utils/const/blocks/blocks.ts
@@ -1,15 +1,31 @@
+import { type BlockQuantityInputRPC } from '../../rpc-mapper';
+import { Hex0x } from '@vechain/sdk-core';
+
 /**
  * Get the correct block number for the given block number.
  *
- * @param blockNumber - The block number to get as a hex string or "latest" or "finalized".
+ * @param block - The block tag to get. 'latest' or 'earliest' or 'pending' or 'safe' or 'finalized' or an object: { blockNumber: number } or { blockHash: string }
  *
  * @note
  *  * Standard RPC method `eth_getBlockByNumber` support following block numbers: hex number of block, 'earliest', 'latest', 'safe', 'finalized', 'pending'. (@see https://ethereum.org/en/developers/docs/apis/json-rpc#default-block)
  *  * Currently, vechain only supports hex number of block, 'latest' and 'finalized'.
  */
-const getCorrectBlockNumberRPCToVechain = (blockNumber: string): string => {
-    if (blockNumber === 'latest') return 'best'; // 'best' is the alias for 'latest' in vechain Thorest
-    return blockNumber;
+const getCorrectBlockNumberRPCToVechain = (
+    block: BlockQuantityInputRPC
+): string => {
+    // Tag block number
+    if (typeof block === 'string') {
+        if (block === 'latest') return 'best'; // 'best' is the alias for 'latest' in vechain Thorest
+        return block;
+    }
+
+    // Object with block number
+    if (block.blockNumber !== undefined) {
+        return Hex0x.of(block.blockNumber);
+    }
+
+    // Object with block hash - Default case
+    return block.blockHash;
 };
 
 export { getCorrectBlockNumberRPCToVechain };
