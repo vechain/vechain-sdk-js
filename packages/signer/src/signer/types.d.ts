ac7cfa4b07ee1bb00fc9bdd656ab4df9cdf35a29
fabiorigam <fabio.rigamonti@icloud.com>
2024-04-02 11:34:18 +0200

    fix: update vechain signer

--- packages/signer/src/signer/types.d.ts
+++ packages/signer/src/signer/types.d.ts
@@ -4,11 +4,23 @@ import { type vechain_sdk_core_ethers } from '../../../core/src';
  * A signer for vechain, adding specific methods for vechain to the ethers signer
  */
 interface VechainSigner extends vechain_sdk_core_ethers.Signer {
+    // The delegator attached to this Signer
+    delegator: Delegator;
+
     /**
      * Sign a transaction with the delegator
      * @returns the fully signed transaction
      */
-    signWithDelegator: () => Promise<string>;
+    signWithDelegator: (tx: TransactionRequest, delegator) => Promise<string>;
+
+    /**
+     * Send a transaction with the delegator
+     * @returns the transaction response
+     */
+    sendWithDelegator: (
+        tx: TransactionRequest,
+        delegator
+    ) => Promise<TransactionResponse>;
 }
 
 export type { VechainSigner };
