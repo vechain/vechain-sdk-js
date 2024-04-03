80e718cbd4a19df5160971168ebcc030eba16f36
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-04-03 09:32:56 +0200

    Add signer package (#762)
    * fix: create package signer
    
    * Reduce errors (#734)
    
    * fix: improve code after intellij scan on package errors
    
    * fix: remove unused JSONRPC.PARSE_ERROR
    
    * fix: remove unused JSONRPC.METHOD_NOT_FOUND
    
    * fix: make class VechainSigner abstract
    
    * fix: redefine vechain signer
    
    * fix: update vechain signer
    
    * fix: improve vechain signer interface
    
    * fix: improve comments and add provider to VechainSigner

---
+++ packages/signer/tests/vechain-signer.unit.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from '@jest/globals';
+
+/**
+ * Unit test for VechainSigner class.
+ *
+ * @group unit/signer
+ */
+describe('Vechain Signer tests', () => {
+    test('signer tests', () => {
+        expect(true).toBeTruthy();
+    });
+});
