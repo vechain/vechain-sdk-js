b05f7a5ae6008b751ac6157e41e4391bd29b1a9f
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-04-03 09:10:45 +0200

    fix: specify compatibility of tags

--- packages/provider/src/utils/const/blocks/blocks.ts
+++ packages/provider/src/utils/const/blocks/blocks.ts
@@ -4,11 +4,13 @@ import { Hex0x } from '@vechain/sdk-core';
 /**
  * Get the correct block number for the given block number.
  *
- * @param block - The block tag to get. 'latest' or 'earliest' or 'pending' or 'safe' or 'finalized' or an object: { blockNumber: number } or { blockHash: string }
+ * @param block - The block tag to get.
+ * 'latest' or 'earliest' or 'pending' or 'safe' or 'finalized'
+ * or an object: { blockNumber: number } or { blockHash: string }
  *
  * @note
- *  * Standard RPC method `eth_getBlockByNumber` support following block numbers: hex number of block, 'earliest', 'latest', 'safe', 'finalized', 'pending'. (@see https://ethereum.org/en/developers/docs/apis/json-rpc#default-block)
- *  * Currently, vechain only supports hex number of block, 'latest' and 'finalized'.
+ *  * Currently VechainThor supports 'earliest', 'latest' and 'finalized' as block tags.
+ *  So 'pending' and 'safe' are converted to 'best' which is the alias for 'latest' and 'finalized' in VechainThor.
  */
 const getCorrectBlockNumberRPCToVechain = (
     block: BlockQuantityInputRPC
