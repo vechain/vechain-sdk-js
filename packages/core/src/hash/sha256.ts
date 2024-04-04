a98212c5ff30be909a51068b930d9ff8661951ea
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-04-04 10:47:57 +0100

    refactor: #712 sha256.ts secured

--- packages/core/src/hash/sha256.ts
+++ packages/core/src/hash/sha256.ts
@@ -37,14 +37,15 @@ function sha256(data: string | Uint8Array, returnType: 'hex'): string;
 /* --- Overloaded functions end --- */
 
 /**
- * Computes the sha256 hash of the given data.
- * Returns the hash as a Buffer or hex string, depending on the returnType.
- * If no returnType is provided, the hash is returned as a Buffer.
+ * Calculates the SHA-256 hash of the given data.
  *
- * @throws{InvalidDataReturnTypeError}
- * @param data - The input data (either a Buffer or string) for which the hash needs to be computed.
- * @param returnType - The type of the return value. Either 'buffer' or 'hex'. Defaults to 'buffer'.
- * @returns A Buffer or string representing the 256-bit sha256 hash.
+ * Secure audit function.
+ * * [_sha256](https://github.com/paulmillr/noble-hashes?tab=readme-ov-file#sha2-sha256-sha384-sha512-sha512_256)
+ *
+ * @param {string | Uint8Array} data - The data to be hashed. It can be either a string or a Uint8Array.
+ * @param {ReturnType} returnType - The return type. Default is 'buffer'. Valid options are 'buffer' or 'string'.
+ * @returns {Buffer | string} - The SHA-256 hash value. If the returnType is 'buffer', it returns a Buffer object. If the returnType is 'string', it returns a hexadecimal string representation
+ * of the hash.
  */
 function sha256(
     data: string | Uint8Array,
