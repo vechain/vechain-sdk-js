74cccf7899993496376312f8e88e9a5c32c9340e
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-04-09 10:53:15 +0200

    chore: comment

--- packages/provider/src/providers/ethers-provider/types.d.ts
+++ packages/provider/src/providers/ethers-provider/types.d.ts
@@ -1,5 +1,8 @@
 import { type vechain_sdk_core_ethers } from '@vechain/sdk-core';
 
+/**
+ * Override the JsonRpcPayload, JsonRpcResult and JsonRpcError types from ethers.
+ */
 type JsonRpcPayload = vechain_sdk_core_ethers.JsonRpcPayload;
 type JsonRpcResult = vechain_sdk_core_ethers.JsonRpcResult;
 type JsonRpcError = vechain_sdk_core_ethers.JsonRpcError;
