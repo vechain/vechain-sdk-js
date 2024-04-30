77bcf8f4c9724fbf73a661cedd55d8f940d61f0d
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-04-30 09:29:12 +0100

    refactor: #666:  keystore encrypt wip

--- packages/core/src/keystore/keystore.ts
+++ packages/core/src/keystore/keystore.ts
@@ -8,12 +8,27 @@ import { secp256k1 } from '../secp256k1';
 import {
     type EncryptOptions,
     type Keystore,
-    type KeystoreAccount, ScryptParams
+    type KeystoreAccount,
+    type ScryptParams
 } from './types';
 
-import { ethers, getBytes } from 'ethers';
-import { getPassword } from 'ethers/lib.esm/wallet/utils';
+import {
+    assertArgument,
+    concat,
+    defaultPath,
+    ethers,
+    getBytes,
+    getBytesCopy,
+    hexlify,
+    keccak256,
+    scrypt,
+    toUtf8Bytes,
+    uuidV4
+} from 'ethers';
+
 import { randomBytes } from '@noble/hashes/utils';
+
+import { CTR } from 'aes-js';
 
 /**
  * Encrypts a given private key into a keystore format using the specified password.
@@ -43,8 +58,9 @@ async function encrypt(
         }
     };
 
-    // Stirng version of keystore
-    const keystoreJsonString = await ethers.encryptKeystoreJson(
+    // String version of keystore
+    // const keystoreJsonString = await ethers.encryptKeystoreJson(
+    const keystoreJsonString = await _encryptKeystoreJson(
         keystoreAccount,
         password,
         encryptOptions
@@ -53,49 +69,205 @@ async function encrypt(
     return JSON.parse(keystoreJsonString) as Keystore;
 }
 
-// async function _encryptKeystoreJson(
-//     account: KeystoreAccount,
-//     password: string | Uint8Array,
-//     options?: EncryptOptions
-// ): Promise<string> {
-//     if (options == null) {
-//         options = {};
-//     }
-//
-//     const passwordBytes = getPassword(password);
-//     const kdf = getEncryptKdfParams(options);
-//     const key = await scrypt(
-//         passwordBytes,
-//         kdf.salt,
-//         kdf.N,
-//         kdf.r,
-//         kdf.p,
-//         64,
-//         options.progressCallback
-//     );
-//     return encryptKeystore(getBytes(key), kdf, account, options);
-// }
+async function _encryptKeystoreJson(
+    account: KeystoreAccount,
+    password: string | Uint8Array,
+    options?: EncryptOptions
+): Promise<string> {
+    if (options == null) {
+        options = {};
+    }
 
-// function getEncryptKdfParams(options: EncryptOptions): ScryptParams {
-//     // Check/generate the salt
-//     const salt =
-//         options.salt != null
-//             ? getBytes(options.salt, 'options.salt')
-//             : randomBytes(32);
-//
-//     // Override the scrypt password-based key derivation function parameters
-//     let N = (1 << 17), r = 8, p = 1;
-//     if (options.scrypt) {
-//         if (options.scrypt.N) { N = options.scrypt.N; }
-//         if (options.scrypt.r) { r = options.scrypt.r; }
-//         if (options.scrypt.p) { p = options.scrypt.p; }
-//     }
-//     assertArgument(typeof(N) === "number" && N > 0 && Number.isSafeInteger(N) && (BigInt(N) & BigInt(N - 1)) === BigInt(0), "invalid scrypt N parameter", "options.N", N);
-//     assertArgument(typeof(r) === "number" && r > 0 && Number.isSafeInteger(r), "invalid scrypt r parameter", "options.r", r);
-//     assertArgument(typeof(p) === "number" && p > 0 && Number.isSafeInteger(p), "invalid scrypt p parameter", "options.p", p);
-//
-//     return { name: "scrypt", dkLen: 32, salt, N, r, p };
-// }
+    const passwordBytes = _getPassword(password);
+    const kdf = getEncryptKdfParams(options);
+    const key = await scrypt(
+        passwordBytes,
+        kdf.salt,
+        kdf.n,
+        kdf.r,
+        kdf.p,
+        64,
+        options.progressCallback
+    );
+    return _encryptKeystore(getBytes(key), kdf, account, options);
+}
+
+/**
+ *  The current version of Ethers.
+ */
+export const version: string = '6.11.1';
+
+function _encryptKeystore(
+    key: Uint8Array,
+    kdf: ScryptParams,
+    account: KeystoreAccount,
+    options: EncryptOptions
+): string {
+    const privateKey = getBytes(account.privateKey, 'privateKey');
+
+    // Override initialization vector
+    const iv =
+        options.iv != null
+            ? getBytes(options.iv, 'options.iv')
+            : randomBytes(16);
+    assertArgument(
+        iv.length === 16,
+        'invalid options.iv length',
+        'options.iv',
+        options.iv
+    );
+
+    // Override the uuid
+    const uuidRandom =
+        options.uuid != null
+            ? getBytes(options.uuid, 'options.uuid')
+            : randomBytes(16);
+    assertArgument(
+        uuidRandom.length === 16,
+        'invalid options.uuid length',
+        'options.uuid',
+        options.iv
+    );
+
+    // This will be used to encrypt the wallet (as per Web3 secret storage)
+    // - 32 bytes   As normal for the Web3 secret storage (derivedKey, macPrefix)
+    // - 32 bytes   AES key to encrypt mnemonic with (required here to be Ethers Wallet)
+    const derivedKey = key.slice(0, 16);
+    const macPrefix = key.slice(16, 32);
+
+    // Encrypt the private key
+    const aesCtr = new CTR(derivedKey, iv);
+    const ciphertext = getBytes(aesCtr.encrypt(privateKey));
+
+    // Compute the message authentication code, used to check the password
+    const mac = keccak256(concat([macPrefix, ciphertext]));
+
+    // See: https://github.com/ethereum/wiki/wiki/Web3-Secret-Storage-Definition
+    const data: Record<string, unknown> = {
+        address: account.address.substring(2).toLowerCase(),
+        id: uuidV4(uuidRandom),
+        version: 3,
+        Crypto: {
+            cipher: 'aes-128-ctr',
+            cipherparams: {
+                iv: hexlify(iv).substring(2)
+            },
+            ciphertext: hexlify(ciphertext).substring(2),
+            kdf: 'scrypt',
+            kdfparams: {
+                salt: hexlify(kdf.salt).substring(2),
+                n: kdf.n,
+                dklen: 32,
+                p: kdf.p,
+                r: kdf.r
+            },
+            mac: mac.substring(2)
+        }
+    };
+
+    // If we have a mnemonic, encrypt it into the JSON wallet
+    if (account.mnemonic != null) {
+        const client = options.client ?? `ethers/${version}`;
+
+        const path = account.mnemonic.path ?? defaultPath;
+        const locale = account.mnemonic.locale ?? 'en';
+
+        // const path = account.mnemonic.path || defaultPath;
+        // const locale = account.mnemonic.locale || 'en';
+
+        const mnemonicKey = key.slice(32, 64);
+
+        const entropy = getBytes(
+            account.mnemonic.entropy,
+            'account.mnemonic.entropy'
+        );
+        const mnemonicIv = randomBytes(16);
+        const mnemonicAesCtr = new CTR(mnemonicKey, mnemonicIv);
+        const mnemonicCiphertext = getBytes(mnemonicAesCtr.encrypt(entropy));
+
+        const now = new Date();
+        const timestamp =
+            now.getUTCFullYear() +
+            '-' +
+            _zpad(now.getUTCMonth() + 1, 2) +
+            '-' +
+            _zpad(now.getUTCDate(), 2) +
+            'T' +
+            _zpad(now.getUTCHours(), 2) +
+            '-' +
+            _zpad(now.getUTCMinutes(), 2) +
+            '-' +
+            _zpad(now.getUTCSeconds(), 2) +
+            '.0Z';
+
+        const gethFilename = `UTC--${timestamp}--${data.address as string}`;
+        data['x-ethers'] = {
+            client,
+            gethFilename,
+            path,
+            locale,
+            mnemonicCounter: hexlify(mnemonicIv).substring(2),
+            mnemonicCiphertext: hexlify(mnemonicCiphertext).substring(2),
+            version: '0.1'
+        };
+    }
+
+    return JSON.stringify(data);
+}
+
+function getEncryptKdfParams(options: EncryptOptions): ScryptParams {
+    // Check/generate the salt
+    const salt =
+        options.salt != null
+            ? getBytes(options.salt, 'options.salt')
+            : randomBytes(32);
+
+    // Override the scrypt password-based key derivation function parameters
+    let N = 1 << 17;
+    let r = 8;
+    let p = 1;
+    if (options.scrypt != null) {
+        if (options.scrypt.N != null) {
+            N = options.scrypt.N;
+        }
+        if (options.scrypt.r != null) {
+            r = options.scrypt.r;
+        }
+        if (options.scrypt.p != null) {
+            p = options.scrypt.p;
+        }
+    }
+    assertArgument(
+        typeof N === 'number' &&
+            N > 0 &&
+            Number.isSafeInteger(N) &&
+            (BigInt(N) & BigInt(N - 1)) === BigInt(0),
+        'invalid scrypt N parameter',
+        'options.N',
+        N
+    );
+    assertArgument(
+        typeof r === 'number' && r > 0 && Number.isSafeInteger(r),
+        'invalid scrypt r parameter',
+        'options.r',
+        r
+    );
+    assertArgument(
+        typeof p === 'number' && p > 0 && Number.isSafeInteger(p),
+        'invalid scrypt p parameter',
+        'options.p',
+        p
+    );
+    return { dklen: 32, n: N, p, r, salt };
+    // return { name: 'scrypt', dkLen: 32, salt, N, r, p };
+}
+
+function _getPassword(password: string | Uint8Array): Uint8Array {
+    if (typeof password === 'string') {
+        return toUtf8Bytes(password, 'NFKC');
+    }
+    return getBytesCopy(password);
+}
 
 /**
  * Decrypts a keystore to obtain the private key using the given password.
@@ -154,6 +326,14 @@ function isValid(keystore: Keystore): boolean {
         }
     } catch (error) {} // Return false if parsing fails.
     return false;
+}
+
+function _zpad(value: string | number, length: number): string {
+    value = String(value);
+    while (value.length < length) {
+        value = '0' + value;
+    }
+    return value;
 }
 
 /**
