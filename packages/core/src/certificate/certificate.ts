4c03ec1548318d969a97f16e425cf567c8db6c53
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-05-13 11:30:16 +0100

    refactor: 809 pure and secure certificate.ts

--- packages/core/src/certificate/certificate.ts
+++ packages/core/src/certificate/certificate.ts
@@ -1,16 +1,17 @@
+import * as utils from '@noble/curves/abstract/utils';
+import fastJsonStableStringify from 'fast-json-stable-stringify';
+import { Hex, Hex0x } from '../utils';
 import { addressUtils } from '../address';
+import { assert, CERTIFICATE } from '@vechain/sdk-errors';
 import { blake2b256 } from '../hash';
 import { secp256k1 } from '../secp256k1';
-import fastJsonStableStringify from 'fast-json-stable-stringify';
-import { Buffer } from 'buffer';
-import { Hex0x } from '../utils';
 import { type Certificate } from './types';
-import { assert, CERTIFICATE } from '@vechain/sdk-errors';
 
 /**
- * Deterministically encodes a certificate into a JSON string.
- * @param cert - The certificate object to be encoded.
- * @returns A JSON string representation of the certificate.
+ * Encodes a certificate object to a JSON string.
+ *
+ * @param {Certificate} cert - The certificate object to encode.
+ * @return {string} - The encoded JSON string.
  */
 function encode(cert: Certificate): string {
     return fastJsonStableStringify({
@@ -23,42 +24,48 @@ function encode(cert: Certificate): string {
 /**
  * Verifies the validity of a certificate.
  *
- * @throws {CertificateNotSignedError, CertificateInvalidSignerError, CertificateInvalidSignatureFormatError}
- * @param cert - The certificate object with a digital signature.
+ * Secure audit function.
+ * - {@link blake2b256}.
+ *
+ * @param {Certificate} cert - The certificate to verify.
+ *
+ * @returns {void} - No return value.
+ *
+ * @throws CertificateInvalidSignatureFormatError - If  the certificate signature's is not a valid hexadecimal expression prefixed with `0x`.
+ * @throws CertificateNotSignedError - If the certificate is not signed.
+ * @throws CertificateInvalidSignerError - If the certificate's signature's doesn't match with the signer;s public key.
  */
 function verify(cert: Certificate): void {
-    // No signature
+    // No signature.
     assert(
-        'verify',
+        'certificate.verify',
         cert.signature !== undefined && cert.signature !== null,
         CERTIFICATE.CERTIFICATE_NOT_SIGNED,
-        "Verification failed: Certificate's signature is missing.",
+        "Verification failed: certificate's signature is missing.",
         { cert }
     );
-
-    // Invalid signature
+    // Invalid hexadecimal as signature.
     assert(
-        'verify',
+        'certificate.verify',
         Hex0x.isValid(cert.signature as string, false, true),
         CERTIFICATE.CERTIFICATE_INVALID_SIGNATURE_FORMAT,
-        'Verification failed: Signature format is invalid.',
+        'Verification failed: signature format is invalid.',
         { cert }
     );
-
-    // Encode the certificate without the signature and get signing hash
+    // Encode the certificate without the signature and get signing hash.
     const encoded = encode({ ...cert, signature: undefined });
     const signingHash = blake2b256(encoded);
     const pubKey = secp256k1.recover(
-        Buffer.from(signingHash),
-        Buffer.from((cert.signature as string).slice(2), 'hex')
+        signingHash,
+        utils.hexToBytes(Hex.canon(cert.signature as string))
     );
-
-    // Signature does not match with the signer's public key
+    // Signature does not match with the signer's public key.
     assert(
-        'verify',
-        addressUtils.fromPublicKey(Buffer.from(pubKey)) === cert.signer,
+        'certificate.verify',
+        addressUtils.fromPublicKey(pubKey).toLowerCase() ===
+            cert.signer.toLowerCase(),
         CERTIFICATE.CERTIFICATE_INVALID_SIGNER,
-        "Verification failed: Signature does not correspond to the signer's public key.",
+        "Verification failed: signature does not correspond to the signer's public key.",
         { pubKey, cert }
     );
 }
