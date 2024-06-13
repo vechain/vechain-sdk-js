8e518060cb0cd71018b0817b9b893017a6d981c3
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-06-13 17:45:51 +0100

    fix: 943 certificate.ts `verify` is thor-devkit back-compatible, `sign`introduced

--- packages/core/tests/certificate/certificate.unit.test.ts
+++ packages/core/tests/certificate/certificate.unit.test.ts
@@ -1,11 +1,31 @@
+import * as utils from '@noble/curves/abstract/utils';
 import { describe, expect, test } from '@jest/globals';
-import { certificate } from '../../src';
-import { cert1, cert2, invalidSignature, sig1, sig2 } from './fixture';
+import { certificate, type Certificate } from '../../src';
+import { cert, certPrivateKey } from './fixture';
+import { privateKey } from '../secp256k1/fixture';
 import {
     CertificateInvalidSignatureFormatError,
     CertificateInvalidSignerError,
-    CertificateNotSignedError
+    CertificateNotSignedError,
+    InvalidSecp256k1PrivateKeyError
 } from '@vechain/sdk-errors';
+// The tdk prefix is for thor-devkit.
+// import {
+//     Certificate as tdk_certificate,
+//     blake2b256 as tdk_blake2b256,
+//     secp256k1 as tdk_secp256k1
+// } from 'thor-devkit';
+
+function isEqualEnough(cert: Certificate, other: Certificate): boolean {
+    return (
+        cert.purpose === other.purpose &&
+        cert.payload.type === other.payload.type &&
+        cert.payload.content === other.payload.content &&
+        cert.domain === other.domain &&
+        cert.timestamp === other.timestamp &&
+        cert.signer.toLowerCase() === other.signer.toLowerCase()
+    );
+}
 
 /**
  * Unit tests for the Certificate module.
@@ -13,95 +33,136 @@ import {
  * @group unit/certificate
  */
 describe('certificate', () => {
-    describe('encode', () => {
-        test('consistent between two certificates - before signature', () => {
-            expect(certificate.encode(cert1)).toStrictEqual(
-                certificate.encode(cert2)
+    describe('sign', () => {
+        // test('compatibility - thor-devkit - compatible', () => {
+        //     // thor-dev-kit doesn't support UTF8 NFC encoding.
+        //     const tdkCompatibleCert = {
+        //         ...cert,
+        //         payload: { ...cert.payload, content: 'fyi' }
+        //     };
+        //     const tdkSignature = Hex0x.of(
+        //         tdk_secp256k1.sign(
+        //             tdk_blake2b256(tdk_certificate.encode(tdkCompatibleCert)),
+        //             Buffer.from(certPrivateKey)
+        //         )
+        //     );
+        //     expect(tdkSignature).toEqual(
+        //         certificate.sign(tdkCompatibleCert, certPrivateKey).signature
+        //     );
+        // });
+
+        // test('compatibility - thor-dev-kit - not compatible because UTF8 normalization not enforced ', () => {
+        //     // thor-dev-kit doesn't support UTF8 NFC encoding for
+        //     const tdkSignature = Hex0x.of(
+        //         tdk_secp256k1.sign(
+        //             tdk_blake2b256(tdk_certificate.encode(cert)),
+        //             Buffer.from(certPrivateKey)
+        //         )
+        //     );
+        //     expect(tdkSignature).not.toEqual(
+        //         certificate.sign(cert, certPrivateKey).signature
+        //     );
+        // });
+
+        test('invalid - illegal private key', () => {
+            expect(() => {
+                certificate.sign(cert, utils.hexToBytes('c0ffeec1b8'));
+            }).toThrowError(InvalidSecp256k1PrivateKeyError);
+        });
+
+        test('valid - challenge signer case sensitivity', () => {
+            const hCert = { ...cert, signer: cert.signer.toUpperCase() };
+            const lCert = { ...cert, signer: cert.signer.toLowerCase() };
+            const signedCert = certificate.sign(cert, privateKey);
+            const hSignedCert = certificate.sign(hCert, privateKey);
+            const lSignedCert = certificate.sign(lCert, privateKey);
+            expect(signedCert.signature).toEqual(hSignedCert.signature);
+            expect(signedCert.signature).toEqual(lSignedCert.signature);
+        });
+
+        test('valid - challenge utf8 normalization', () => {
+            const signedCert = certificate.sign(cert, certPrivateKey);
+            expect(isEqualEnough(cert, signedCert)).toBeTruthy();
+            expect(signedCert.signature).toBeDefined();
+        });
+
+        test('valid - challenge interface extension', () => {
+            const extendedCert = {
+                ...cert,
+                extended: 'property',
+                signature: '0x00'
+            };
+            const signedCert = certificate.sign(cert, certPrivateKey);
+            const signedExtendedCertificate = certificate.sign(
+                extendedCert,
+                certPrivateKey
             );
-        });
-
-        test('consistent between two certificates - after signature', () => {
-            expect(certificate.encode({ ...cert1, signature: sig1 })).toEqual(
-                certificate.encode({
-                    ...cert2,
-                    signature: sig2
-                })
+            expect(isEqualEnough(cert, extendedCert)).toBeTruthy();
+            expect(isEqualEnough(cert, signedExtendedCertificate)).toBeTruthy();
+            expect(signedCert.signature).toEqual(
+                signedExtendedCertificate.signature
             );
-        });
-    });
-
-    describe('match', () => {
-        const cert = new TextEncoder().encode(
-            certificate
-                .encode({ ...cert1, signature: undefined })
-                .normalize('NFC')
-        );
-
-        test('valid - because signature', () => {
-            expect(() => {
-                certificate.match(cert, cert1.signer, sig1);
-            }).not.toThrowError();
-        });
-
-        test('valid - because signature - uppercase', () => {
-            expect(() => {
-                certificate.match(cert, cert1.signer, sig1.toUpperCase());
-            }).not.toThrowError();
-        });
-
-        test('invalid - because signer address', () => {
-            expect(() => {
-                certificate.match(cert, '0x', sig1);
-            }).toThrowError(CertificateInvalidSignerError);
-        });
-
-        test('invalid - because invalid signature format', () => {
-            expect(() => {
-                certificate.match(cert, cert1.signer, invalidSignature);
-            }).toThrowError(CertificateInvalidSignatureFormatError);
         });
     });
 
     describe('verify', () => {
-        test('valid - because signature', () => {
-            expect(() => {
-                certificate.verify({ ...cert1, signature: sig1 });
-            }).not.toThrowError();
-        });
+        // test('compatibility - thor-devkit - compatible', () => {
+        //     const tdkCompatibleCert = {
+        //         ...cert,
+        //         payload: { ...cert.payload, content: 'fyi' }
+        //     };
+        //     const tdkSignature = Hex0x.of(
+        //         tdk_secp256k1.sign(
+        //             tdk_blake2b256(tdk_certificate.encode(tdkCompatibleCert)),
+        //             Buffer.from(certPrivateKey)
+        //         )
+        //     );
+        //     const tdkSignedCert = {
+        //         ...tdkCompatibleCert,
+        //         signature: tdkSignature
+        //     };
+        //     expect(() => {
+        //         certificate.verify(tdkSignedCert);
+        //     }).not.toThrowError();
+        // });
 
-        test('valid - because signature - uppercase', () => {
+        test('invalid - illegal signer address', () => {
+            const signedCert = certificate.sign(cert, certPrivateKey);
+            const invalidCert = {
+                ...cert,
+                signer: '0xC1b8C0fFeE',
+                signature: signedCert.signature
+            };
             expect(() => {
-                certificate.verify({
-                    ...cert1,
-                    signature: sig1.toUpperCase()
-                });
-            }).not.toThrowError();
-        });
-
-        test('invalid - because signer address', () => {
-            expect(() => {
-                certificate.verify({
-                    ...cert1,
-                    signature: sig1,
-                    signer: '0x'
-                });
+                certificate.verify(invalidCert);
             }).toThrowError(CertificateInvalidSignerError);
         });
 
-        test('invalid - because missing signature', () => {
+        test('invalid - because invalid signature format', () => {
+            const invalidCert = {
+                ...cert,
+                signature: 'C0fFeE'
+            };
             expect(() => {
-                certificate.verify({ ...cert1, signer: '0x' });
+                certificate.verify(invalidCert);
+            }).toThrowError(CertificateInvalidSignatureFormatError);
+        });
+
+        test('invalid - undefined signature', () => {
+            const invalidCert = {
+                ...cert,
+                signature: undefined
+            };
+            expect(() => {
+                certificate.verify(invalidCert);
             }).toThrowError(CertificateNotSignedError);
         });
 
-        test('invalid - because invalid signature format', () => {
+        test('valid - happy path', () => {
+            const signedCert = certificate.sign(cert, certPrivateKey);
             expect(() => {
-                certificate.verify({
-                    ...cert1,
-                    signature: invalidSignature,
-                    signer: '0x'
-                });
-            }).toThrowError(CertificateInvalidSignatureFormatError);
+                certificate.verify(signedCert);
+            }).not.toThrowError();
         });
     });
 });
