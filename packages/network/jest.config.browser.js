fbce697eb6520fb54a6df45a114472fe05fe0539
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-06-24 11:16:32 +0200

    fix: update jest env for ws

--- packages/network/jest.config.browser.js
+++ packages/network/jest.config.browser.js
@@ -2,6 +2,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: '../../customEnv.js',
+    setupFiles: ['./jest.browser-setup.js'],
     testPathIgnorePatterns: [
         'tests/utils/poll/event/event-poll.unit.test.ts',
         'tests/utils/http/http-client.solo.test.ts',
