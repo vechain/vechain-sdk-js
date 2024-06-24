cfae49d1e5cf6580abbf103b1afdefbdec11a2bb
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-06-24 10:59:08 +0200

    fix: refactor code and remove isomorphic-ws

--- packages/network/jest.browser-setup.js
+++
@@ -1,9 +0,0 @@
-// Jest configuration for mocking isomorphic-ws based on environment
-if (typeof window === 'undefined') {
-  // Running in Node.js environment
-  jest.mock('isomorphic-ws', () => require('ws'));
-} else {
-  // Running in browser environment
-  global.WebSocket = window.WebSocket;
-  jest.mock('isomorphic-ws', () => window.WebSocket);
-}
