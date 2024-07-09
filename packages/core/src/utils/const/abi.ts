f2634f24104ae7fa36dadb762093f0ceb969e3d4
Valazan <al-fede@hotmail.it>
2024-07-09 14:13:56 +0200

    feat: adding ERC1155 abi const

--- packages/core/src/utils/const/abi.ts
+++ packages/core/src/utils/const/abi.ts
@@ -719,6 +719,326 @@ const ERC721_ABI = [
 ] as const;
 
 /**
+ * ABI of the ERC1155 token standard.
+ *
+ * @see [EIP 1155](https://eips.ethereum.org/EIPS/eip-1155)
+ */
+const ERC1155_ABI = [
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'account',
+                type: 'address'
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'operator',
+                type: 'address'
+            },
+            {
+                indexed: false,
+                internalType: 'bool',
+                name: 'approved',
+                type: 'bool'
+            }
+        ],
+        name: 'ApprovalForAll',
+        type: 'event'
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'operator',
+                type: 'address'
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'from',
+                type: 'address'
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'to',
+                type: 'address'
+            },
+            {
+                indexed: false,
+                internalType: 'uint256[]',
+                name: 'ids',
+                type: 'uint256[]'
+            },
+            {
+                indexed: false,
+                internalType: 'uint256[]',
+                name: 'values',
+                type: 'uint256[]'
+            }
+        ],
+        name: 'TransferBatch',
+        type: 'event'
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'operator',
+                type: 'address'
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'from',
+                type: 'address'
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'to',
+                type: 'address'
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'id',
+                type: 'uint256'
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'value',
+                type: 'uint256'
+            }
+        ],
+        name: 'TransferSingle',
+        type: 'event'
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: false,
+                internalType: 'string',
+                name: 'value',
+                type: 'string'
+            },
+            {
+                indexed: true,
+                internalType: 'uint256',
+                name: 'id',
+                type: 'uint256'
+            }
+        ],
+        name: 'URI',
+        type: 'event'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: 'account',
+                type: 'address'
+            },
+            {
+                internalType: 'uint256',
+                name: 'id',
+                type: 'uint256'
+            }
+        ],
+        name: 'balanceOf',
+        outputs: [
+            {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256'
+            }
+        ],
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address[]',
+                name: 'accounts',
+                type: 'address[]'
+            },
+            {
+                internalType: 'uint256[]',
+                name: 'ids',
+                type: 'uint256[]'
+            }
+        ],
+        name: 'balanceOfBatch',
+        outputs: [
+            {
+                internalType: 'uint256[]',
+                name: '',
+                type: 'uint256[]'
+            }
+        ],
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: 'account',
+                type: 'address'
+            },
+            {
+                internalType: 'address',
+                name: 'operator',
+                type: 'address'
+            }
+        ],
+        name: 'isApprovedForAll',
+        outputs: [
+            {
+                internalType: 'bool',
+                name: '',
+                type: 'bool'
+            }
+        ],
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: 'from',
+                type: 'address'
+            },
+            {
+                internalType: 'address',
+                name: 'to',
+                type: 'address'
+            },
+            {
+                internalType: 'uint256[]',
+                name: 'ids',
+                type: 'uint256[]'
+            },
+            {
+                internalType: 'uint256[]',
+                name: 'amounts',
+                type: 'uint256[]'
+            },
+            {
+                internalType: 'bytes',
+                name: 'data',
+                type: 'bytes'
+            }
+        ],
+        name: 'safeBatchTransferFrom',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: 'from',
+                type: 'address'
+            },
+            {
+                internalType: 'address',
+                name: 'to',
+                type: 'address'
+            },
+            {
+                internalType: 'uint256',
+                name: 'id',
+                type: 'uint256'
+            },
+            {
+                internalType: 'uint256',
+                name: 'amount',
+                type: 'uint256'
+            },
+            {
+                internalType: 'bytes',
+                name: 'data',
+                type: 'bytes'
+            }
+        ],
+        name: 'safeTransferFrom',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: 'operator',
+                type: 'address'
+            },
+            {
+                internalType: 'bool',
+                name: 'approved',
+                type: 'bool'
+            }
+        ],
+        name: 'setApprovalForAll',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'bytes4',
+                name: 'interfaceId',
+                type: 'bytes4'
+            }
+        ],
+        name: 'supportsInterface',
+        outputs: [
+            {
+                internalType: 'bool',
+                name: '',
+                type: 'bool'
+            }
+        ],
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        inputs: [
+            {
+                internalType: 'uint256',
+                name: 'id',
+                type: 'uint256'
+            }
+        ],
+        name: 'uri',
+        outputs: [
+            {
+                internalType: 'string',
+                name: '',
+                type: 'string'
+            }
+        ],
+        stateMutability: 'view',
+        type: 'function'
+    }
+] as const;
+
+/**
  * ABI of the VIP180 token standard.
  *
  * @see [VIP 180](https://github.com/vechain/VIPs/blob/master/vips/VIP-180.md)
@@ -732,4 +1052,19 @@ const VIP180_ABI = ERC20_ABI;
  */
 const VIP181_ABI = ERC721_ABI;
 
-export { PARAMS_ABI, VIP180_ABI, VIP181_ABI, ERC20_ABI, ERC721_ABI };
+/**
+ * ABI of the VIP210 token standard.
+ *
+ * @see [VIP 210](https://github.com/vechain/VIPs/blob/master/vips/VIP-210.md)
+ */
+const VIP210_ABI = ERC1155_ABI;
+
+export {
+    PARAMS_ABI,
+    VIP180_ABI,
+    VIP181_ABI,
+    VIP210_ABI,
+    ERC20_ABI,
+    ERC721_ABI,
+    ERC1155_ABI
+};
