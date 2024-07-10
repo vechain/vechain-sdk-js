61402a3b88891cdd7072441b2bf3e980ba511a1f
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-07-10 11:45:06 +0100

    feat: class Revision documented

--- packages/core/src/utils/revision/experimental/Revision.ts
+++ packages/core/src/utils/revision/experimental/Revision.ts
@@ -1,9 +1,35 @@
 import { HEX } from '../../hex';
 import { assert, DATA } from '@vechain/sdk-errors';
 
+/**
+ * Represents a revision for a Thor transaction or block.
+ *
+ * @remark The string representation of the revision is always expressed as a number in base 10.
+ */
 class Revision extends String {
+    /**
+     * Regular expression pattern for revision strings.
+     * Revision strings can be one of the following:
+     * - "best": indicating the best revision
+     * - "finalized": indicating a finalized revision
+     * - A positive numeric string indicating a specific revision
+     *
+     * @type {RegExp}
+     */
     private static readonly REGEX_REVISION = /^(best|finalized|\d+)$/;
 
+    /**
+     * Determines if the given value is valid.
+     * This is true if the given value is
+     * - "best" string: indicating the best revision,
+     * - "finalized" string: indicating a finalized revision,
+     * - An {@link HEX} object,
+     * - A positive numeric string indicating a specific revision,
+     * - A positive number.
+     *
+     * @param {HEX | number | string} value - The value to be validated.
+     * @returns {boolean} - Returns `true` if the value is valid, `false` otherwise.
+     */
     public static isValid(value: HEX | number | string): boolean {
         switch (typeof value) {
             case 'number':
@@ -15,6 +41,16 @@ class Revision extends String {
         }
     }
 
+    /**
+     * Creates a new Revision object from the given value.
+     *
+     * @param {HEX | number | string} value - The value to create the Revision from.
+     * @throws {InvalidDataTypeError} if the given value is not a valid revision: see {@link isValid}.
+     *
+     * @returns {Revision} - The created Revision object.
+     *
+     * @remark The string representation of the revision is always expressed as a number in base 10.
+     */
     public static of(value: HEX | number | string): Revision {
         assert(
             'Revision.of',
