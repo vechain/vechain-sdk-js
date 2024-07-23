8fa07c51c1d92a5dc461f9caa7c61b2b8b894ba6
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-07-23 11:50:33 +0200

    fix: fix failing tests

--- packages/network/jest.browser-setup.js
+++ packages/network/jest.browser-setup.js
@@ -1,5 +1,10 @@
 require('whatwg-fetch');
 
+const fetchMock = require('jest-fetch-mock');
+
+// Don't auto-enable mocks
+fetchMock.dontMock();
+
 // Jest configuration for WebSocket mocking based on environment
 if (typeof window === 'undefined') {
   // Running in Node.js environment
