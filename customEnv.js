98f66da5ac27ba69d3728addfdec5dec3326a879
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-07-23 12:08:52 +0200

    fix: remove comments

--- customEnv.js
+++ customEnv.js
@@ -17,11 +17,6 @@ class CustomizedTestEnvironment extends TestEnvironment {
         this.global.TextEncoder = globalThis.TextEncoder;
         this.global.TextDecoder = globalThis.TextDecoder;
         this.global.Uint8Array = globalThis.Uint8Array;
-
-        // Add fetch to the global object
-        // if (!this.global.fetch) {
-        //     this.global.fetch = require('whatwg-fetch').fetch;
-        // }
     }
 }
 
