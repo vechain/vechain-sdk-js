6c54b55f4c2203a2d7e6c2de4d730b8c0c89c834
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-07-31 16:59:56 +0200

    fix: fix failing tests

--- packages/network/tests/provider/helpers/provider-internal-wallets/base-wallet/fixture.ts
+++ packages/network/tests/provider/helpers/provider-internal-wallets/base-wallet/fixture.ts
@@ -1,6 +1,7 @@
 // Generate 10 random accounts
 import { addressUtils, secp256k1 } from '@vechain/sdk-core';
 import { type ProviderInternalWalletAccount } from '../../../../../src';
+import { secp256k1 as n_secp256k1 } from '@noble/curves/secp256k1';
 
 /**
  * Fixture of ProviderInternalWalletAccount randomly generated.
@@ -8,13 +9,12 @@ import { type ProviderInternalWalletAccount } from '../../../../../src';
 const accountsFixture: ProviderInternalWalletAccount[] = Array.from(
     { length: 10 },
     () => {
-        const privateKey = await secp256k1.generatePrivateKey();
-        const privateKeyBuffer = Buffer.from(privateKey);
+        const privateKey = Buffer.from(n_secp256k1.utils.randomPrivateKey());
         const publicKey = Buffer.from(secp256k1.derivePublicKey(privateKey));
         const address = addressUtils.fromPublicKey(publicKey);
 
         return {
-            privateKey: privateKeyBuffer,
+            privateKey,
             publicKey,
             address
         } satisfies ProviderInternalWalletAccount;
