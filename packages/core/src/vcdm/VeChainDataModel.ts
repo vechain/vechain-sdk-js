ebaf0669bfef9f3cca1a3e71124b8b24e38bbd4d
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-08-01 16:48:57 +0100

    feat: 1103 **VeChain Data Model** introduction
    BREAKING CHANGE: `vcdm` packages collects the data model classes, interfaces and types

--- packages/core/src/vcdm/VeChainDataModel.ts
+++ packages/core/src/vcdm/VeChainDataModel.ts
@@ -5,13 +5,13 @@
 export interface VeChainDataModel<T> {
     // Properties.
     /**
-     * Return this object cast to a big integer value
+     * Return this instance cast to a big integer value
      * @throws InvalidCastType if this object can't cast to a big integer.
      */
     get bi(): bigint;
 
     /**
-     * Return this object cast to a buffer of bytes.
+     * Return this instance cast to a buffer of bytes.
      */
     get bytes(): Uint8Array;
 
@@ -23,11 +23,18 @@ export interface VeChainDataModel<T> {
 
     // Methods.
     /**
-     * Compare this object with `that` in a lexicographic meaningful way.
+     * Compare this instance with `that` in a lexicographic meaningful way.
      *
-     * @param that object to compare.
+     * @param {T} that object to compare.
      * @return a negative number if `this` < `that`, zero if `this` = `that`, a positive number if `this` > that`.
      */
     compareTo: (that: T) => number;
+
+    /**
+     * Checks if the given value is equal to the current instance.
+     *
+     * @param {T} that - The value to compare.
+     * @returns {boolean} - True if the values are equal, false otherwise.
+     */
     isEqual: (that: T) => boolean;
 }
