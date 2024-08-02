8b61f0f26c1921b09f3dc0559b1dac767fc8887a
leszek-vechain <leszek.lorek@vechain.org>
2024-08-02 09:58:32 +0100

    test: add test
    - works somehow but not with SDK

--- apps/sdk-cloudflare-integration/src/index.test.ts
+++ apps/sdk-cloudflare-integration/src/index.test.ts
@@ -1,26 +1,10 @@
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import { unstable_dev } from "wrangler";
-import type { UnstableDevWorker } from "wrangler";
+import { describe, expect, it } from 'vitest';
+import { SELF } from 'cloudflare:test';
+import '../src';
 
-describe("Worker", () => {
-	// let worker: UnstableDevWorker;
-
-	// beforeAll(async () => {
-	// 	worker = await unstable_dev("src/index.ts", {
-	// 		experimental: { disableExperimentalWarning: true },
-	// 	});
-	// });
-
-	// afterAll(async () => {
-	// 	await worker.stop();
-	// });
-
-	it("Should sign a transaction", async () => {
-		// const resp = await worker.fetch();
-		// if (resp) {
-		// 	const text = await resp.text();
-		// 	expect(text).toBeDefined();
-		// }
-		expect(1).toBe(1);
-	});
-});
+describe('Worker', () => {
+    it('should sign a transaction)', async () => {
+        const response = await SELF.fetch('http://example.com/?a=3&b=4');
+        expect(await response.text()).toBe('Hello World!');
+    });
+})
