9c6888d03fecfb78a20b12c2662fbf079de30f72
leszek-vechain <158060059+leszek-vechain@users.noreply.github.com>
2024-08-09 21:18:00 +0100

    add test for sdk-cloudflare-integration (#1154)
    * test: add test
    
    - works somehow but not with SDK
    
    * fix: fix failing test
    
    * fix: working tests
    
    * test: fixed test
    
    - it works with vite config
    
    ---------
    
    Signed-off-by: leszek-vechain <158060059+leszek-vechain@users.noreply.github.com>
    Co-authored-by: Fabio Rigamonti <fabio.rigamonti@icloud.com>

---
+++ apps/sdk-cloudflare-integration/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
+
+export default defineWorkersConfig({
+    test: {
+        poolOptions: {
+            workers: {
+                wrangler: { configPath: './wrangler.toml' }
+            }
+        },
+        server: {
+            deps: {
+                inline: ['crypto', 'ethers', '@vechain/sdk-core']
+            }
+        }
+    }
+});
