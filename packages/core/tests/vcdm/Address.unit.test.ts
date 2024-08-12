cdc7c5b7d1c2fef5d6585c5661c140d7aecbc13e
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2024-08-12 17:15:21 +0100

    feat: more changes, still tests to add

--- packages/core/tests/vcdm/Address.unit.test.ts
+++ packages/core/tests/vcdm/Address.unit.test.ts
@@ -9,12 +9,20 @@ import { Address } from '../../src';
 describe('Address class tests', () => {
     describe('Construction tests', () => {
         test('Return an Address instance if the passed argument is valid', () => {
-            const exp = '0xcfb79a9c950b78e14c43efa621ebcf9660dbe01f';
-            const address = Address.of(exp);
+            let exp = '0xcfb79a9c950b78e14c43efa621ebcf9660dbe01f';
+            let address = Address.of(exp);
+            expect(address).toBeInstanceOf(Address);
+            exp = '0xCfb79a9c950b78E14c43efa621ebcf9660dbe01F';
+            address = Address.of(exp);
+            expect(address).toBeInstanceOf(Address);
+            exp = '0xCFB79A9C950B78E14C43EFA621EBCF9660DBE01F';
+            address = Address.of(exp);
             expect(address).toBeInstanceOf(Address);
         });
-        test('Throw an error if the passed argument is negative', () => {
-            const exp = '-0xcaffee';
+        test('Throw an error if the passed argument is an invalid address', () => {
+            let exp = '-0xcaffee';
+            expect(() => Address.of(exp)).toThrow(InvalidDataType);
+            exp = '0xcaffee';
             expect(() => Address.of(exp)).toThrow(InvalidDataType);
         });
     });
