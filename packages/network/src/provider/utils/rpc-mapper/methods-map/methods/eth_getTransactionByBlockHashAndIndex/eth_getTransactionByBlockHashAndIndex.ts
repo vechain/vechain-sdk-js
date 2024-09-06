76c4b0e7c53fb88250813962d5c7bff53f42dafe
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-09-06 08:57:40 +0200

    feat: sketch implementation of eth_getTransactionByBlockHashAndIndex

--- packages/network/src/provider/utils/rpc-mapper/methods-map/methods/eth_getTransactionByBlockHashAndIndex/eth_getTransactionByBlockHashAndIndex.ts
+++ packages/network/src/provider/utils/rpc-mapper/methods-map/methods/eth_getTransactionByBlockHashAndIndex/eth_getTransactionByBlockHashAndIndex.ts
@@ -1,4 +1,16 @@
-import { VeChainSDKLogger } from '@vechain/sdk-logging';
+import { type ThorClient } from '../../../../../../thor-client';
+import {
+    JSONRPCInternalError,
+    JSONRPCInvalidParams,
+    stringifyData
+} from '@vechain/sdk-errors';
+import { RPC_DOCUMENTATION_URL } from '../../../../../../utils';
+import {
+    type TransactionRPC,
+    transactionsFormatter
+} from '../../../../formatter';
+import { RPCMethodsMap } from '../../../rpc-mapper';
+import { RPC_METHODS } from '../../../../const';
 
 /**
  * RPC Method eth_getTransactionByBlockHashAndIndex implementation
@@ -10,18 +22,50 @@ import { VeChainSDKLogger } from '@vechain/sdk-logging';
  * * params[1]: ...
  * * params[n]: ...
  */
-const ethGetTransactionByBlockHashAndIndex =
-    async (): Promise<'METHOD NOT IMPLEMENTED'> => {
-        // Not implemented yet
-        VeChainSDKLogger('warning').log({
-            title: 'eth_getTransactionByBlockHashAndIndex',
-            messages: [
-                'Method "eth_getTransactionByBlockHashAndIndex" has not been implemented yet.'
-            ]
+const ethGetTransactionByBlockHashAndIndex = async (
+    thorClient: ThorClient,
+    params: unknown[]
+): Promise<TransactionRPC | null> => {
+    if (
+        params.length !== 2 ||
+        typeof params[0] !== 'string' ||
+        typeof params[1] !== 'string'
+    )
+        throw new JSONRPCInvalidParams(
+            'eth_getTransactionByBlockHashAndIndex',
+            -32602,
+            `Invalid input params for "eth_getTransactionByBlockHashAndIndex" method. See ${RPC_DOCUMENTATION_URL} for details.`,
+            { params }
+        );
+
+    try {
+        const [hash, index] = params as [string, string];
+
+        // Get the VeChainThor transaction
+        const tx = await thorClient.transactions.getTransaction(hash, {
+            head: index
         });
 
-        // To avoid eslint error
-        return await Promise.resolve('METHOD NOT IMPLEMENTED');
-    };
+        if (tx === null) return null;
+
+        // Get the chain id
+        const chainId = (await RPCMethodsMap(thorClient)[
+            RPC_METHODS.eth_chainId
+        ]([])) as string;
+
+        return transactionsFormatter.formatToRPCStandard(tx, chainId, 1);
+    } catch (e) {
+        throw new JSONRPCInternalError(
+            'eth_getTransactionByBlockHashAndIndex()',
+            -32603,
+            'Method "eth_getTransactionByBlockHashAndIndex" failed.',
+            {
+                params: stringifyData(params),
+                url: thorClient.httpClient.baseURL,
+                innerError: stringifyData(e)
+            }
+        );
+    }
+};
 
 export { ethGetTransactionByBlockHashAndIndex };
--- packages/network/src/provider/utils/rpc-mapper/rpc-mapper.ts
+++ packages/network/src/provider/utils/rpc-mapper/rpc-mapper.ts
@@ -249,10 +249,14 @@ const RPCMethodsMap = (
                 return await ethGetBlockTransactionCountByNumber();
             },
 
-        [RPC_METHODS.eth_getTransactionByBlockHashAndIndex]:
-            async (): Promise<'METHOD NOT IMPLEMENTED'> => {
-                return await ethGetTransactionByBlockHashAndIndex();
-            },
+        [RPC_METHODS.eth_getTransactionByBlockHashAndIndex]: async (
+            params
+        ): Promise<TransactionRPC | null> => {
+            return await ethGetTransactionByBlockHashAndIndex(
+                thorClient,
+                params
+            );
+        },
 
         [RPC_METHODS.eth_getTransactionByBlockNumberAndIndex]:
             async (): Promise<'METHOD NOT IMPLEMENTED'> => {
