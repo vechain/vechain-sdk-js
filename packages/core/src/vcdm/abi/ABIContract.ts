7ae24ddf283fa75a23d2e3c9345539eed3759582
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2024-09-18 13:41:52 +0100

    feat: added method

--- packages/core/src/vcdm/abi/ABIContract.ts
+++ packages/core/src/vcdm/abi/ABIContract.ts
@@ -1,8 +1,13 @@
-import { getAbiItem, type Abi as ViemABI } from 'viem';
+import { InvalidAbiDataToEncodeOrDecode } from '@vechain/sdk-errors';
+import {
+    getAbiItem,
+    type DecodeFunctionDataReturnType,
+    type Abi as ViemABI
+} from 'viem';
+import { type Hex } from '../Hex';
 import { ABI } from './ABI';
 import { ABIFunction } from './ABIFunction';
 import { type ABIItemType } from './ABIItem';
-import { InvalidAbiDataToEncodeOrDecode } from '@vechain/sdk-errors';
 
 class ABIContract extends ABI {
     private readonly abi: ViemABI;
@@ -36,6 +41,35 @@ class ABIContract extends ABI {
                 'ABIContract.encodeFunctionInput()',
                 `Encoding failed: Data format is invalid. Function data does not match the expected format for ABI type encoding.`,
                 { functionName, functionData },
+                error
+            );
+        }
+    }
+
+    /**
+     * Decode the function data of an encoded function
+     * @param {string} functionName The name of the function defined in the ABI.
+     * @param {Hex} encodedFunctionInput The encoded function data.
+     * @returns {DecodeFunctionDataReturnType} an array of the decoded function data
+     * @throws {InvalidAbiDataToEncodeOrDecode}
+     */
+    public decodeFunctionInput(
+        functionName: string,
+        encodedFunctionInput: Hex
+    ): DecodeFunctionDataReturnType {
+        try {
+            const functionAbiItem = getAbiItem({
+                abi: this.abi,
+                name: functionName
+            });
+            const functionAbi = new ABIFunction(functionAbiItem as ABIItemType);
+
+            return functionAbi.decodeData(encodedFunctionInput);
+        } catch (error) {
+            throw new InvalidAbiDataToEncodeOrDecode(
+                'ABIContract.decodeFunctionInput()',
+                'Decoding failed: Data must be a valid hex string encoding a compliant ABI type.',
+                { functionName, encodedFunctionInput },
                 error
             );
         }
