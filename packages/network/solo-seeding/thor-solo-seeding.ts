7a6e3ababd61183ef9e542fcb9e7b8d9c46e3e6c
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2024-09-19 14:00:14 +0100

    feat: fixed some tests

--- packages/network/solo-seeding/thor-solo-seeding.ts
+++ packages/network/solo-seeding/thor-solo-seeding.ts
@@ -1,9 +1,9 @@
 import { expect } from '@jest/globals';
 import {
+    ABIContract,
     ABIFunction,
     ABIItem,
     clauseBuilder,
-    coder,
     networkInfo,
     Transaction,
     TransactionHandler,
@@ -30,10 +30,12 @@ import { TESTING_CONTRACT_BYTECODE } from './const';
 const CLAUSES_VTHO = THOR_SOLO_ACCOUNTS.slice(0, 10).map((account) => ({
     to: BUILT_IN_CONTRACTS.ENERGY_ADDRESS,
     value: 0,
-    data: coder.encodeFunctionInput(BUILT_IN_CONTRACTS.ENERGY_ABI, 'transfer', [
-        account.address,
-        Units.parseEther('500000000').bi
-    ])
+    data: ABIContract.ofAbi(BUILT_IN_CONTRACTS.ENERGY_ABI)
+        .encodeFunctionInput('transfer', [
+            account.address,
+            Units.parseEther('500000000').bi
+        ])
+        .toString()
 }));
 
 /**
