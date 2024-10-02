173b62186896b0ea2002679b91ff476456e73624
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-10-02 12:00:12 +0200

    fix: improve tests by creating account only once

--- packages/network/tests/thor-client/debug/debug-thorest.solo.test.ts
+++ packages/network/tests/thor-client/debug/debug-thorest.solo.test.ts
@@ -1,5 +1,10 @@
-import { beforeEach, describe, expect, test } from '@jest/globals';
-import { THOR_SOLO_URL, ThorClient, type TracerName } from '../../../src';
+import { describe, test } from '@jest/globals';
+import {
+    THOR_SOLO_URL,
+    ThorClient,
+    type TracerName,
+    type TransactionReceipt
+} from '../../../src';
 import {
     sendTransactionWithAccountIndex,
     traceContractCallTestnetFixture
@@ -14,12 +19,81 @@ import { transfer1VTHOClause } from '../transactions/fixture';
  * @group integration/clients/thor-client/debug-solo
  */
 describe('ThorClient - Debug Module - Solo', () => {
-    // ThorClient instance
+    /**
+     * ThorClient instance
+     */
     let thorSoloClient: ThorClient;
 
-    beforeEach(() => {
+    /**
+     * Transactions to debug.
+     *
+     * Before all tests, we send transactions and then
+     * we will use the debug endpoints to debug them.
+     */
+    const transactions: Array<{
+        tracerName: TracerName;
+        senderAccountIndex: number;
+        txReceipt?: TransactionReceipt | null;
+    }> = [];
+
+    /**
+     * Tracer-AccountsIndex list.
+     * This is necessary to associate the tracer name with the sender account index.
+     */
+    const tracersAccounts: Array<{
+        tracerName: TracerName;
+        senderAccountIndex: number;
+    }> = [
+        { tracerName: '', senderAccountIndex: 7 },
+        { tracerName: '4byte', senderAccountIndex: 8 },
+        { tracerName: 'call', senderAccountIndex: 9 },
+        { tracerName: 'noop', senderAccountIndex: 10 },
+        { tracerName: 'prestate', senderAccountIndex: 11 },
+        { tracerName: 'unigram', senderAccountIndex: 12 },
+        { tracerName: 'bigram', senderAccountIndex: 13 },
+        { tracerName: 'trigram', senderAccountIndex: 14 },
+        { tracerName: 'evmdis', senderAccountIndex: 15 },
+        { tracerName: 'opcount', senderAccountIndex: 16 },
+        { tracerName: null, senderAccountIndex: 17 }
+    ];
+
+    /**
+     * Get tracer name by account index
+     */
+    const getTracerNameByAccountIndex = (accountIndex: number): TracerName => {
+        return tracersAccounts.find(
+            (tracerAccount) => tracerAccount.senderAccountIndex === accountIndex
+        )?.tracerName as TracerName;
+    };
+
+    /**
+     * Get the transaction receipt by account index
+     */
+    const getTransactionReceiptByAccountIndex = (
+        accountIndex: number
+    ): TransactionReceipt | null => {
+        return transactions.find(
+            (transaction) => transaction.senderAccountIndex === accountIndex
+        )?.txReceipt as TransactionReceipt | null;
+    };
+
+    /**
+     * Make the transactions to debug before all tests
+     */
+    beforeAll(async () => {
         thorSoloClient = ThorClient.fromUrl(THOR_SOLO_URL);
-    });
+        for (const tracerAccount of tracersAccounts) {
+            const txReceipt = await sendTransactionWithAccountIndex(
+                tracerAccount.senderAccountIndex,
+                thorSoloClient
+            );
+            transactions.push({
+                tracerName: tracerAccount.tracerName,
+                senderAccountIndex: tracerAccount.senderAccountIndex,
+                txReceipt
+            });
+        }
+    }, 50000);
 
     /**
      * traceTransactionClause tests
@@ -28,27 +102,11 @@ describe('ThorClient - Debug Module - Solo', () => {
         /**
          * Test a result of a transaction clause for each kind of trace name
          */
-        (
-            [
-                { tracerName: '', senderAccountIndex: 10 },
-                { tracerName: '4byte', senderAccountIndex: 11 },
-                { tracerName: 'call', senderAccountIndex: 12 },
-                { tracerName: 'noop', senderAccountIndex: 13 },
-                { tracerName: 'prestate', senderAccountIndex: 14 },
-                { tracerName: 'unigram', senderAccountIndex: 15 },
-                { tracerName: 'bigram', senderAccountIndex: 16 },
-                { tracerName: 'trigram', senderAccountIndex: 17 },
-                { tracerName: 'evmdis', senderAccountIndex: 18 },
-                { tracerName: 'opcount', senderAccountIndex: 19 },
-                { tracerName: null, senderAccountIndex: 20 }
-            ] as Array<{ tracerName: TracerName; senderAccountIndex: number }>
-        ).forEach((testCase) => {
-            test(`traceTransactionClause - ${testCase.tracerName as string}`, async () => {
-                // Send a transaction in advance in order to debug it later
-                const txReceipt = await sendTransactionWithAccountIndex(
-                    testCase.senderAccountIndex,
-                    thorSoloClient
-                );
+        [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].forEach((accountIndex) => {
+            test(`traceTransactionClause - ${getTracerNameByAccountIndex(accountIndex)}`, async () => {
+                // Get the transaction receipt
+                const txReceipt =
+                    getTransactionReceiptByAccountIndex(accountIndex);
 
                 // Debug the transaction clause
                 const result =
@@ -61,7 +119,7 @@ describe('ThorClient - Debug Module - Solo', () => {
                             },
                             config: {}
                         },
-                        testCase.tracerName
+                        getTracerNameByAccountIndex(accountIndex)
                     );
                 expect(result).toBeDefined();
             }, 10000);
@@ -75,26 +133,11 @@ describe('ThorClient - Debug Module - Solo', () => {
         /**
          * Test a result of a contract call for each kind of trace name
          */
-        (
-            [
-                { tracerName: '', senderAccountIndex: 10 },
-                { tracerName: '4byte', senderAccountIndex: 11 },
-                { tracerName: 'call', senderAccountIndex: 12 },
-                { tracerName: 'noop', senderAccountIndex: 13 },
-                { tracerName: 'prestate', senderAccountIndex: 14 },
-                { tracerName: 'unigram', senderAccountIndex: 15 },
-                { tracerName: 'bigram', senderAccountIndex: 16 },
-                { tracerName: 'trigram', senderAccountIndex: 17 },
-                { tracerName: 'evmdis', senderAccountIndex: 18 },
-                { tracerName: 'opcount', senderAccountIndex: 19 }
-            ] as Array<{ tracerName: TracerName; senderAccountIndex: number }>
-        ).forEach((testCase) => {
-            test(`traceContractCall - ${testCase.tracerName as string}`, async () => {
-                // Send a transaction in advance in order to debug it later
-                const txReceipt = await sendTransactionWithAccountIndex(
-                    testCase.senderAccountIndex,
-                    thorSoloClient
-                );
+        [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].forEach((accountIndex) => {
+            test(`traceContractCall - ${getTracerNameByAccountIndex(accountIndex)}`, async () => {
+                // Get the transaction receipt
+                const txReceipt =
+                    getTransactionReceiptByAccountIndex(accountIndex);
 
                 // Debug the contract call
                 const result = await thorSoloClient.debug.traceContractCall(
@@ -110,10 +153,10 @@ describe('ThorClient - Debug Module - Solo', () => {
                         },
                         config: {}
                     },
-                    testCase.tracerName
+                    getTracerNameByAccountIndex(accountIndex)
                 );
                 expect(result).toBeDefined();
             });
-        });
+        }, 10000);
     });
 });
