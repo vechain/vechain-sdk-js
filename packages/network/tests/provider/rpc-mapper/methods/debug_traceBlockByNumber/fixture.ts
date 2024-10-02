0d59929d938bf98694da327f57ba408473d0a73e
Rodolfo Pietro Calabrò <33911400+rodolfopietro97@users.noreply.github.com>
2024-10-02 14:03:19 +0200

    feat: test all tracers with solo (#1374)
    * fix: remove other tracers in testnet
    
    * fix: improve fixture-thorest.ts
    
    * fix: traceContractCall
    
    * fix: improve tests by creating account only once
    
    * fix: doc examples
    
    * feat: add debug endpoint solo testing to RPC methods
    
    ---------
    
    Co-authored-by: Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>

--- packages/network/tests/provider/rpc-mapper/methods/debug_traceBlockByNumber/fixture.ts
+++ packages/network/tests/provider/rpc-mapper/methods/debug_traceBlockByNumber/fixture.ts
@@ -44,55 +44,6 @@ const debugTraceBlockByNumberFixture = [
             }
         ]
     }
-    // The commented out lines should be used as part of the test on solo {@link https://github.com/vechain/vechain-sdk-js/issues/1357}
-    // Prestate tracer
-    // {
-    //     input: {
-    //         params: [
-    //             HexInt.of(17727716).toString(),
-    //             {
-    //                 tracer: 'presStateTracer',
-    //                 tracerConfig: { onlyTopCall: true }
-    //             }
-    //         ]
-    //     },
-    //     expected: [
-    //         {
-    //             txHash: '0x7e7d7ec1510425fd6f68e3dd2369e57af44114fb95b5a26fade14621821c74f3',
-    //             result: {
-    //                 '0x6e1ffe60656421eb12de92433c5a319ba606bb81': {
-    //                     balance: '0x0',
-    //                     nonce: 0
-    //                 },
-    //                 '0x7487d912d03ab9de786278f679592b3730bdd540': {
-    //                     balance: '0x422ca8b0a00a425000000',
-    //                     nonce: 0
-    //                 },
-    //                 '0xb4094c25f86d628fdd571afc4077f0d0196afb48': {
-    //                     balance: '0x14b3cf4cc2f3044b700000',
-    //                     nonce: 0
-    //                 }
-    //             }
-    //         },
-    //         {
-    //             txHash: '0x5f1eb314b18a906e26f68a6b8a79fcea47edc65d1c30bccccdb5ff3f92f4acee',
-    //             result: {
-    //                 '0x6e1ffe60656421eb12de92433c5a319ba606bb81': {
-    //                     balance: '0x0',
-    //                     nonce: 0
-    //                 },
-    //                 '0x7487d912d03ab9de786278f679592b3730bdd540': {
-    //                     balance: '0x422ca8b0a00a425000000',
-    //                     nonce: 0
-    //                 },
-    //                 '0xb4094c25f86d628fdd571afc4077f0d0196afb48': {
-    //                     balance: '0x14b3cf4cc2f3044b700000',
-    //                     nonce: 0
-    //                 }
-    //             }
-    //         }
-    //     ]
-    // }
 ];
 
 export { debugTraceBlockByNumberFixture };
