7cb63a8e518bbdd213cafd8cfd336a5fb00da718
rodolfopietro97 <rodolfo@rodolfopietro.it>
2024-10-02 12:15:57 +0200

    fix: doc examples

--- docs/examples/debug/debug-trace-contract-call.ts
+++ docs/examples/debug/debug-trace-contract-call.ts
@@ -1,31 +1,31 @@
-// The commented out lines should be used as part of the test on solo {@link https://github.com/vechain/vechain-sdk-js/issues/1357}
-// import { TESTNET_URL, ThorClient } from '@vechain/sdk-network';
+import { TESTNET_URL, ThorClient } from '@vechain/sdk-network';
 
-// // START_SNIPPET: DebugTraceContractCallSnippet
+// START_SNIPPET: DebugTraceContractCallSnippet
 
-// // 1 - Create thor client for testnet
-// const thorClient = ThorClient.fromUrl(TESTNET_URL);
+// 1 - Create thor client for testnet
+const thorClient = ThorClient.fromUrl(TESTNET_URL);
 
-// // 2 - Trace the contract call.
-// const result = await thorClient.debug.traceContractCall(
-//     {
-//         contractInput: {
-//             to: '0x0000000000000000000000000000456E65726779',
-//             data: '0xa9059cbb0000000000000000000000000000000000000000000000000000456e65726779000000000000000000000000000000000000000000000004563918244f400000',
-//             value: '0x0'
-//         },
-//         transactionOptions: {
-//             caller: '0x625fCe8dd8E2C05e82e77847F3da06AF6e55A7AF',
-//             gasPayer: '0x625fCe8dd8E2C05e82e77847F3da06AF6e55A7AF',
-//             expiration: 18,
-//             blockRef: '0x0101d05409d55cce'
-//         },
-//         config: {}
-//     },
-//     null
-// );
+// 2 - Trace the contract call.
+const result = await thorClient.debug.traceContractCall(
+    {
+        contractInput: {
+            to: '0x0000000000000000000000000000456E65726779',
+            data: '0xa9059cbb0000000000000000000000000000000000000000000000000000456e65726779000000000000000000000000000000000000000000000004563918244f400000',
+            value: '0x0'
+        },
+        transactionOptions: {
+            caller: '0x625fCe8dd8E2C05e82e77847F3da06AF6e55A7AF',
+            gasPayer: '0x625fCe8dd8E2C05e82e77847F3da06AF6e55A7AF',
+            expiration: 18,
+            blockRef: '0x0101d05409d55cce'
+        },
+        config: {}
+    },
+    // Note that in the testnet only the 'call' option is available.
+    'call'
+);
 
-// // 3 - Print the result.
-// console.log(result);
+// 3 - Print the result.
+console.log(result);
 
-// // END_SNIPPET: DebugTraceContractCallSnippet
+// END_SNIPPET: DebugTraceContractCallSnippet
