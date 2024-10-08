046bc58ddfd49c590bf6f1a7d19120839feed98c
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-10-08 09:01:34 +0200

    fix: fix typos

--- packages/network/src/provider/utils/rpc-mapper/methods/eth_estimateGas/eth_estimateGas.ts
+++ packages/network/src/provider/utils/rpc-mapper/methods/eth_estimateGas/eth_estimateGas.ts
@@ -23,7 +23,7 @@ import { RPC_DOCUMENTATION_URL } from '../../../../../utils';
  * @param params - The standard array of rpc call parameters.
  *                 * params[0]: The transaction call object.
  *                 * params[1]: A string representing a block number, or one of the string tags latest, earliest, or pending.
- * @returns A hexadecimal number representing of the estimation of the gas for a given transaction.
+ * @returns A hexadecimal number representing the estimation of the gas for a given transaction.
  * @throws {JSONRPCInvalidParams, JSONRPCInternalError}
  */
 const ethEstimateGas = async (
