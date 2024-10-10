bb1d9fba14320e6abc04cddf34e6ca5df8537a27
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2024-10-10 15:29:32 +0100

    feat: sign working

--- packages/aws-kms-adapter/tests/KMSVeChainSigner.solo.test.ts
+++ packages/aws-kms-adapter/tests/KMSVeChainSigner.solo.test.ts
@@ -1,5 +1,14 @@
-import { THOR_SOLO_URL, ThorClient } from '@vechain/sdk-network';
+import {
+    ABIContract,
+    Address,
+    Clause,
+    HexUInt,
+    Transaction,
+    type TransactionClause
+} from '@vechain/sdk-core';
+import { signerUtils, THOR_SOLO_URL, ThorClient } from '@vechain/sdk-network';
 import { KMSVeChainProvider, KMSVeChainSigner } from '../src';
+import { TESTING_CONTRACT_ABI, TESTING_CONTRACT_ADDRESS } from './fixture';
 
 /**
  * AWS KMS VeChain signer tests - solo
@@ -34,7 +43,71 @@ describe('KMSVeChainSigner - Thor Solo', () => {
     describe('getAddress', () => {
         test('should get the address from the public key', async () => {
             expect(signer).toBeInstanceOf(KMSVeChainSigner);
-            expect(await signer.getAddress()).toBe('');
+            expect(await signer.getAddress()).toBe(
+                '0x4D31C0916a310DA4ddEC0DbED626e848937Fd465'
+            );
         });
+    });
+
+    /**
+     * Test suite for signTransaction method
+     */
+    describe('signTransaction', () => {
+        test('should sign a transaction successfully', async () => {
+            const sampleClause = Clause.callFunction(
+                Address.of(TESTING_CONTRACT_ADDRESS),
+                ABIContract.ofAbi(TESTING_CONTRACT_ABI).getFunction('deposit'),
+                [123]
+            ) as TransactionClause;
+
+            const originAddress = await signer.getAddress();
+
+            const gasResult = await thorClient.gas.estimateGas(
+                [sampleClause],
+                originAddress
+            );
+
+            const txBody = await thorClient.transactions.buildTransactionBody(
+                [sampleClause],
+                gasResult.totalGas,
+                {
+                    isDelegated: false
+                }
+            );
+
+            const signedRawTx = await signer.signTransaction(
+                signerUtils.transactionBodyToTransactionRequestInput(
+                    txBody,
+                    originAddress
+                )
+            );
+            const signedTx = Transaction.decode(
+                HexUInt.of(signedRawTx.slice(2)).bytes,
+                true
+            );
+
+            expect(signedTx).toBeDefined();
+            const expectedBody = {
+                chainTag: 246,
+                clauses: [
+                    {
+                        data: '0xb6b55f25000000000000000000000000000000000000000000000000000000000000007b',
+                        to: '0xb2c20a6de401003a671659b10629eb82ff254fb8',
+                        value: 0
+                    }
+                ],
+                dependsOn: null,
+                expiration: 32,
+                gas: 57491,
+                gasPriceCoef: 0
+            };
+            expect(signedTx.body).toMatchObject(expectedBody);
+            expect(signedTx.origin.toString()).toBe(
+                Address.checksum(HexUInt.of(originAddress))
+            );
+            expect(signedTx.isDelegated).toBe(false);
+            expect(signedTx.isSigned).toBe(true);
+            expect(signedTx.signature).toBeDefined();
+        }, 8000);
     });
 });
