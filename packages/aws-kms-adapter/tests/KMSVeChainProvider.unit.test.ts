46ff763bef5faa5c42871782557e679823164a46
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2024-10-10 18:45:43 +0100

    feat: unit tests started

--- packages/aws-kms-adapter/tests/KMSVeChainProvider.unit.test.ts
+++ packages/aws-kms-adapter/tests/KMSVeChainProvider.unit.test.ts
@@ -1,1 +1,31 @@
-// TODO: tests
+import { type ThorClient } from '@vechain/sdk-network';
+import { KMSVeChainProvider } from '../src';
+
+/**
+ * AWS KMS VeChain provider tests - unit
+ *
+ * @group unit/providers/vechain-aws-kms-provider
+ */
+describe('KMSVeChainProvider', () => {
+    describe('constructor', () => {
+        it('should return the instance of the client', () => {
+            const instance = new KMSVeChainProvider(
+                {} as unknown as ThorClient,
+                'keyId',
+                'region'
+            );
+            expect(instance).toBeInstanceOf(KMSVeChainProvider);
+        });
+    });
+    describe('getSigner', () => {
+        it('should return the instance of the signer', async () => {
+            const instance = new KMSVeChainProvider(
+                {} as unknown as ThorClient,
+                'keyId',
+                'region'
+            );
+            const signer = await instance.getSigner();
+            expect(signer).toBeDefined();
+        });
+    });
+});
