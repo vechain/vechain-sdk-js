33db5dd6b7373a5f0064f9c14f0aae58c8d5def6
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2024-10-11 16:58:57 +0100

    feat: created key with custom material

--- localstack/init/kms.sh
+++ localstack/init/kms.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-awslocal kms create-key
+CUSTOM_KEY_MATERIAL="MHQCAQEEIH+SkMxExf0rlf4h1q1v5fqcF34c1vO0yWqXsT4J6qFYoAcGBSuBBAAKoUQDQgAEMDsHgoGGtL5+q81ZRo5ZnFfTOvkNhnhHugDH2XvK8zBogAYAEHTnd7fByPdyjzVvqqdYp5yzB2d+bzpLBLbqMA=="
+CUSTOM_ID="bffb20d8-35ca-4408-9d54-f775b929b38d"
+
+awslocal kms create-key --key-usage SIGN_VERIFY --key-spec ECC_SECG_P256K1 --tags "[{\"TagKey\":\"_custom_key_material_\",\"TagValue\":\"$CUSTOM_KEY_MATERIAL\"},{\"TagKey\":\"_custom_id_\",\"TagValue\":\"$CUSTOM_ID\"}]"
