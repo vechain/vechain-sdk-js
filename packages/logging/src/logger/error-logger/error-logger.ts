3ab0bf049b612557dd29c8fca35dd1545e24ec17
Fabio Rigamonti <fabio.rigamonti@icloud.com>
2024-10-23 14:58:07 +0200

    fix: improve logger

--- packages/logging/src/logger/error-logger/error-logger.ts
+++ packages/logging/src/logger/error-logger/error-logger.ts
@@ -7,13 +7,14 @@ import { type VechainSDKError } from '@vechain/sdk-errors';
 const _logErrorFunction: LogFunctionType<'error'> = {
     log: <TErrorData>(error: VechainSDKError<TErrorData>) => {
         console.error(
-            `\n****************** ERROR ON: ${error.methodName} ******************\n` +
-                `- Error message: '${error.errorMessage}'` +
-                '\n- Error data:\n',
+            `\n****************** ERROR ON: %s ******************\n` +
+                `- Error message: '%s'` +
+                '\n- Error data:\n%o' +
+                `\n- Internal error:\n%o\n`,
+            error.methodName,
+            error.errorMessage,
             error.data,
-            `\n- Internal error:\n`,
-            error.innerError,
-            `\n`
+            error.innerError
         );
     }
 };
