d2c0a131ac528b46758f357dd3edf86f72bb18d7
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-11-04 12:55:37 +0100

    fix: add tests

---
+++ apps/sdk-vite-integration/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chromium',
+      provider: 'playwright',
+    },
+  },
+})
