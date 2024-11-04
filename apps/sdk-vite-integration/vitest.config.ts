84167836712ca564723b54b4c16c31bba288223d
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-11-04 12:56:16 +0100

    fix: fix vitest error

--- apps/sdk-vite-integration/vitest.config.ts
+++ apps/sdk-vite-integration/vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, ViteUserConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as ViteUserConfig["plugins"],
   test: {
     browser: {
       enabled: true,
