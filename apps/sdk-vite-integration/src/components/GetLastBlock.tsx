2c65ef2bda08d234336d238d2af13acd3ebfa2d8
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-11-07 15:06:47 +0100

    Create a Vite sample application (#1451)
    * feat: add vite blank project
    
    * fix: update yarn.lock
    
    * fix: add Hash page
    
    * fix: add SDK network
    
    * fix: complete vite SDK integration
    
    * fix: add another example
    
    * fix: update tsconfig for vite sample dApp
    
    * fix: refactor code
    
    * fix: refactor code
    
    * fix: refactor code
    
    * fix: add tests
    
    * fix: fix vitest error
    
    * fix: fix error tests
    
    * fix: fix error tests
    
    * fix: fix error when testing

---
+++ apps/sdk-vite-integration/src/components/GetLastBlock.tsx
@@ -0,0 +1,38 @@
+import { type CompressedBlockDetail, ThorClient } from "@vechain/sdk-network";
+import { useState } from "react";
+
+const GetLastBlock = () => {
+    const [block, setBlock] = useState<CompressedBlockDetail | null>(null);
+
+    // URL of the VeChain mainnet
+    const mainnetUrl = 'https://mainnet.vechain.org';
+
+    // Thor client instance
+    const thorClient = ThorClient.fromUrl(mainnetUrl);
+
+    // Function to fetch the last block
+    const fetchLastBlock = async () => {
+        try {
+            const lastBlock = await thorClient.blocks.getBlockCompressed("best");
+            setBlock(lastBlock);
+        } catch (error) {
+            console.error("Error fetching the last block:", error);
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={fetchLastBlock}>
+                Get Last Block
+            </button>
+            {block && (
+                <div>
+                    <h3>Last Block Details:</h3>
+                    <pre>{JSON.stringify(block, null, 2)}</pre>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default GetLastBlock;
---
+++ apps/sdk-vite-integration/src/const/const.tsx
@@ -0,0 +1,18 @@
+import { ThorClient } from '@vechain/sdk-network';
+
+/**
+ * Url of the VeChain mainnet
+ */
+const mainnetUrl = 'https://mainnet.vechain.org';
+
+/**
+ * Thor client instance
+ */
+const thorClient = ThorClient.fromUrl(mainnetUrl);
+
+/**
+ * Explorer url
+ */
+const explorerUrl = 'https://explore.vechain.org';
+
+export { thorClient, explorerUrl };
