22137eafde5857629be66669ceefeb40013f9b9d
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-11-11 14:43:49 +0100

    fix: downgrade eslint-plugin-sonarjs for node compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -86,6 +86,7 @@ export default [{
         "sonarjs/new-cap": "off",
         "security/detect-object-injection": "off",
         "security/detect-unsafe-regex": "off",
-        "sonarjs/sonar-no-unused-vars": "off"
+        "sonarjs/sonar-no-unused-vars": "off",
+        "sonarjs/sonar-no-fallthrough": "off"
     },
 }];
