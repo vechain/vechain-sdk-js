af56fe6592767d249a63f453dfa2e30f21538ced
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2024-12-01 11:36:58 +0000

    feat: v2 thor node dev...

--- packages/v2-network/src/thor/node/RetrieveConnectedPeers.ts
+++ packages/v2-network/src/thor/node/RetrieveConnectedPeers.ts
@@ -2,23 +2,24 @@ import { type HttpClient, type HttpPath } from '../../http';
 import { type ThorRequest } from '../ThorRequest';
 import { type PeerResponse } from './PeerResponse';
 import { BlockId, UInt } from '@vechain/sdk-core';
+import { type GetPeersResponse } from './GetPeersResponse';
 
 class RetrieveConnectedPeers implements ThorRequest {
     public static readonly PATH: HttpPath = { path: '/node/network/peers' };
 
-    async askTo(httpClient: HttpClient): Promise<PeerResponse[]> {
+    async askTo(httpClient: HttpClient): Promise<GetPeersResponse> {
         const response = await httpClient.get(RetrieveConnectedPeers.PATH);
         const responseBody: ResponseElement[] =
             (await response.json()) as ResponseElement[];
-        return responseBody.map((peer) => {
+        return responseBody.map((responseElement) => {
             return {
-                name: peer.name,
-                bestBlockID: BlockId.of(peer.bestBlockID),
-                totalScore: UInt.of(peer.totalScore),
-                peerID: peer.peerID,
-                netAddr: peer.netAddr,
-                inbound: peer.inbound,
-                duration: UInt.of(peer.duration)
+                name: responseElement.name,
+                bestBlockID: BlockId.of(responseElement.bestBlockID),
+                totalScore: UInt.of(responseElement.totalScore),
+                peerID: responseElement.peerID,
+                netAddr: responseElement.netAddr,
+                inbound: responseElement.inbound,
+                duration: UInt.of(responseElement.duration)
             } satisfies PeerResponse;
         });
     }
