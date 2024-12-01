21b60e380d36f057cf8b919199f0043a032ea88c
leszek-vechain <leszek.lorek@vechain.org>
2024-12-01 17:23:40 +0000

    fix: import and bidi
    - change process in vitest configs

--- apps/sdk-vite-integration/vitest.jsdom.config.ts
+++ apps/sdk-vite-integration/vitest.jsdom.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()] as ViteUserConfig["plugins"],
+  define: {
+    'process.env': JSON.stringify({})
+  },
   test: {
     environment: 'jsdom',
     browser: {
