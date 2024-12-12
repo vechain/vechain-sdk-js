1e0e536110e3deaa15acd28e7e36025bf9c367e5
Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>
2024-12-12 14:26:50 +0100

    [TransactionRequestInput] - add fields (#1562)
    * fix: add coment and delegateUrl params
    
    * fix: fix failing test
    
    * fix: increase testing time
    
    ---------
    
    Co-authored-by: Miguel Angel Rojo <freemanz1486@gmail.com>

--- packages/network/src/thor-client/contracts/types.d.ts
+++ packages/network/src/thor-client/contracts/types.d.ts
@@ -30,6 +30,16 @@ declare module 'viem/node_modules/abitype' {
 type ContractTransactionOptions = {
     value?: number;
     signTransactionOptions?: SignTransactionOptions;
+
+    /**
+     * The delegation URL to use to sponsor the transaction.
+     */
+    delegationUrl?: string;
+
+    /**
+     * A comment describing the transaction request.
+     */
+    comment?: string;
 } & TransactionBodyOptions &
     ClauseOptions;
 
