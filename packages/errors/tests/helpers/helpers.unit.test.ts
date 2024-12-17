db1aa68a5f624c630cab8b261420acd568d2de16
leszek-vechain <158060059+leszek-vechain@users.noreply.github.com>
2024-12-17 13:36:07 +0000

    fix(1557): fix for big int serialization (#1581)
    - changed serialization of bigint
    - cleaned up tests and added new one
    
    Co-authored-by: Fabio Rigamonti <73019897+fabiorigam@users.noreply.github.com>

--- packages/errors/tests/helpers/helpers.unit.test.ts
+++ packages/errors/tests/helpers/helpers.unit.test.ts
@@ -73,65 +73,18 @@ describe('Error package helpers unit tests', () => {
         });
 
         /**
-         * Error message builder function
+         * An error message is built with big int data
          */
-        test('Should be able to build an error message', () => {
+        test('Should be able to build an error message - bigint', () => {
             const errorMessage = createErrorMessage(
                 'simpleMethod',
                 'Error message',
                 {
-                    data: 'test'
-                },
-                new Error('Internal error')
-            );
-            expect(errorMessage).toBeDefined();
-        });
-
-        /**
-         * An error message is built with inner error undefined
-         */
-        test('Should be able to build an error message - inner error undefined', () => {
-            const errorMessage = createErrorMessage(
-                'simpleMethod',
-                'Error message',
-                {
-                    data: 'test'
+                    data: 10n
                 }
             );
             expect(errorMessage).toBeDefined();
-        });
-
-        /**
-         * An error message is built with circular dependency on data
-         */
-        test('Should be able to build an error message - circular dependency', () => {
-            // Simple circular dependency object
-            const circularDependencyObject: {
-                prop1: string;
-                prop2: {
-                    prop3: string;
-                    prop4?: unknown;
-                };
-            } = {
-                prop1: 'value1',
-                prop2: {
-                    prop3: 'value3'
-                }
-            };
-
-            // Introduce circular reference
-            circularDependencyObject.prop2.prop4 = circularDependencyObject;
-
-            const errorMessage = createErrorMessage(
-                'simpleMethod',
-                'Error message',
-                {
-                    params: [-1],
-                    data: circularDependencyObject
-                },
-                new Error('Internal error')
-            );
-            expect(errorMessage).toBeDefined();
+            expect(errorMessage).toContain('10n');
         });
     });
 
