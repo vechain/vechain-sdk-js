f4dd7ae74b258962fa551d103ce0b417ab3e7fa3
Luc Grandin <104393342+GrandinLuc@users.noreply.github.com>
2024-12-24 12:46:58 +0100

    Revert "Revert "refactor: using only `viem` for EIP712" (#1547)"
    This reverts commit b8e5a35726ba1cde3bfe3d8a7f8a14c895f3b7d8.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import localRules from "eslint-plugin-local-rules";
-import sonarjs from "eslint-plugin-sonarjs";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import localRules from 'eslint-plugin-local-rules';
+import sonarjs from 'eslint-plugin-sonarjs';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,85 +16,120 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default [{
-    ignores: ["**/node_modules/", "**/dist/", "**/coverage/", "**/coverageUnit/", "**/jest.config.*", "**/eslint.config.mjs", "**/jest.browser-setup.js"],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-    "standard-with-typescript",
-    "prettier",
-    "plugin:sonarjs/recommended-legacy",
-    "plugin:security/recommended-legacy",
-), {
-    files: ["**/src/**/*.ts", "**/tests/**/*.ts"],
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        "local-rules": localRules,
-        sonarjs,
+export default [
+    {
+        ignores: [
+            '**/node_modules/',
+            '**/dist/',
+            '**/coverage/',
+            '**/coverageUnit/',
+            '**/jest.config.*',
+            '**/eslint.config.mjs',
+            '**/jest.browser-setup.js',
+            '**/coverageIntegration/**',  // Add this line
+            '**/lcov-report/**',          // Add this line
+            '**/*.js',                    // Add this to ignore all JS files if needed
+        ]
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
+    ...compat.extends(
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+        'standard-with-typescript',
+        'prettier',
+        'plugin:sonarjs/recommended-legacy',
+        'plugin:security/recommended-legacy'
+    ),
+    {
+        files: ['**/src/**/*.ts', '**/tests/**/*.ts'],
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+            'local-rules': localRules,
+            sonarjs
         },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "script",
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.commonjs
+            },
 
-        parserOptions: {
-            project: "./tsconfig.json",
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'script',
+
+            parserOptions: {
+                project: './tsconfig.json'
+            }
         },
+
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_'
+                }
+            ],
+
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-non-null-assertion': 'error',
+            '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+            '@typescript-eslint/prefer-nullish-coalescing': 'error',
+            '@typescript-eslint/prefer-optional-chain': 'error',
+            '@typescript-eslint/non-nullable-type-assertion-style': 'off',
+            '@typescript-eslint/no-invalid-void-type': 'off',
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/ban-types': 'off',
+            '@typescript-eslint/no-throw-literal': 'off',
+            '@typescript-eslint/lines-between-class-members': 'off',
+
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    trailingComma: 'none',
+                    tabWidth: 4
+                }
+            ],
+
+            'no-warning-comments': 'warn',
+            'no-multi-str': 'off',
+            'local-rules/disallow-buffer-from-alloc': 'error',
+            'local-rules/disallow-instanceof-uint8array': 'error',
+            'sonarjs/different-types-comparison': 'off',
+            'sonarjs/no-ignored-exceptions': 'off',
+            'sonarjs/no-nested-functions': 'off',
+            'sonarjs/function-return-type': 'off',
+            'sonarjs/no-nested-conditional': 'off',
+            'sonarjs/cognitive-complexity': 'off',
+            'sonarjs/new-cap': 'off',
+            'security/detect-object-injection': 'off',
+            'security/detect-unsafe-regex': 'off',
+            'security/detect-non-literal-fs-filename': 'off',
+            'sonarjs/sonar-no-unused-vars': 'off',
+            'sonarjs/sonar-no-fallthrough': 'off',
+            'sonarjs/no-empty-function': [
+                'error',
+                {
+                    allow: ['arrowFunctions', 'functions', 'methods']
+                }
+            ],
+            'import/no-extraneous-dependencies': [
+                'error',
+                {
+                    devDependencies: false,
+                    optionalDependencies: false,
+                    peerDependencies: false
+                }
+            ]
+        }
     },
-
-    rules: {
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/explicit-function-return-type": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        "@typescript-eslint/no-unnecessary-type-assertion": "error",
-        "@typescript-eslint/prefer-nullish-coalescing": "error",
-        "@typescript-eslint/prefer-optional-chain": "error",
-        "@typescript-eslint/non-nullable-type-assertion-style": "off",
-        "@typescript-eslint/no-invalid-void-type": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-        "@typescript-eslint/ban-types": "off",
-        "@typescript-eslint/no-throw-literal": "off",
-        "@typescript-eslint/lines-between-class-members": "off",
-
-        "prettier/prettier": ["error", {
-            singleQuote: true,
-            trailingComma: "none",
-            tabWidth: 4,
-        }],
-
-        "no-warning-comments": "warn",
-        "no-multi-str": "off",
-        "local-rules/disallow-buffer-from-alloc": "error",
-        "local-rules/disallow-instanceof-uint8array": "error",
-        "sonarjs/different-types-comparison": "off",
-        "sonarjs/no-ignored-exceptions": "off",
-        "sonarjs/no-nested-functions": "off",
-        "sonarjs/function-return-type": "off",
-        "sonarjs/no-nested-conditional": "off",
-        "sonarjs/cognitive-complexity": "off",
-        "sonarjs/new-cap": "off",
-        "security/detect-object-injection": "off",
-        "security/detect-unsafe-regex": "off",
-        "security/detect-non-literal-fs-filename": "off",
-        "sonarjs/sonar-no-unused-vars": "off",
-        "sonarjs/sonar-no-fallthrough": "off",
-        "import/no-extraneous-dependencies": ["error", {"devDependencies": false, "optionalDependencies": false, "peerDependencies": false}]
+    {
+        files: ['**/tests/**/*.ts'],
+        rules: {
+            'import/no-extraneous-dependencies': 'off'
+        }
     }
-}, {
-    files: ["**/tests/**/*.ts"],
-    rules: {
-        "import/no-extraneous-dependencies": "off",
-    }
-}];
+];
