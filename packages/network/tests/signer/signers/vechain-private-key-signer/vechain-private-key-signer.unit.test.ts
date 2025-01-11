9bb32ac8f3dc5d31bb1e1ac4b47c5be70fefabf5
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-01-11 13:34:41 +0000

    feat: first commit

--- packages/network/tests/signer/signers/vechain-private-key-signer/vechain-private-key-signer.unit.test.ts
+++ packages/network/tests/signer/signers/vechain-private-key-signer/vechain-private-key-signer.unit.test.ts
@@ -361,6 +361,19 @@ describe('VeChain base signer tests', () => {
                     eip712TestCases.valid.data
                 );
             expect(actualWithoutPrimaryType).toBe(expected);
+
+            // Using VeChain chainId as string and bigint
+            const expectedVeChain = await new Wallet(
+                eip712TestCases.valid.privateKey
+            ).signTypedData(
+                {
+                    ...eip712TestCases.valid.domain,
+                    chainId:
+                        '1176455790972829965191905223412607679856028701100105089447013101863'
+                },
+                eip712TestCases.valid.types,
+                eip712TestCases.valid.data
+            );
             const actualWithStringChainId =
                 await privateKeySigner.signTypedData(
                     {
@@ -372,7 +385,7 @@ describe('VeChain base signer tests', () => {
                     eip712TestCases.valid.data,
                     eip712TestCases.valid.primaryType
                 );
-            expect(actualWithStringChainId).toBe(expected);
+            expect(actualWithStringChainId).toBe(expectedVeChain);
             const actualWithBigintChainId =
                 await privateKeySigner.signTypedData(
                     {
@@ -384,7 +397,7 @@ describe('VeChain base signer tests', () => {
                     eip712TestCases.valid.data,
                     eip712TestCases.valid.primaryType
                 );
-            expect(actualWithBigintChainId).toBe(expected);
+            expect(actualWithBigintChainId).toBe(expectedVeChain);
         });
     });
 });
