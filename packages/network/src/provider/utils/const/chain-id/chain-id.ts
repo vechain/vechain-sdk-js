8a119e1e67498ce08b9d2261925566da632755c1
clayton neal <clayton.neal@vechain.org>
2025-02-21 16:43:11 +0000

    fix: mainnet id

--- packages/network/src/provider/utils/const/chain-id/chain-id.ts
+++ packages/network/src/provider/utils/const/chain-id/chain-id.ts
@@ -5,7 +5,7 @@
  */
 const CHAIN_ID = {
     MAINNET:
-        '0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6',
+        '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a',
     TESTNET:
         '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127'
 };
