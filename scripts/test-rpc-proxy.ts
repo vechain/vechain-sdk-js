0266ae5d57419d50a7fea131895dcf118587314b
clayton neal <clayton.neal@vechain.org>
2025-02-24 14:34:54 +0000

    fix: rpc test

--- scripts/test-rpc-proxy.ts
+++ scripts/test-rpc-proxy.ts
@@ -4,12 +4,14 @@ const endpointsTestCases = [
     {
         method: 'net_version',
         params: [],
-        expected: '0x186aa'
+        expected:
+            '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127'
     },
     {
         method: 'eth_chainId',
         params: [],
-        expected: '0x186aa'
+        expected:
+            '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127'
     },
     {
         method: 'web3_clientVersion',
