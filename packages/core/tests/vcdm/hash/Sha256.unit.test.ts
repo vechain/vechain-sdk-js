452048ffb17d1f4418d8d4fb0f261030a157d19f
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-03-16 12:31:54 +0000

    refactor: 1870 core/vcdm/hash

--- packages/core/tests/vcdm/hash/Sha256.unit.test.ts
+++ packages/core/tests/vcdm/hash/Sha256.unit.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { InvalidOperation } from '@vechain/sdk-errors';
-import { Hex, Sha256 } from '../../../src';
+import { Hex, IllegalArgumentError, Sha256 } from '../../../src';
 import { CONTENT, NO_CONTENT } from './fixture';
 
 // Hex on purpose because it must be equal to the returned HexUInt hash.
@@ -41,6 +40,6 @@ describe('Sha256 class tests', () => {
     });
 
     test('Throw an exception for illegal content', () => {
-        expect(() => Sha256.of('0xfoe')).toThrow(InvalidOperation);
+        expect(() => Sha256.of('0xfoe')).toThrow(IllegalArgumentError);
     });
 });
