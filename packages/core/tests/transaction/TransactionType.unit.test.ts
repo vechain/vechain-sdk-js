b3d4eb7431cf53226c61704a2aaea1e8d751d5c1
clayton neal <clayton.neal@vechain.org>
2025-04-01 10:36:20 +0100

    chore: enable tests

---
+++ packages/core/tests/transaction/TransactionType.unit.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from '@jest/globals';
+import {
+    TransactionType,
+    fromTransactionType,
+    toTransactionType
+} from '../../src';
+import { InvalidTransactionType } from '@vechain/sdk-errors';
+
+describe('TransactionType', () => {
+    test('should be able to convert toTransactionType', () => {
+        expect(toTransactionType(81)).toBe(TransactionType.EIP1559);
+        expect(toTransactionType(0)).toBe(TransactionType.Legacy);
+    });
+    test('should be able to convert fromTransactionType', () => {
+        expect(fromTransactionType(TransactionType.EIP1559)).toBe(81);
+        expect(fromTransactionType(TransactionType.Legacy)).toBe(0);
+    });
+    test('should throw an error if the transaction type is not valid', () => {
+        expect(() => toTransactionType(256)).toThrow(InvalidTransactionType);
+
+        // Test error properties
+        const throwingFunction = (): TransactionType => toTransactionType(256);
+        expect(throwingFunction).toThrowError(
+            expect.objectContaining({
+                message: 'Invalid transaction type',
+                data: {
+                    transactionType: '256',
+                    validTypes: '0, 81'
+                }
+            })
+        );
+    });
+});
