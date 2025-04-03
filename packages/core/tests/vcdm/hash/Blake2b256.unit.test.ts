874aae58a21428f4f4587904826a6df0f4762a2c
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-04-03 17:44:38 +0100

    feat: 1857 /blocks galactica...

--- packages/core/tests/vcdm/hash/Blake2b256.unit.test.ts
+++ packages/core/tests/vcdm/hash/Blake2b256.unit.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from '@jest/globals';
-import { Blake2b256, Hex, IllegalArgumentError } from '../../../src';
+import { Blake2b256, Hex, HexUInt32, IllegalArgumentError } from '../../../src';
 import { CONTENT, NO_CONTENT } from './fixture';
 
 // Hex on purpose because it must be equal to the returned HexUInt hash.
-const CONTENT_BLAKE2B256 = Hex.of(
+const CONTENT_BLAKE2B256 = HexUInt32.of(
     '0x6a908bb80109908919c0bf5d0594c890700dd46acc097f9f28bfc85a0a2e6c0c'
 );
 
 // Hex on purpose because it must be equal to the returned HexUInt hash.
-const NO_CONTENT_BLAKE2B256 = Hex.of(
+const NO_CONTENT_BLAKE2B256 = HexUInt32.of(
     '0x0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8'
 );
 
@@ -36,7 +36,7 @@ describe('Blake2b256 class tests', () => {
 
     test('Return hash for no content', () => {
         const hash = Blake2b256.of(NO_CONTENT);
-        expect(hash.isEqual(NO_CONTENT_BLAKE2B256)).toBe(true);
+        expect(hash.isEqual(NO_CONTENT_BLAKE2B256));
     });
 
     test('Throw an exception for illegal content', () => {
