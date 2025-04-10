54455336882059d6ba3a990665963575e1368863
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-04-10 16:07:10 +0200

    test: 2005 `Block.baseFeePerGas` **galactica** checks

---
+++ packages/network/tests/thor-client/blocks/blocks.solo.test.ts
@@ -0,0 +1,36 @@
+import { afterEach, beforeEach, describe, expect } from '@jest/globals';
+import { SimpleHttpClient, THOR_SOLO_URL, ThorClient } from '../../../src';
+import { HexUInt } from '@vechain/sdk-core';
+
+/**
+ * Blocks Module integration tests
+ *
+ * @group integration/clients/thor-client/blocks
+ */
+describe('ThorClient - Blocks Module mock tests', () => {
+    let thorClient: ThorClient;
+
+    beforeEach(() => {
+        thorClient = new ThorClient(new SimpleHttpClient(THOR_SOLO_URL), {
+            isPollingEnabled: true
+        });
+    });
+
+    afterEach(() => {
+        thorClient.destroy();
+    });
+
+    test('OK <- getBlockCompressed(1)', async () => {
+        const block = await thorClient.blocks.getBlockCompressed(1); // Block 1 has transactions.
+        expect(block).toBeDefined();
+        expect(block?.baseFeePerGas).not.toBeNull();
+        expect(HexUInt.isValid0x(block?.baseFeePerGas as string)).toBeTruthy();
+    });
+
+    test('OK <- getBlockExpanded(1)', async () => {
+        const block = await thorClient.blocks.getBlockExpanded(1); // Block 1 has transactions.
+        expect(block).toBeDefined();
+        expect(block?.baseFeePerGas).not.toBeNull();
+        expect(HexUInt.isValid0x(block?.baseFeePerGas as string)).toBeTruthy();
+    });
+});
