c3864fd228a24bb2314c8075d99e61d370e5b818
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-04-11 16:38:15 +0100

    feat: modified eth_gasprice

--- packages/network/src/provider/utils/rpc-mapper/methods/eth_gasPrice/eth_gasPrice.ts
+++ packages/network/src/provider/utils/rpc-mapper/methods/eth_gasPrice/eth_gasPrice.ts
@@ -1,3 +1,4 @@
+import { HexUInt } from '@vechain/sdk-core';
 import { type ThorClient } from '../../../../../thor-client';
 
 /**
@@ -6,11 +7,23 @@ import { type ThorClient } from '../../../../../thor-client';
  * @returns The current gas price in Wei unit considering that 1 VTHO equals 1e18 Wei.
  */
 const ethGasPrice = async (thorClient: ThorClient): Promise<string> => {
-    const {
-        result: { plain }
-    } = await thorClient.contracts.getBaseGasPrice();
+    const bestBlock = await thorClient.blocks.getBestBlockCompressed();
+    const baseFeePerGas = bestBlock?.baseFeePerGas;
 
-    return '0x' + BigInt(plain as bigint).toString(16);
+    // Legacy transactions
+    if (baseFeePerGas === undefined) {
+        const {
+            result: { plain }
+        } = await thorClient.contracts.getBaseGasPrice();
+        return HexUInt.of(plain as bigint).toString();
+    }
+
+    const maxPriorityFeePerGas = await thorClient.gas.getMaxPriorityFeePerGas();
+
+    const baseFee = HexUInt.of(baseFeePerGas).bi;
+    const priority = HexUInt.of(maxPriorityFeePerGas).bi;
+
+    return HexUInt.of(baseFee + priority).toString();
 };
 
 export { ethGasPrice };
