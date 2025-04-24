7566df632ae6bb8564c783ff063e5780b69c8a9f
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-04-24 18:03:59 +0100

    feat: eth_call should reject when the simulation is reverted

--- packages/network/src/provider/utils/rpc-mapper/methods/eth_call/eth_call.ts
+++ packages/network/src/provider/utils/rpc-mapper/methods/eth_call/eth_call.ts
@@ -3,14 +3,14 @@ import {
     JSONRPCInvalidParams,
     stringifyData
 } from '@vechain/sdk-errors';
-import { type DefaultBlock, DefaultBlockToRevision } from '../../../const';
-import { type TransactionObjectInput } from './types';
 import {
     type SimulateTransactionClause,
     type SimulateTransactionOptions,
     type ThorClient
 } from '../../../../../thor-client';
 import { RPC_DOCUMENTATION_URL } from '../../../../../utils';
+import { type DefaultBlock, DefaultBlockToRevision } from '../../../const';
+import { type TransactionObjectInput } from './types';
 
 /**
  * RPC Method eth_call implementation
@@ -30,12 +30,13 @@ const ethCall = async (
         params.length !== 2 ||
         typeof params[0] !== 'object' ||
         (typeof params[1] !== 'object' && typeof params[1] !== 'string')
-    )
+    ) {
         throw new JSONRPCInvalidParams(
             'eth_call',
             `Invalid input params for "eth_call" method. See ${RPC_DOCUMENTATION_URL} for details.`,
             { params }
         );
+    }
 
     try {
         const [inputOptions, block] = params as [
@@ -63,9 +64,23 @@ const ethCall = async (
             } satisfies SimulateTransactionOptions
         );
 
+        if (simulatedTx.length === 0 || simulatedTx[0].reverted) {
+            throw new JSONRPCInternalError(
+                'eth_call()',
+                'Method "eth_call" failed when simulating the transaction.',
+                {
+                    params: stringifyData(params),
+                    innerError: simulatedTx[0].vmError
+                }
+            );
+        }
+
         // Return simulated transaction data
         return simulatedTx[0].data;
     } catch (e) {
+        if (e instanceof JSONRPCInternalError) {
+            throw e;
+        }
         throw new JSONRPCInternalError(
             'eth_call()',
             'Method "eth_call" failed.',
