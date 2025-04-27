8459619cbc1e4a93a74786f717b4637edb1c2b59
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-04-27 17:08:18 +0100

    feat: 1857 galactica /blocks align...

--- packages/thorest/tests/thor/blocks/RetrieveExpandedBlock.unit.test.ts
+++ packages/thorest/tests/thor/blocks/RetrieveExpandedBlock.unit.test.ts
@@ -6,7 +6,7 @@ import {
     type ExpandedBlockResponseJSON,
     type HttpClient,
     RetrieveBlockError,
-    RetrieveExpandedBlock
+    RetrieveExpandedBlock, RetrieveRegularBlock
 } from '../../../src';
 
 class InvalidRevision extends Revision {
@@ -155,5 +155,88 @@ describe('RetrieveExpandedBlock UNIT tests', () => {
         expect(actual).toBeDefined();
         expect(actual).toBeInstanceOf(ExpandedBlockResponse);
         expect(actual).toEqual(new ExpandedBlockResponse(expected));
+    });
+
+    test('ok <- block BEST', async () => {
+        const status = 200;
+        const expected = {
+            number: 1595,
+            id: '0x0000063b9400e519c27d5e79e0890210232a40a67168cc1dc89f071fb403b21a',
+            size: 366,
+            parentID:
+                '0x0000063a57f49442603c62b1008b18e5257a510bd8a2ed657037cb1f5698800b',
+            timestamp: 1745769810,
+            gasLimit: 150000000,
+            beneficiary: '0xf077b491b355e64048ce21e3a6fc4751eeea77fa',
+            gasUsed: 0,
+            totalScore: 1595,
+            txsRoot:
+                '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
+            txsFeatures: 1,
+            stateRoot:
+                '0x921a9a23b453acebc662889d6bb3d427d73b022b9cdc6fcafe24fc4b6d5d3f31',
+            receiptsRoot:
+                '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
+            com: false,
+            signer: '0xf077b491b355e64048ce21e3a6fc4751eeea77fa',
+            isTrunk: true,
+            isFinalized: false,
+            baseFeePerGas: '0x9184e72a000',
+            transactions: []
+        };
+        const actual = (
+            await RetrieveExpandedBlock.of(Revision.of(1)).askTo(
+                mockHttpClient<Response>(mockResponse(expected, status))
+            )
+        ).response;
+        expect(actual).toBeDefined();
+        expect(actual).toBeInstanceOf(ExpandedBlockResponse);
+        expect(actual).toEqual(new ExpandedBlockResponse(expected));
+    });
+
+    test('ok <- block FINALIZED', async () => {
+        const status = 200;
+        const expected = {
+            number: 0,
+            id: '0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6',
+            size: 170,
+            parentID:
+                '0xffffffff00000000000000000000000000000000000000000000000000000000',
+            timestamp: 1526400000,
+            gasLimit: 10000000,
+            beneficiary: '0x0000000000000000000000000000000000000000',
+            gasUsed: 0,
+            totalScore: 0,
+            txsRoot:
+                '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
+            txsFeatures: 0,
+            stateRoot:
+                '0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550',
+            receiptsRoot:
+                '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
+            com: false,
+            signer: '0x0000000000000000000000000000000000000000',
+            isTrunk: true,
+            isFinalized: true,
+            transactions: []
+        };
+        const actual = (
+            await RetrieveExpandedBlock.of(Revision.of(1)).askTo(
+                mockHttpClient<Response>(mockResponse(expected, status))
+            )
+        ).response;
+        expect(actual).toBeDefined();
+        expect(actual).toBeInstanceOf(ExpandedBlockResponse);
+        expect(actual).toEqual(new ExpandedBlockResponse(expected));
+    });
+
+    test('null <- block not found', async () => {
+        const status = 200;
+        const actual = (
+            await RetrieveRegularBlock.of(
+                Revision.of(Math.pow(2, 32) - 1) // Max block address value.
+            ).askTo(mockHttpClient<Response>(mockResponse(null, status)))
+        ).response;
+        expect(actual).toBeNull();
     });
 });
