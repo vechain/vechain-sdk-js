42e1c6729b686b47335735ade0cf3a88edbcbd72
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-04-29 10:25:13 +0100

    feat: code review comments

--- packages/network/src/provider/utils/rpc-mapper/methods/eth_gasPrice/eth_gasPrice.ts
+++ packages/network/src/provider/utils/rpc-mapper/methods/eth_gasPrice/eth_gasPrice.ts
@@ -1,4 +1,5 @@
 import { HexUInt } from '@vechain/sdk-core';
+import { JSONRPCInternalError } from '@vechain/sdk-errors';
 import { type ThorClient } from '../../../../../thor-client';
 
 /**
@@ -7,17 +8,25 @@ import { type ThorClient } from '../../../../../thor-client';
  * @returns The current gas price in Wei unit considering that 1 VTHO equals 1e18 Wei.
  */
 const ethGasPrice = async (thorClient: ThorClient): Promise<string> => {
-    const bestBlock = await thorClient.blocks.getBestBlockCompressed();
-    const baseFeePerGas = bestBlock?.baseFeePerGas;
-
-    // Legacy transactions
-    if (baseFeePerGas === undefined) {
+    // Check if Galactica hardfork has happened
+    const galacticaForked = await thorClient.forkDetector.detectGalactica();
+    if (!galacticaForked) {
         const {
             result: { plain }
         } = await thorClient.contracts.getBaseGasPrice();
         return HexUInt.of(plain as bigint).toString(true);
     }
 
+    const bestBlock = await thorClient.blocks.getBestBlockCompressed();
+    const baseFeePerGas = bestBlock?.baseFeePerGas;
+
+    if (baseFeePerGas === undefined) {
+        throw new JSONRPCInternalError(
+            'eth_gasPrice',
+            'Last block should have baseFeePerGas defined.',
+            { params: bestBlock, url: thorClient.httpClient.baseURL }
+        );
+    }
     const maxPriorityFeePerGas = await thorClient.gas.getMaxPriorityFeePerGas();
 
     const baseFee = HexUInt.of(baseFeePerGas).bi;
