dd149f3ab22b403261cfd662525b13cdc468e876
Luc Grandin <104393342+GrandinLuc@users.noreply.github.com>
2025-05-06 11:50:54 +0200

    fix: logging error

--- packages/network/src/http/SimpleHttpClient.ts
+++ packages/network/src/http/SimpleHttpClient.ts
@@ -165,6 +165,7 @@ class SimpleHttpClient implements HttpClient {
                 }
             );
         } catch (error) {
+            console.log('socket error:', error);
             throw new InvalidHTTPRequest(
                 'HttpClient.http()',
                 (error as Error).message,
