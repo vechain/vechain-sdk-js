d304e629d66e64f43bddecb70c6979660a5bfef5
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-05-09 15:38:06 +0100

    fix: 2075 FixedPointNumber.unit.test.ts

---
+++ packages/core/tests/hdkey/Bug2000.unit.test.ts
@@ -0,0 +1,39 @@
+import { describe } from '@jest/globals';
+import { HDKey, Hex } from '../../src';
+
+describe('bug 2000', () => {
+    const mnemonics = [
+        'process',
+        'spell',
+        'distance',
+        'daughter',
+        'step',
+        'glove',
+        'moment',
+        'exercise',
+        'library',
+        'check',
+        'toward',
+        'early'
+    ];
+
+    const derivationPath = "m/44'/818'/0'/0";
+
+    test('test', () => {
+        const hdnode = HDKey.fromMnemonic(mnemonics, derivationPath);
+        for (let i = 0; i < 5; i++) {
+            const a = hdnode.deriveChild(i);
+            console.log(
+                `A: ${i}: `,
+                Hex.of(a.privateKey as Uint8Array).toString()
+            );
+            const path = `${derivationPath}/${i}`;
+            const b = HDKey.fromMnemonic(mnemonics, path);
+            console.log(
+                `B: ${i}: `,
+                Hex.of(b.privateKey as Uint8Array).toString()
+            );
+            expect(a.privateKey).toEqual(b.privateKey);
+        }
+    });
+});
