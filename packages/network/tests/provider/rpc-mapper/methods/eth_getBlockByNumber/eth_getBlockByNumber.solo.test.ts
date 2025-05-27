da94f5a4abdb7a53b1e81819e8b3e3bb7f725c57
Giulia Argiolas <giulia.argiolas@vechain.org>
2025-05-27 10:57:44 +0200

    fix: retry logic and restore tests

--- packages/network/tests/provider/rpc-mapper/methods/eth_getBlockByNumber/eth_getBlockByNumber.solo.test.ts
+++ packages/network/tests/provider/rpc-mapper/methods/eth_getBlockByNumber/eth_getBlockByNumber.solo.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from '@jest/globals';
+import { afterEach, beforeEach, describe, expect, test } from '@jest/globals';
 import {
     type BlocksRPC,
     RPC_METHODS,
@@ -9,33 +9,29 @@ import {
     type TransactionRPC
 } from '../../../../../src';
 import { HexUInt } from '@vechain/sdk-core';
-import { ethGetBlockByNumberTestCases } from './fixture';
 
 /**
  * RPC Mapper integration tests for 'eth_getBlockByNumber' method
  *
- * @group integration/rpc-mapper/methods/eth_getBlockByNumber
+ * @group galactica/integration/rpc-mapper/methods/eth_getBlockByNumber
  */
 describe('RPC Mapper - eth_getBlockByNumber method tests', () => {
     // Add retry configuration for all tests in this suite
     jest.retryTimes(3, { logErrorsBeforeRetry: true });
-
-    // Increase timeout for RPC tests
-    const TIMEOUT = 30000; // 30 seconds
 
     /**
      * Thor client instance
      */
     let thorClient: ThorClient;
 
-    /**
-     * Init thor client before each test
-     */
     beforeEach(() => {
-        // Init thor client
         thorClient = new ThorClient(new SimpleHttpClient(THOR_SOLO_URL), {
             isPollingEnabled: true
         });
+    });
+
+    afterEach(() => {
+        thorClient.destroy();
     });
 
     describe('GALACTICA - baseFeePerGas', () => {
@@ -110,65 +106,5 @@ describe('RPC Mapper - eth_getBlockByNumber method tests', () => {
             });
             console.log(JSON.stringify(block, null, 2));
         });
-    });
-
-    /**
-     * eth_getBlockByNumber RPC call tests - Positive cases
-     */
-    describe('eth_getBlockByNumber - Positive cases', () => {
-        /**
-         * Test cases for eth_getBlockByNumber RPC method
-         */
-        ethGetBlockByNumberTestCases.forEach(
-            ({ description, params, expected }) => {
-                test(
-                    description,
-                    async () => {
-                        const rpcCall =
-                            await RPCMethodsMap(thorClient)[
-                                RPC_METHODS.eth_getBlockByNumber
-                            ](params);
-
-                        // Compare the result with the expected value
-                        expect(rpcCall).toStrictEqual(expected);
-                    },
-                    TIMEOUT
-                );
-            }
-        );
-
-        /**
-         * Positive case 1 - Should be able to get block by number
-         */
-        test(
-            'eth_getBlockByNumber - Should be able to get block by number',
-            async () => {
-                const rpcCall = (await RPCMethodsMap(thorClient)[
-                    RPC_METHODS.eth_getBlockByNumber
-                ](['0x1', false])) as BlocksRPC;
-
-                expect(rpcCall).toBeDefined();
-                expect(rpcCall.number).toBe('0x1');
-            },
-            TIMEOUT
-        );
-
-        /**
-         * Positive case 2 - Should be able to get block by number with full transaction objects
-         */
-        test(
-            'eth_getBlockByNumber - Should be able to get block by number with full transaction objects',
-            async () => {
-                const rpcCall = (await RPCMethodsMap(thorClient)[
-                    RPC_METHODS.eth_getBlockByNumber
-                ](['0x1', true])) as BlocksRPC;
-
-                expect(rpcCall).toBeDefined();
-                expect(rpcCall.number).toBe('0x1');
-                expect(rpcCall.transactions).toBeDefined();
-                expect(Array.isArray(rpcCall.transactions)).toBe(true);
-            },
-            TIMEOUT
-        );
     });
 });
--- packages/network/tests/provider/rpc-mapper/methods/eth_getLogs/eth_getLogs.mock.solo.test.ts
+++ packages/network/tests/provider/rpc-mapper/methods/eth_getLogs/eth_getLogs.mock.solo.test.ts
@@ -1,24 +1,22 @@
 import { beforeEach, describe, expect, test } from '@jest/globals';
 import {
+    type LogsRPC,
     RPC_METHODS,
     RPCMethodsMap,
     THOR_SOLO_URL,
     ThorClient
 } from '../../../../../src';
-import { mockLogsFixture } from './fixture';
-import { type LogsRPC } from '../../../../../src/provider/utils/formatter/logs/types';
+import { JSONRPCInternalError } from '@vechain/sdk-errors';
+import { logsFixture, mockLogsFixture } from './fixture';
 
 /**
  * RPC Mapper integration tests for 'eth_getLogs' method
  *
- * @group integration/rpc-mapper/methods/eth_getLogs
+ * @group integration/rpc-mapper/methods/eth_getLogs-mock
  */
 describe('RPC Mapper - eth_getLogs method tests', () => {
     // Add retry configuration for all tests in this suite
     jest.retryTimes(3, { logErrorsBeforeRetry: true });
-
-    // Increase timeout for RPC tests
-    const TIMEOUT = 30000; // 30 seconds
 
     /**
      * Thor client instance
@@ -38,19 +36,46 @@ describe('RPC Mapper - eth_getLogs method tests', () => {
      */
     describe('eth_getLogs - Positive cases', () => {
         /**
-         * Test cases for eth_getLogs RPC method
+         * Positive cases. Should be able to get logs
          */
         mockLogsFixture.forEach((fixture, index) => {
-            test(
-                `eth_getLogs - Should be able to get logs test - ${index + 1}`,
-                async () => {
-                    const rpcCall = (await RPCMethodsMap(thorClient)[
-                        RPC_METHODS.eth_getLogs
-                    ]([fixture.input])) as LogsRPC[];
-                    expect(rpcCall.slice(0, 4)).toStrictEqual(fixture.expected);
-                },
-                TIMEOUT
+            test(`eth_getLogs - Should be able to get logs test - ${index + 1}`, async () => {
+                // Mock the getGenesisBlock method to return null
+                jest.spyOn(
+                    thorClient.logs,
+                    'filterRawEventLogs'
+                ).mockResolvedValue([]);
+
+                // Call RPC method
+                const logs = (await RPCMethodsMap(thorClient)[
+                    RPC_METHODS.eth_getLogs
+                ]([fixture.input])) as LogsRPC[];
+
+                expect(logs.slice(0, 4)).toStrictEqual(fixture.expected);
+            }, 6000);
+        });
+    });
+
+    /**
+     * eth_getLogs RPC call tests - Negative cases
+     */
+    describe('eth_getLogs - Negative cases', () => {
+        /**
+         * Negative case 2 - Should throw an error for invalid input if request is invalid
+         */
+        test('eth_getLogs - Should throw error if request is invalid', async () => {
+            // Mock the filterGroupedEventLogs method to throw error
+            jest.spyOn(thorClient.logs, 'filterRawEventLogs').mockRejectedValue(
+                new Error()
             );
+
+            await expect(
+                async () =>
+                    // Call RPC method
+                    (await RPCMethodsMap(thorClient)[RPC_METHODS.eth_getLogs]([
+                        logsFixture[0].input
+                    ])) as LogsRPC[]
+            ).rejects.toThrowError(JSONRPCInternalError);
         });
     });
 });
