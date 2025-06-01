65769744369b9a7d9288725e646235da8f2d8adc
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-06-01 16:07:09 +0100

    feat: 2033 blocks...

--- packages/thorest/src/thor/utils/ThorResponse.ts
+++
@@ -1,9 +0,0 @@
-import { type ThorRequest } from './ThorRequest';
-
-export interface ThorResponse<
-    RequestClass extends ThorRequest<RequestClass, ResponseClass>,
-    ResponseClass
-> {
-    request: RequestClass;
-    response: ResponseClass;
-}
