85e0dd91d100c6a7fd2e41b10f3e069ec3deff98
Luca Nicola Debiasi <63785793+lucanicoladebiasi@users.noreply.github.com>
2025-06-13 08:02:00 +0900

    Update packages/thorest/src/thor/logs/EventLogsResponseJSON.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Luca Nicola Debiasi <63785793+lucanicoladebiasi@users.noreply.github.com>

--- packages/thorest/src/thor/logs/EventLogsResponseJSON.ts
+++ packages/thorest/src/thor/logs/EventLogsResponseJSON.ts
@@ -1,4 +1,5 @@
-import type { EventLogResponseJSON } from '@thor';
+-import type { EventLogResponseJSON } from '@thor';
++import type { EventLogResponseJSON } from '@thor/logs/EventLogResponseJSON';
 
 /**
  * [EventLogFilterRequest](http://localhost:8669/doc/stoplight-ui/#/schemas/EventLogFilterRequest)
