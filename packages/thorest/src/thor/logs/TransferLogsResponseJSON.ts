8f2176a203f8053d24765b26ea0b922b142291db
Luca Nicola Debiasi <63785793+lucanicoladebiasi@users.noreply.github.com>
2025-06-13 08:10:24 +0900

    Update TransferLogsResponseJSON.ts
    Signed-off-by: Luca Nicola Debiasi <63785793+lucanicoladebiasi@users.noreply.github.com>

--- packages/thorest/src/thor/logs/TransferLogsResponseJSON.ts
+++ packages/thorest/src/thor/logs/TransferLogsResponseJSON.ts
@@ -1,5 +1,3 @@
-import { type TransferLogResponseJSON } from '@thor/logs/TransferLogResponseJSON';
+import type { TransferLogResponseJSON } from '@thor/logs/TransferLogResponseJSON';
 
-interface TransferLogsResponseJSON extends Array<TransferLogResponseJSON> {}
-
-export type { TransferLogsResponseJSON };
+export type TransferLogsResponseJSON = TransferLogResponseJSON[];
