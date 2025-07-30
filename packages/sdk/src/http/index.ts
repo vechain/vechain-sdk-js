fc25bc7612d46909534268f5a8e2b47aef9acdb4
Andreas Frank <andreas.frank@MacBook-Pro-Andreas.local>
2025-07-30 15:30:37 +0300

    feat: removed http factory

--- packages/sdk/src/http/index.ts
+++ packages/sdk/src/http/index.ts
@@ -1,6 +1,4 @@
 export * from './FetchHttpClient';
 export type { HttpClient } from './HttpClient';
-export type { HttpClientFactory } from './HttpClientFactory';
-export { DefaultHttpClientFactory } from './HttpClientFactory';
 export type { HttpPath } from './HttpPath';
 export type { HttpQuery } from './HttpQuery';
