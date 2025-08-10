4be0c25353cb5f299966849a8f6f69cc79bb027c
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-08-10 16:13:25 +0100

    feat: 2361 rfeactored Clause.ts

--- packages/sdk/src/thor/json/ClauseJSON.ts
+++ packages/sdk/src/thor/json/ClauseJSON.ts
@@ -2,9 +2,26 @@
  * [Clause](http://localhost:8669/doc/stoplight-ui/#/schemas/Clause)
  */
 interface ClauseJSON {
+    /**
+     * The address that sent the VET.
+     */
     to: string | null; // hex address
-    value: string; // hex
-    data: string; // hex
+
+    /**
+     * The amount (wei) of VET to be transferred.
+     *
+     * Thor expresses the amount as a hexadecimal {@link Quantity} expression,
+     * aligned to nibble (4 bits); for example, zero is expressed as `0x0`.
+     *
+     * Creating a {@link TransactionRequest}, is convenient to express the
+     * value as a `bigint` type.
+     */
+    value: bigint | string;
+
+    /**
+     * The input data for the clause (in bytes).
+     */
+    data?: string; // hex
 }
 
 export { type ClauseJSON };
