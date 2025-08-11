ffe5ac5292202352ef55e0ccbcd72ee5f377ea7d
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-08-11 18:47:16 +0100

    feat: 2361 refactored Clause.ts

--- packages/sdk/src/thor/json/ClauseJSON.ts
+++ packages/sdk/src/thor/json/ClauseJSON.ts
@@ -3,25 +3,23 @@
  */
 interface ClauseJSON {
     /**
-     * The address that sent the VET.
+     * The recipient of the clause. Null indicates contract deployment.
      */
     to: string | null; // hex address
 
     /**
-     * The amount (wei) of VET to be transferred.
+     * The hexadecimal representation of the amount (wei) of VET to be transferred.
      *
      * Thor expresses the amount as a hexadecimal {@link Quantity} expression,
      * aligned to nibble (4 bits); for example, zero is expressed as `0x0`.
      *
-     * Creating a {@link TransactionRequest}, is convenient to express the
-     * value as a `bigint` type.
      */
-    value: bigint | string;
+    value: string; // hex ^0x[0-9a-f]*$
 
     /**
      * The input data for the clause (in bytes).
      */
-    data?: string; // hex
+    data?: string; // hex ^0x[0-9a-f]*$
 }
 
 export { type ClauseJSON };
