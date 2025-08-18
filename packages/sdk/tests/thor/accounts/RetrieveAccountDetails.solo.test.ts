08201d259b6d51d69495059f0e9012313d1f251f
Luc Grandin <104393342+GrandinLuc@users.noreply.github.com>
2025-08-18 11:40:15 +0200

    feat: added revision support to RetrieveAccountDetails

--- packages/sdk/tests/thor/accounts/RetrieveAccountDetails.solo.test.ts
+++ packages/sdk/tests/thor/accounts/RetrieveAccountDetails.solo.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, test } from '@jest/globals';
 import { RetrieveAccountDetails, ThorNetworks } from '@thor';
 import { FetchHttpClient } from '@http';
-import { Address, Hex } from '@vcdm';
+import { Address, Hex, Revision } from '@vcdm';
+import { VTHO_ADDRESS } from '@utils';
 
 /**
  * VeChain retrieve account details - solo
@@ -9,10 +10,43 @@ import { Address, Hex } from '@vcdm';
  * @group integration/thor/accounts
  */
 describe('RetrieveAccountDetails solo tests', () => {
-    test('ok <- askTo', async () => {
+    test('ok <- askTo (without revision)', async () => {
         const response = (
             await RetrieveAccountDetails.of(
                 Address.of('0x0000000000000000000000000000456E65726779')
+            ).askTo(FetchHttpClient.at(new URL(ThorNetworks.SOLONET)))
+        ).response;
+        expect(Hex.of(response.energy).toString()).toBe('0x00');
+        expect(response.hasCode).toBe(true);
+        expect(Hex.of(response.balance).toString()).toBe('0x00');
+    });
+
+    test('ok <- askTo with BEST revision', async () => {
+        const response = (
+            await RetrieveAccountDetails.of(
+                Address.of(VTHO_ADDRESS), Revision.BEST
+            ).askTo(FetchHttpClient.at(new URL(ThorNetworks.SOLONET)))
+        ).response;
+        expect(Hex.of(response.energy).toString()).toBe('0x00');
+        expect(response.hasCode).toBe(true);
+        expect(Hex.of(response.balance).toString()).toBe('0x00');
+    });
+
+    test('ok <- askTo with FINALIZED revision', async () => {
+        const response = (
+            await RetrieveAccountDetails.of(
+                Address.of(VTHO_ADDRESS), Revision.FINALIZED
+            ).askTo(FetchHttpClient.at(new URL(ThorNetworks.SOLONET)))
+        ).response;
+        expect(Hex.of(response.energy).toString()).toBe('0x00');
+        expect(response.hasCode).toBe(true);
+        expect(Hex.of(response.balance).toString()).toBe('0x00');
+    });
+
+    test('ok <- askTo with numeric revision', async () => {
+        const response = (
+            await RetrieveAccountDetails.of(
+                Address.of(VTHO_ADDRESS), Revision.of(0)
             ).askTo(FetchHttpClient.at(new URL(ThorNetworks.SOLONET)))
         ).response;
         expect(Hex.of(response.energy).toString()).toBe('0x00');
