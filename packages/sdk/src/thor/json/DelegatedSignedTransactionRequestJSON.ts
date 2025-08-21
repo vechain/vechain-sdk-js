f439db43b5f0e41787125b7eb8d0517b5f44d74d
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-08-21 10:15:01 +0100

    feat: 2311 TransactionRequest...

--- packages/sdk/src/thor/json/DelegatedSignedTransactionRequestJSON.ts
+++
@@ -1,8 +0,0 @@
-import { type SignedTransactionRequestJSON } from '@thor/json/SignedTransactionRequestJSON';
-
-interface DelegatedSignedTransactionRequestJSON extends SignedTransactionRequestJSON {
-    gasPayer: string;
-    gasPayerSignature: Uint8Array;
-}
-
-export { type DelegatedSignedTransactionRequestJSON };
--- packages/sdk/src/thor/json/SignedTransactionRequestJSON.ts
+++
@@ -1,9 +0,0 @@
-import { type TransactionRequestJSON } from '@thor/json/TransactionRequestJSON';
-
-interface SignedTransactionRequestJSON extends TransactionRequestJSON {
-    origin: string;
-    senderSignature: Uint8Array;
-    signature: Uint8Array;
-}
-
-export { type SignedTransactionRequestJSON };
