3abc74083b0c953afe373436b7d28035784e58d8
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-09-24 07:46:06 +0100

    feat 2476: Transaction Request pattern refactoring...

--- packages/sdk/src/thor/thorest/json/GasPayerSignedTransactionRequestJSON.ts
+++
@@ -1,8 +0,0 @@
-import { type TransactionRequestJSON } from '@thor/thorest/json/TransactionRequestJSON';
-
-interface GasPayerSignedTransactionRequestJSON extends TransactionRequestJSON {
-    gasPayer: string; // hex address
-    gasPayerSignature: string; // hex signature
-}
-
-export { type GasPayerSignedTransactionRequestJSON };
