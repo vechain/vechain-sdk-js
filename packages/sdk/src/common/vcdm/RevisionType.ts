765d81b0053a92725e643b068b6d41dbc441e847
clayton neal <clayton.neal@vechain.org>
2025-10-08 09:32:21 +0100

    fix: remove fork detector

--- packages/sdk/src/common/vcdm/RevisionType.ts
+++ packages/sdk/src/common/vcdm/RevisionType.ts
@@ -2,9 +2,9 @@
  * The type of a Revision.
  */
 enum RevisionType {
-    Label,
-    BlockNumber,
-    BlockId,
+    Label = 0,
+    BlockNumber = 1,
+    BlockId = 2
 }
 
 export { RevisionType };
