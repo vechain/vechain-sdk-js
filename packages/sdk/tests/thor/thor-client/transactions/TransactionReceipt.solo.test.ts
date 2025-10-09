648ee1c16e7938988f835217553f0a2c69016b66
clayton neal <clayton.neal@vechain.org>
2025-10-09 13:05:59 +0100

    chore: add tests

--- packages/sdk/tests/thor/thor-client/transactions/TransactionReceipt.solo.test.ts
+++ packages/sdk/tests/thor/thor-client/transactions/TransactionReceipt.solo.test.ts
@@ -1,5 +1,5 @@
 import { FetchHttpClient } from '@common/http';
-import { Address } from '@common/vcdm';
+import { Hex } from '@common/vcdm';
 import { describe, expect, test } from '@jest/globals';
 import { ThorClient } from '@thor/thor-client';
 import { TransactionReceipt } from '@thor/thor-client/model/transactions';
@@ -12,7 +12,31 @@ import { getConfigData } from '@vechain/sdk-solo-setup';
 describe('TransactionReceipt SOLO tests', () => {
     test('ok <- tx receipt for vtho seeding', async () => {
         const soloConfig = getConfigData();
-        const soloTxId = Address.of(soloConfig.SEED_VTHO_TX_ID);
+        const soloTxId = Hex.of(soloConfig.SEED_VTHO_TX_ID);
+        const thorClient = ThorClient.at(
+            FetchHttpClient.at(new URL(ThorNetworks.SOLONET))
+        );
+        const receipt =
+            await thorClient.transactions.getTransactionReceipt(soloTxId);
+        expect(receipt).toBeDefined();
+        expect(receipt).not.toBeNull();
+        expect(receipt).toBeInstanceOf(TransactionReceipt);
+    });
+    test('ok <- tx receipt for vet seeding', async () => {
+        const soloConfig = getConfigData();
+        const soloTxId = Hex.of(soloConfig.SEED_VET_TX_ID);
+        const thorClient = ThorClient.at(
+            FetchHttpClient.at(new URL(ThorNetworks.SOLONET))
+        );
+        const receipt =
+            await thorClient.transactions.getTransactionReceipt(soloTxId);
+        expect(receipt).toBeDefined();
+        expect(receipt).not.toBeNull();
+        expect(receipt).toBeInstanceOf(TransactionReceipt);
+    });
+    test('ok <- tx receipt for test token seeding', async () => {
+        const soloConfig = getConfigData();
+        const soloTxId = Hex.of(soloConfig.SEED_TEST_TOKEN_TX_ID);
         const thorClient = ThorClient.at(
             FetchHttpClient.at(new URL(ThorNetworks.SOLONET))
         );
